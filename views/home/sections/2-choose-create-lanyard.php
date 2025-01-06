b3466b210afd678d9ca9cbde65980ffe815467b6
Laura  Rozo <laurarozo@MacBook-Pro-de-Laura.local>
2025-01-06 13:17:50 -0500

    “...”

--- views/home/sections/2-choose-create-lanyard.php
+++ views/home/sections/2-choose-create-lanyard.php
@@ -1,57 +1,52 @@
 <section class="chooseCreateLanyard">
+    <h1>Information About Our Customisation Options</h1>
+
+
   <!-- Section for choosing or creating lanyards -->
     <div class="container_choose_lanyard_information">
       <div class="container_choose_material_information">
-        <div class="box_material_information">
-
+        <div class="box_material_information box_material_information_border_top">
+          <h3>Material</h3>
         </div>
         <div class="box_material_information">
-
+          <h3>Lantard Type </h3>
         </div>
         <div class="box_material_information">
-
+          <h3>Width</h3>
         </div>
         <div class="box_material_information">
-
+          <h3>Side printed </h3>
         </div>
         <div class="box_material_information">
-
+          <h3>Clips</h3>
         </div>
         <div class="box_material_information">
-
+          <h3>Attachment </h3>
         </div>
         <div class="box_material_information">
-
+          <h3>Accessories </h3>
         </div>
         <div class="box_material_information">
-
+          <h3>Color Quantity </h3>
+        </div>
+        <div class="box_material_information">
+          <h3>Artwork-Manual </h3>
+        </div>
+        <div class="box_material_information">
+          <h3>Artwork </h3>
+        </div>
+        <div class="box_material_information">
+          <h3>Background</h3>
+        </div>
+        <div class="box_material_information">
+          <h3>Text </h3>
+        </div>
+        <div class="box_material_information">
+          <h3>Image</h3>
         </div>
       </div>
       <div class="container_choose_material_information">
-        <div class="container_box_information">
-          <div class="container_box">
-
-          </div>
-          <div class="container_box">
-
-          </div>
-          <div class="container_box">
-
-          </div>
-          <div class="container_box">
-
-          </div>
-          <div class="container_box">
-
-          </div>
-
-        </div>
-        <div class="container_img_information">
-
-        </div>
-        <div class="container_general_information">
-
-        </div>
+        
       </div>
     </div>
 
