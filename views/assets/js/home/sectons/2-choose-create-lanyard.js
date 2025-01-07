33a9b12cc3cabbd73b4bd2260a87c3be431a68aa
Laura  Rozo <laurarozo@MacBook-Pro-de-Laura.local>
2025-01-06 22:23:36 -0500

    “...”

--- views/assets/js/home/sectons/2-choose-create-lanyard.js
+++ views/assets/js/home/sectons/2-choose-create-lanyard.js
@@ -1,8 +1,14 @@
 class ChooseCreateLanyard {
   constructor() {
-
+    arros_material_information[0].addEventListener("click", function(){
+      alert("ho");
+    });
+    arros_material_information[1].addEventListener("click", function(){
+      alert("hi");
+    });
 
   }
-
-
+}
+const box_information2 = document.querySelectorAll(".box_information2");
+const arros_material_information = document.querySelectorAll(".arros_material_information");
 const chooseCreateLanyard = new ChooseCreateLanyard();
--- views/home/sections/2-choose-create-lanyard.php
+++ views/home/sections/2-choose-create-lanyard.php
@@ -45,8 +45,29 @@
           <h3>Image</h3>
         </div>
       </div>
+
       <div class="container_choose_material_information">
-        
+        <div class="box_container_information">
+          <div class="arrows_container_information">
+            <img class="arros_material_information" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_left.png" alt="">
+            <img class="arros_material_information" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_right.png" alt="">
+          </div>
+
+          <div class="box_information2" style="display:none;">
+            <h1>MATERIAL</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
+            <h3>Ribbed Polyester</h3>
+            <p>Durable, textured fabric known for its esilience and vivid color retention,
+              ideal for frequent use and detailed designs.</p>
+          </div>
+          <div class="box_information2">
+            <h1>MATERIAL</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-2-Tabular.jpg" alt="">
+            <h3>Tubular</h3>
+            <p>Made from tube-stitched polyester, these lanyards
+               offer a comfortable, shoelace-like texture, perfect for economical, everyday use..</p>
+          </div>
+        </div>
       </div>
     </div>
 
