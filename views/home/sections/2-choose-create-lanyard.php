94a6fff6a352d7356425d32c94e3802551966a61
Laura  Rozo <laurarozo@MacBook-Pro-de-Laura.local>
2025-01-07 16:36:54 -0500

    “...”

--- views/home/sections/2-choose-create-lanyard.php
+++ views/home/sections/2-choose-create-lanyard.php
@@ -45,7 +45,6 @@
           <h3>Image</h3>
         </div>
       </div>
-
       <div class="container_choose_material_information">
         <div class="box_container_information">
           <div class="arrows_container_information">
@@ -62,12 +61,287 @@
           </div>
           <div class="box_information2">
             <h1>MATERIAL</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-5-Dye-sub-Recycled-PET.jpg" alt="">
+            <h3>Dye Sub RPET</h3>
+            <p>Made from recycled PET, these lanyards feature vibrant, sublimation-dyed
+              graphics, combining sustainability with high-quality design.</p>
+          </div>
+          <div class="box_information2">
+            <h1>MATERIAL</h1>
             <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-2-Tabular.jpg" alt="">
             <h3>Tubular</h3>
             <p>Made from tube-stitched polyester, these lanyards
                offer a comfortable, shoelace-like texture, perfect for economical, everyday use..</p>
           </div>
+          <div class="box_information2">
+            <h1>MATERIAL</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-4-Dye-sublimation.jpg" alt="">
+            <h3>Dye Sub polyester</h3>
+            <p>High-resolution, color-rich designs achieved through dye
+              sublimation on smooth polyester, blending quality and durability.</p>
+          </div>
+          <div class="box_information2">
+            <h1>MATERIAL</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-5-Dye-sub-Recycled-PET.jpg" alt="">
+            <h3>RPET Polyester</h3>
+            <p>Manufactured with recycled plastic from PET bottles, durable, eco-friendly, and perfect for a sustainable world.</p>
+          </div>
         </div>
+
+        <div class="box_container_information">
+          <div class="arrows_container_information_lanyard_type">
+            <img class="arrows_information_lanyard_type" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_left.png" alt="">
+            <img class="arrows_information_lanyard_type" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_right.png" alt="">
+          </div>
+          <div class="box_information2_lanyard_type">
+            <h1>LANYARD TYPE</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
+            <h3>Ribbed Polyester</h3>
+            <p>Durable, textured fabric known for its esilience and vivid color retention,
+              ideal for frequent use and detailed designs.</p>
+          </div>
+          <div class="box_information2_lanyard_type">
+            <h1>LANYARD TYPE</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
+            <h3>Ribbed Polyester2</h3>
+            <p>Durable, textured fabric known for its esilience and vivid color retention,
+              ideal for frequent use and detailed designs.</p>
+          </div>
+        </div>
+
+        <div class="box_container_information">
+          <div class="arrows_container_information_width">
+            <img class="arrows_information_width" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_left.png" alt="">
+            <img class="arrows_information_width" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_right.png" alt="">
+          </div>
+          <div class="box_information2_width">
+            <h1>WIDTH</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
+            <h3>Ribbed Polyester</h3>
+            <p>Durable, textured fabric known for its esilience and vivid color retention,
+              ideal for frequent use and detailed designs.</p>
+          </div>
+          <div class="box_information2_width">
+            <h1>WIDTH</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
+            <h3>Ribbed Polyester2</h3>
+            <p>Durable, textured fabric known for its esilience and vivid color retention,
+              ideal for frequent use and detailed designs.</p>
+          </div>
+        </div>
+
+        <div class="box_container_information">
+          <div class="arrows_container_information_side_printed">
+            <img class="arrows_information_side_printed" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_left.png" alt="">
+            <img class="arrows_information_side_printed" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_right.png" alt="">
+          </div>
+          <div class="box_information2_side_printed">
+            <h1>SIDE PRINTED</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
+            <h3>Ribbed Polyester</h3>
+            <p>Durable, textured fabric known for its esilience and vivid color retention,
+              ideal for frequent use and detailed designs.</p>
+          </div>
+          <div class="box_information2_side_printed">
+            <h1>SIDE PRINTED</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
+            <h3>Ribbed Polyester2</h3>
+            <p>Durable, textured fabric known for its esilience and vivid color retention,
+              ideal for frequent use and detailed designs.</p>
+          </div>
+        </div>
+
+        <div class="box_container_information">
+          <div class="arrows_container_information_clips">
+            <img class="arrows_information_clips" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_left.png" alt="">
+            <img class="arrows_information_clips" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_right.png" alt="">
+          </div>
+          <div class="box_information2_clips">
+            <h1>CLIPS</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
+            <h3>Ribbed Polyester</h3>
+            <p>Durable, textured fabric known for its esilience and vivid color retention,
+              ideal for frequent use and detailed designs.</p>
+          </div>
+          <div class="box_information2_clips">
+            <h1>CLIPS</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
+            <h3>Ribbed Polyester2</h3>
+            <p>Durable, textured fabric known for its esilience and vivid color retention,
+              ideal for frequent use and detailed designs.</p>
+          </div>
+        </div>
+
+        <div class="box_container_information">
+          <div class="arrows_container_information_attachment">
+            <img class="arrows_information_attachment" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_left.png" alt="">
+            <img class="arrows_information_attachment" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_right.png" alt="">
+          </div>
+          <div class="box_information2_attachment">
+            <h1>ATTACHMENT</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
+            <h3>Ribbed Polyester</h3>
+            <p>Durable, textured fabric known for its esilience and vivid color retention,
+              ideal for frequent use and detailed designs.</p>
+          </div>
+          <div class="box_information2_attachment">
+            <h1>ATTACHMENT</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
+            <h3>Ribbed Polyester2</h3>
+            <p>Durable, textured fabric known for its esilience and vivid color retention,
+              ideal for frequent use and detailed designs.</p>
+          </div>
+        </div>
+
+        <div class="box_container_information">
+          <div class="arrows_container_information_accessories">
+            <img class="arrows_information_accessories" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_left.png" alt="">
+            <img class="arrows_information_accessories" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_right.png" alt="">
+          </div>
+          <div class="box_information2_accessories">
+            <h1>ACCESORIES</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
+            <h3>Ribbed Polyester</h3>
+            <p>Durable, textured fabric known for its esilience and vivid color retention,
+              ideal for frequent use and detailed designs.</p>
+          </div>
+          <div class="box_information2_accessories">
+            <h1>ACCESORIES</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
+            <h3>Ribbed Polyester2</h3>
+            <p>Durable, textured fabric known for its esilience and vivid color retention,
+              ideal for frequent use and detailed designs.</p>
+          </div>
+        </div>
+
+        <div class="box_container_information">
+          <div class="arrows_container_information_color_quantity">
+            <img class="arrows_information_color_quantity" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_left.png" alt="">
+            <img class="arrows_information_color_quantity" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_right.png" alt="">
+          </div>
+          <div class="box_information2_color_quantity">
+            <h1>COLOR QUANTITY</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
+            <h3>Ribbed Polyester</h3>
+            <p>Durable, textured fabric known for its esilience and vivid color retention,
+              ideal for frequent use and detailed designs.</p>
+          </div>
+          <div class="box_information2_color_quantity">
+            <h1>COLOR QUANTITY</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
+            <h3>Ribbed Polyester2</h3>
+            <p>Durable, textured fabric known for its esilience and vivid color retention,
+              ideal for frequent use and detailed designs.</p>
+          </div>
+        </div>
+
+        <div class="box_container_information">
+          <div class="arrows_container_information_artwork_manual">
+            <img class="arrows_information_artwork_manual" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_left.png" alt="">
+            <img class="arrows_information_artwork_manual" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_right.png" alt="">
+          </div>
+          <div class="box_information2_artwork_manual">
+            <h1>ARTWORK-MANUAL</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
+            <h3>Ribbed Polyester</h3>
+            <p>Durable, textured fabric known for its esilience and vivid color retention,
+              ideal for frequent use and detailed designs.</p>
+          </div>
+          <div class="box_information2_artwork_manual">
+            <h1>ARTWORK-MANUAL</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
+            <h3>Ribbed Polyester2</h3>
+            <p>Durable, textured fabric known for its esilience and vivid color retention,
+              ideal for frequent use and detailed designs.</p>
+          </div>
+        </div>
+
+        <div class="box_container_information">
+          <div class="arrows_container_information_artwork">
+            <img class="arrows_information_artwork" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_left.png" alt="">
+            <img class="arrows_information_artwork" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_right.png" alt="">
+          </div>
+          <div class="box_information2_artwork">
+            <h1>ARTWORK</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
+            <h3>Ribbed Polyester</h3>
+            <p>Durable, textured fabric known for its esilience and vivid color retention,
+              ideal for frequent use and detailed designs.</p>
+          </div>
+          <div class="box_information2_artwork">
+            <h1>ARTWORK</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
+            <h3>Ribbed Polyester2</h3>
+            <p>Durable, textured fabric known for its esilience and vivid color retention,
+              ideal for frequent use and detailed designs.</p>
+          </div>
+        </div>
+
+        <div class="box_container_information">
+          <div class="arrows_container_information_background">
+            <img class="arrows_information_background" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_left.png" alt="">
+            <img class="arrows_information_background" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_right.png" alt="">
+          </div>
+          <div class="box_information2_background">
+            <h1>BACKGROUND</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
+            <h3>Ribbed Polyester</h3>
+            <p>Durable, textured fabric known for its esilience and vivid color retention,
+              ideal for frequent use and detailed designs.</p>
+          </div>
+          <div class="box_information2_background">
+            <h1>BACKGROUND</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
+            <h3>Ribbed Polyester2</h3>
+            <p>Durable, textured fabric known for its esilience and vivid color retention,
+              ideal for frequent use and detailed designs.</p>
+          </div>
+        </div>
+
+        <div class="box_container_information">
+          <div class="arrows_container_information_text">
+            <img class="arrows_information_text" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_left.png" alt="">
+            <img class="arrows_information_text" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_right.png" alt="">
+          </div>
+          <div class="box_information2_text">
+            <h1>TEXT</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
+            <h3>Ribbed Polyester</h3>
+            <p>Durable, textured fabric known for its esilience and vivid color retention,
+              ideal for frequent use and detailed designs.</p>
+          </div>
+          <div class="box_information2_text">
+            <h1>TEXT</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
+            <h3>Ribbed Polyester2</h3>
+            <p>Durable, textured fabric known for its esilience and vivid color retention,
+              ideal for frequent use and detailed designs.</p>
+          </div>
+        </div>
+
+        <div class="box_container_information">
+          <div class="arrows_container_information_image">
+            <img class="arrows_information_image" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_left.png" alt="">
+            <img class="arrows_information_image" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_right.png" alt="">
+          </div>
+          <div class="box_information2_image">
+            <h1>IMAGE</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
+            <h3>Ribbed Polyester</h3>
+            <p>Durable, textured fabric known for its esilience and vivid color retention,
+              ideal for frequent use and detailed designs.</p>
+          </div>
+          <div class="box_information2_image">
+            <h1>IMAGE</h1>
+            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
+            <h3>Ribbed Polyester2</h3>
+            <p>Durable, textured fabric known for its esilience and vivid color retention,
+              ideal for frequent use and detailed designs.</p>
+          </div>
+        </div>
+
+
+
       </div>
     </div>
 
