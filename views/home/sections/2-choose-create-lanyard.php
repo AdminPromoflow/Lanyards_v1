663745791b1c646eab1d4fd1b6703286184aa3c5
Laura  Rozo <laurarozo@MacBook-Pro-de-Laura.local>
2025-01-07 22:16:35 -0500

    “...”

--- views/home/sections/2-choose-create-lanyard.php
+++ views/home/sections/2-choose-create-lanyard.php
@@ -95,40 +95,95 @@
           </div>
           <div class="box_information2_lanyard_type">
             <h1>LANYARD TYPE</h1>
-            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
-            <h3>Ribbed Polyester</h3>
-            <p>Durable, textured fabric known for its esilience and vivid color retention,
-              ideal for frequent use and detailed designs.</p>
+            <img class="arrows_information_lanyard_type" src="../../views/assets/img/home/2-choose-create-lanyard/2_lanyard_type/two-ends.png" alt="">
+            <h3>Two ends</h3>
+            <p>A lanyard with two ends is a strap featuring two functional ends, typically for holding accessories or ID cards securely.</p>
           </div>
           <div class="box_information2_lanyard_type">
             <h1>LANYARD TYPE</h1>
-            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
-            <h3>Ribbed Polyester2</h3>
-            <p>Durable, textured fabric known for its esilience and vivid color retention,
-              ideal for frequent use and detailed designs.</p>
+            <img class="arrows_information_lanyard_type" src="../../views/assets/img/home/2-choose-create-lanyard/2_lanyard_type/one-end.png" alt="">
+            <h3>One end</h3>
+            <p>A lanyard type one end is a strap with a single functional end for holding accessories or ID cards..</p>
           </div>
         </div>
 
         <div class="box_container_information">
-          <div class="arrows_container_information_width">
-            <img class="arrows_information_width" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_left.png" alt="">
-            <img class="arrows_information_width" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_right.png" alt="">
+            <div class="arrows_container_information_width">
+              <img class="arrows_information_width" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_left.png" alt="">
+              <img class="arrows_information_width" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_right.png" alt="">
+            </div>
+
+            <div class="box_information2_width">
+              <h1>WIDTH</h1>
+              <img class="arrows_information_width" src="../../views/assets/img/home/2_width/one-end-10mm.png" alt="">
+              <h3>One end 10mm</h3>
+              <p>A lanyard one end 10mm is a narrow strap, 10mm wide, with a single functional end for attachments.</p>
+            </div>
+
+            <div class="box_information2_width">
+              <h1>WIDTH</h1>
+              <img class="arrows_information_width" src="../../views/assets/img/home/2_width/one-end-15mm.png" alt="">
+              <h3>One end 15mm</h3>
+              <p>A lanyard one end 15mm is a strap, 15mm wide, with a single functional end for attachments.</p>
+            </div>
+
+            <div class="box_information2_width">
+              <h1>WIDTH</h1>
+              <img class="arrows_information_width" src="../../views/assets/img/home/2_width/one-end-20mm.png" alt="">
+              <h3>One end 20mm</h3>
+              <p>A lanyard one end 20mm is a strap, 20mm wide, with a single functional end for attachments.</p>
+            </div>
+
+            <div class="box_information2_width">
+              <h1>WIDTH</h1>
+              <img class="arrows_information_width" src="../../views/assets/img/home/2_width/one-end-25mm.png" alt="">
+              <h3>One end 25mm</h3>
+              <p>A lanyard one end 25mm is a wider strap, 25mm wide, with a single functional end for attachments.</p>
+            </div>
+
+            <div class="box_information2_width">
+              <h1>WIDTH</h1>
+              <img class="arrows_information_width" src="../../views/assets/img/home/2_width/one-end-30mm.png" alt="">
+              <h3>One end 30mm</h3>
+              <p>A lanyard one end 30mm is a wide strap, 30mm wide, with a single functional end for attachments.</p>
+            </div>
+
+            <div class="box_information2_width">
+              <h1>WIDTH</h1>
+              <img class="arrows_information_width" src="../../views/assets/img/home/2_width/two-end-10mm.png" alt="">
+              <h3>Two end 10mm</h3>
+              <p>A lanyard two ends 10mm is a narrow strap, 10mm wide, with two functional ends for holding items securely.</p>
+            </div>
+
+            <div class="box_information2_width">
+              <h1>WIDTH</h1>
+              <img class="arrows_information_width" src="../../views/assets/img/home/2_width/two-end-15mm.png" alt="">
+              <h3>Two end 15mm</h3>
+              <p> A lanyard two ends 15mm is a strap, 15mm wide, with two functional ends for holding items securely.</p>
+            </div>
+
+            <div class="box_information2_width">
+              <h1>WIDTH</h1>
+              <img class="arrows_information_width" src="../../views/assets/img/home/2_width/two-end-20mm.png" alt="">
+              <h3>Two end 20mm</h3>
+              <p>A lanyard two ends 20mm is a strap, 20mm wide, with two functional ends for holding items securely.</p>
+            </div>
+
+            <div class="box_information2_width">
+              <h1>WIDTH</h1>
+              <img class="arrows_information_width" src="../../views/assets/img/home/2_width/two-end-25mm.png" alt="">
+              <h3>Two end 25mm</h3>
+              <p>A lanyard two ends 25mm is a wider strap, 25mm wide, with two functional ends for holding items securely.</p>
+            </div>
+
+            <div class="box_information2_width">
+              <h1>WIDTH</h1>
+              <img class="arrows_information_width" src="../../views/assets/img/home/2_width/two-end-30mm.png" alt="">
+              <h3>Two end 30mm</h3>
+              <p>A lanyard two ends 30mm is a wide strap, 30mm wide, with two functional ends for holding items securely.</p>
+            </div>
+
           </div>
-          <div class="box_information2_width">
-            <h1>WIDTH</h1>
-            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
-            <h3>Ribbed Polyester</h3>
-            <p>Durable, textured fabric known for its esilience and vivid color retention,
-              ideal for frequent use and detailed designs.</p>
-          </div>
-          <div class="box_information2_width">
-            <h1>WIDTH</h1>
-            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
-            <h3>Ribbed Polyester2</h3>
-            <p>Durable, textured fabric known for its esilience and vivid color retention,
-              ideal for frequent use and detailed designs.</p>
-          </div>
-        </div>
 
         <div class="box_container_information">
           <div class="arrows_container_information_side_printed">
@@ -137,17 +192,15 @@
           </div>
           <div class="box_information2_side_printed">
             <h1>SIDE PRINTED</h1>
-            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
-            <h3>Ribbed Polyester</h3>
-            <p>Durable, textured fabric known for its esilience and vivid color retention,
-              ideal for frequent use and detailed designs.</p>
+            <img src="../../views/assets/img/home/2_side_printed/one-side.png" alt="">
+            <h3>One side printed</h3>
+            <p>A lanyard one side printed is a strap with printing on one side for personalisation or identification.</p>
           </div>
           <div class="box_information2_side_printed">
             <h1>SIDE PRINTED</h1>
-            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
-            <h3>Ribbed Polyester2</h3>
-            <p>Durable, textured fabric known for its esilience and vivid color retention,
-              ideal for frequent use and detailed designs.</p>
+            <img src="../../views/assets/img/home/2_side_printed/two-side.png" alt="">
+            <h3>Two sides printed</h3>
+            <p>A lanyard two sides printed is a strap with printing on both sides for enhanced visibility and personalisation.</p>
           </div>
         </div>
 
@@ -156,19 +209,42 @@
             <img class="arrows_information_clips" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_left.png" alt="">
             <img class="arrows_information_clips" src="../../views/assets/img/home/2-choose-create-lanyard/arrow_right.png" alt="">
           </div>
+
           <div class="box_information2_clips">
             <h1>CLIPS</h1>
-            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
-            <h3>Ribbed Polyester</h3>
-            <p>Durable, textured fabric known for its esilience and vivid color retention,
-              ideal for frequent use and detailed designs.</p>
+            <img src="../../views/assets/img/home/2_clip/crab_hook.png" alt="">
+            <h3>Crab</h3>
+            <p>A crab hook is a sturdy metal hook, designed to securely and durably attach ID cards, badges, or accessories for frequent use.</p>
           </div>
           <div class="box_information2_clips">
             <h1>CLIPS</h1>
-            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
-            <h3>Ribbed Polyester2</h3>
-            <p>Durable, textured fabric known for its esilience and vivid color retention,
-              ideal for frequent use and detailed designs.</p>
+            <img src="../../views/assets/img/home/2_clip/crocodile_clip.png" alt="">
+            <h3>Crocodile</h3>
+            <p>A crocodile clip is a metal clip with teeth, designed to securely attach ID cards, badges, or accessories to clothing.</p>
+          </div>
+          <div class="box_information2_clips">
+            <h1>CLIPS</h1>
+            <img src="../../views/assets/img/home/2_clip/dog_clip.png" alt="">
+            <h3>Dog</h3>
+            <p>A dog clip is a metal hook with a spring, designed to securely attach ID cards, badges, or accessories.</p>
+          </div>
+          <div class="box_information2_clips">
+            <h1>CLIPS</h1>
+            <img src="../../views/assets/img/home/2_clip/metal_j_hook.png" alt="">
+            <h3>Metal J hook</h3>
+            <p>A metal J hook is a J-shaped metal hook, designed to securely and practically attach ID cards, badges, or accessories.</p>
+          </div>
+          <div class="box_information2_clips">
+            <h1>CLIPS</h1>
+            <img src="../../views/assets/img/home/2_clip/slide_clip.png" alt="">
+            <h3>Slide</h3>
+            <p>A crab hook is a sturdy metal hook, designed to securely and durably attach ID cards, badges, or accessories for frequent use.</p>
+          </div>
+          <div class="box_information2_clips">
+            <h1>CLIPS</h1>
+            <img src="../../views/assets/img/home/2_clip/swvel_clip.png" alt="">
+            <h3>Swivel</h3>
+            <p>A swivel clip is a metal hook with a rotating base, designed to securely and flexibly attach ID cards or accessories.</p>
           </div>
         </div>
 
@@ -179,17 +255,21 @@
           </div>
           <div class="box_information2_attachment">
             <h1>ATTACHMENT</h1>
-            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
-            <h3>Ribbed Polyester</h3>
-            <p>Durable, textured fabric known for its esilience and vivid color retention,
-              ideal for frequent use and detailed designs.</p>
+            <img src="../../views/assets/img/home/2_attachment/quick-release-black.png" alt="">
+            <h3>Quick release black</h3>
+            <p>A black attachment is an accessory that connects lanyards to ID cards, designed in black for a professional appearance.</p>
           </div>
           <div class="box_information2_attachment">
             <h1>ATTACHMENT</h1>
-            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
-            <h3>Ribbed Polyester2</h3>
-            <p>Durable, textured fabric known for its esilience and vivid color retention,
-              ideal for frequent use and detailed designs.</p>
+            <img src="../../views/assets/img/home/2_attachment/quick-release-metal.png" alt="">
+            <h3>Quick release metal</h3>
+            <p>A black attachment is an accessory that connects lanyards to ID cards, designed in metal for a professional appearance.</p>
+          </div>
+          <div class="box_information2_attachment">
+            <h1>ATTACHMENT</h1>
+            <img src="../../views/assets/img/home/2_attachment/quick-release-plastics-colour.png" alt="">
+            <h3>Quick release plastics colour</h3>
+            <p>A black attachment is an accessory that connects lanyards to ID cards, designed in plastics colour for a professional appearance..</p>
           </div>
         </div>
 
@@ -200,17 +280,38 @@
           </div>
           <div class="box_information2_accessories">
             <h1>ACCESORIES</h1>
-            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
-            <h3>Ribbed Polyester</h3>
-            <p>Durable, textured fabric known for its esilience and vivid color retention,
-              ideal for frequent use and detailed designs.</p>
+            <img src="../../views/assets/img/home/2_accessories/6.png" alt="">
+            <h3>Rigid Card Holder</h3>
+            <p>A Rigid Card Holder is a durable accessory for securely holding ID cards,
+              available in Clear Plastic (closed face),Coloured Plastic (closed face)  or
+              Coloured Plastic (open face).</p>
           </div>
           <div class="box_information2_accessories">
             <h1>ACCESORIES</h1>
-            <img src="../../views/assets/img/home/2-choose-create-lanyard/Material-1-Flat-Polyester.jpg" alt="">
-            <h3>Ribbed Polyester2</h3>
-            <p>Durable, textured fabric known for its esilience and vivid color retention,
-              ideal for frequent use and detailed designs.</p>
+            <img src="../../views/assets/img/home/2_accessories/7.png" alt="">
+            <h3>Plastic Wallet</h3>
+            <p>A Plastic Wallet is a transparent lanyard accessory for protecting
+              cards, available in three sizes: 86x56mm, 103x70mm, 138x88mm.</p>
+          </div>
+          <div class="box_information2_accessories">
+            <h1>ACCESORIES</h1>
+            <img src="../../views/assets/img/home/2_accessories/9.png" alt="">
+            <h3>Cord Lock</h3>
+            <p>A Cord Lock is a lanyard accessory that adjusts the cord's length, providing both comfort and security.</p>
+          </div>
+          <div class="box_information2_accessories">
+            <h1>ACCESORIES</h1>
+            <img src="../../views/assets/img/home/2_accessories/11.png" alt="">
+            <h3>Whistle</h3>
+            <p>A Whistle is a practical and audible lanyard accessory, designed
+              for emergencies, sports, or recreational activities with a loud sound.</p>
+          </div>
+          <div class="box_information2_accessories">
+            <h1>ACCESORIES</h1>
+            <img src="../../views/assets/img/home/2_accessories/12.png" alt="">
+            <h3>Retractable Reel</h3>
+            <p>A Retractable Reel is an extendable lanyard accessory for cards,
+               available in various types: plastic, round, oval, metal, printing(per screen).</p>
           </div>
         </div>
 
@@ -345,4 +446,5 @@
       </div>
     </div>
 
+  <!--  -->
 </section>
