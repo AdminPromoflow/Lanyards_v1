83435d380373babce208dcf4a14de073b46609b0
Laura  Rozo <laurarozo@MacBook-Pro-de-Laura.local>
2025-01-13 15:03:43 -0500

    “...”

--- views/assets/js/customize-lanyard/sections/checkout.js
+++ views/assets/js/customize-lanyard/sections/checkout.js
@@ -5,14 +5,14 @@ class Checkout {
 updateItems(){
 
 
-checkoutMaterial.innerHTML = "Material: " + material.getMaterialSelected(oneTwoEndsClass.getTypeLanyardSelected());
+/*checkoutMaterial.innerHTML = "Material: " + material.getMaterialSelected(oneTwoEndsClass.getTypeLanyardSelected());
 checkoutLanyardLype.innerHTML = "Lanyard type: " + oneTwoEndsClass.getTypeLanyardSelected();
 checkoutWidth.innerHTML = "Width: " + widthClass.getWidthSelected();
 checkoutSidePrinted.innerHTML = "Side printed: " + sidePrintedClass.getSidePrintedSelected();
 checkoutClips.innerHTML = "Clips: " + clipClass.getClipSelected();
 checkoutAttachment.innerHTML = "Attachment: " + attachmentClass.getAttachmentSelected();
 checkoutColourQuantity.innerHTML = "Colour Quantity: " + colourClass.getColourSelected();
-checkoutArtworkManual.innerHTML = "Artwork - Manual: " + artworkManualClass.getArtworkManual();
+checkoutArtworkManual.innerHTML = "Artwork - Manual: " + artworkManualClass.getArtworkManual();*/
 }
 }
 const checkoutMaterial = document.getElementById("checkout-material");
@@ -24,4 +24,4 @@ const checkoutAttachment = document.getElementById("checkout-attachment");
 const checkoutColourQuantity = document.getElementById("checkout-colour-quantity");
 const checkoutArtworkManual = document.getElementById("checkout-artwork-manual");
 
-const artworkClassFinal = new Checkout();
+const checkout = new Checkout();
