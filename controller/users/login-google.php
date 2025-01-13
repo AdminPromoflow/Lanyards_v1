115bd7a5f18d9c7088d29931e81443cbe8ce4305
Laura  Rozo <laurarozo@MacBook-Pro-de-Laura.local>
2025-01-13 15:57:01 -0500

    “...”

--- controller/users/login-google.php
+++ controller/users/login-google.php
@@ -53,7 +53,7 @@ class ApiHandlerLoginGoogle
     }
 
     private function handleLoginGoogleSecondPart() {
-      echo "string";exit;
+      echo json_encode("string");exit;
         // Configuración inicial de Google OAuth
         $clientID = '1022332881668-587bktseqso57k6m2dmpfao53vasg83b.apps.googleusercontent.com';
         $clientSecret = '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';
