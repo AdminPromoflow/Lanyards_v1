e79ed410a4118beaa9d34898ec29357cb831550a
Laura  Rozo <laurarozo@MacBook-Pro-de-Laura.local>
2025-01-13 15:19:44 -0500

    “...”

--- views/about_us/index.php
+++ views/about_us/index.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="../../views/assets/img/about_us/favicon.ico" type="image/x-icon">
 
   <!-- Include Google Fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
