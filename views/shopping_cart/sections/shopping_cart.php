99b900b00e3b71f79668d7d436ddb382574df728
Laura  Rozo <laurarozo@MacBook-Pro-de-Laura.local>
2025-01-18 15:52:09 -0500

    “...”

---
+++ views/shopping_cart/sections/shopping_cart.php
@@ -0,0 +1,248 @@
+<link rel="stylesheet" href="../../views/assets/css/shopping_cart/sections/shopping_cart.css">
+<section class="section_shopping_cart">
+  <h1>Shopping Cart</h1>
+  <div class="container_shopping_cart">
+
+    <div class="boxes_shopping_cart">
+      <div class="items_shopping_cart">
+        <div class="product_items_shopping_cart">
+          <div class="elements_product_items_shopping_cart">
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+            <img src="../../views/assets/img/shopping_cart/sections/arrow_right.png" alt="">
+          </div>
+        </div>
+        <div class="descriptions_items_shopping_cart">
+          <div class="elements_descriptions_items_shopping_cart">
+            <h3>Material</h3>
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_descriptions_items_shopping_cart">
+            <h3>Lanyard type</h3>
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_descriptions_items_shopping_cart">
+            <h3>Width</h3>
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_descriptions_items_shopping_cart">
+            <h3>Side printed</h3>
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_descriptions_items_shopping_cart">
+            <h3>Clips</h3>
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_descriptions_items_shopping_cart">
+            <h3>Attachment</h3>
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_descriptions_items_shopping_cart">
+            <h3>Accessories</h3>
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_descriptions_items_shopping_cart">
+            <h3>Colour Quantity</h3>
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+          </div>
+
+        </div>
+        <div class="summary_items_shopping_cart">
+          <div class="elements_summary_items_shopping_cart">
+            <h3>Cost per unit</h3>
+            <h3></h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_summary_items_shopping_cart">
+            <h3>Amount</h3>
+            <h3></h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_summary_items_shopping_cart">
+            <h3>Subtotal</h3>
+            <h3></h3>
+            <h3>hola3</h3>
+          </div>
+
+        </div>
+      </div>
+      <div class="items_shopping_cart">
+        <div class="product_items_shopping_cart">
+          <div class="elements_product_items_shopping_cart">
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+            <img src="../../views/assets/img/shopping_cart/sections/arrow_right.png" alt="">
+          </div>
+        </div>
+        <div class="descriptions_items_shopping_cart">
+          <div class="elements_descriptions_items_shopping_cart">
+            <h3>Material</h3>
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_descriptions_items_shopping_cart">
+            <h3>Lanyard type</h3>
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_descriptions_items_shopping_cart">
+            <h3>Width</h3>
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_descriptions_items_shopping_cart">
+            <h3>Side printed</h3>
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_descriptions_items_shopping_cart">
+            <h3>Clips</h3>
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_descriptions_items_shopping_cart">
+            <h3>Attachment</h3>
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_descriptions_items_shopping_cart">
+            <h3>Accessories</h3>
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_descriptions_items_shopping_cart">
+            <h3>Colour Quantity</h3>
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+          </div>
+
+        </div>
+        <div class="summary_items_shopping_cart">
+          <div class="elements_summary_items_shopping_cart">
+            <h3>Cost per unit</h3>
+            <h3></h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_summary_items_shopping_cart">
+            <h3>Amount</h3>
+            <h3></h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_summary_items_shopping_cart">
+            <h3>Subtotal</h3>
+            <h3></h3>
+            <h3>hola3</h3>
+          </div>
+
+        </div>
+      </div>
+      <div class="items_shopping_cart">
+        <div class="product_items_shopping_cart">
+          <div class="elements_product_items_shopping_cart">
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+            <img src="../../views/assets/img/shopping_cart/sections/arrow_right.png" alt="">
+          </div>
+        </div>
+        <div class="descriptions_items_shopping_cart">
+          <div class="elements_descriptions_items_shopping_cart">
+            <h3>Material</h3>
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_descriptions_items_shopping_cart">
+            <h3>Lanyard type</h3>
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_descriptions_items_shopping_cart">
+            <h3>Width</h3>
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_descriptions_items_shopping_cart">
+            <h3>Side printed</h3>
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_descriptions_items_shopping_cart">
+            <h3>Clips</h3>
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_descriptions_items_shopping_cart">
+            <h3>Attachment</h3>
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_descriptions_items_shopping_cart">
+            <h3>Accessories</h3>
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_descriptions_items_shopping_cart">
+            <h3>Colour Quantity</h3>
+            <h3>hola2</h3>
+            <h3>hola3</h3>
+          </div>
+
+        </div>
+        <div class="summary_items_shopping_cart">
+          <div class="elements_summary_items_shopping_cart">
+            <h3>Cost per unit</h3>
+            <h3></h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_summary_items_shopping_cart">
+            <h3>Amount</h3>
+            <h3></h3>
+            <h3>hola3</h3>
+          </div>
+          <div class="elements_summary_items_shopping_cart">
+            <h3>Subtotal</h3>
+            <h3></h3>
+            <h3>hola3</h3>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+    <div class="boxes_shopping_cart">
+      <h3>You Orden</h3>
+      <div class="container_boxes_shopping_cart">
+        <div class="items2_shopping_cart">
+          <h3>Hola1</h3>
+          <h3>Hola2</h3>
+        </div>
+        <div class="items2_shopping_cart">
+          <div class="items2_shopping_cart">
+            <h3>Hola1</h3>
+            <h3>Hola2</h3>
+          </div>
+        </div>
+        <div class="items2_shopping_cart">
+          <div class="items2_shopping_cart">
+            <h3>Hola1</h3>
+            <h3>Hola2</h3>
+          </div>
+        </div>
+      </div>
+      <div class="container_boxes_shopping_cart">
+
+      </div>
+    </div>
+  </div>
+
+</section>
+<script src="../../views/assets/js/shopping_cart/sections/shopping_cart.js" type="text/javascript">
+
+</script>
