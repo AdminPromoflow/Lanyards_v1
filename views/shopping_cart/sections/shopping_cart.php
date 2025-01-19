9c98c68813068317187a475f8b7bd9057d18d778
Laura  Rozo <laurarozo@MacBook-Pro-de-Laura.local>
2025-01-18 20:13:59 -0500

    “...”

--- views/shopping_cart/sections/shopping_cart.php
+++ views/shopping_cart/sections/shopping_cart.php
@@ -1,6 +1,7 @@
 <link rel="stylesheet" href="../../views/assets/css/shopping_cart/sections/shopping_cart.css">
 <section class="section_shopping_cart">
   <h1>Shopping Cart</h1>
+  <p>Hello, these are the products that you have added to your shopping cart.</p>
   <div class="container_shopping_cart">
 
     <div class="boxes_shopping_cart">
@@ -214,22 +215,49 @@
 
         </div>
       </div>
-
     </div>
     <div class="boxes_shopping_cart">
-      <h3>You Orden</h3>
+      <h3 class="title_boxes_shopping_cart">You Orden</h3>
       <div class="boxes_container_shopping_cart">
         <div class="items2_shopping_cart">
-          <h3>hi1</h3>
-          <h3>hi1</h3>
-        </div>
-        <div class="items2_shopping_cart">
-
-        </div>
-        <div class="items2_shopping_cart">
-
+          <h3>Product</h3>
+          <h3>Subtotal</h3>
         </div>
 
+        <div class="elements_boxes_container_shopping_cart">
+          <h3>Material</h3>
+          <h3>hola2</h3>
+        </div>
+        <div class="elements_boxes_container_shopping_cart">
+          <h3>Material</h3>
+          <h3>hola2</h3>
+        </div>
+        <div class="elements_boxes_container_shopping_cart">
+          <h3>Material</h3>
+          <h3>hola2</h3>
+        </div>
+      </div>
+      <div class="boxes2_container_shopping_cart">
+
+        <div class="elements_boxes2_container_shopping_cart">
+          <h3>Material</h3>
+          <h3>hola2</h3>
+        </div>
+        <div class="elements_boxes2_container_shopping_cart">
+          <h3>Material</h3>
+          <h3>hola2</h3>
+        </div>
+        <div class="elements_boxes2_container_shopping_cart">
+          <h3>Material</h3>
+          <h3>hola2</h3>
+        </div>
+      </div>
+      <div class="boxes3_container_shopping_cart">
+        <label>
+            <input type="checkbox" name="option3" value="Option3">
+            By clicking 'Place Order,' I confirm that I have read and agree to the website's terms and conditions.
+        </label>
+        <button type="button" name="button">Place Order</button>
       </div>
 
     </div>
