31d0768c614b17cb577cd7d5db8a56f82a9ff7c0
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-01-28 09:01:57 -0500

    “...”

--- views/global/charging.php
+++ views/global/charging.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="../../views/assets/css/global/charging.css">
 <div class="charging_background" id="">
-  
+  <img src="../../views/assets/img/charging/charge.gif" alt="">
 </div>
 <script src="../../views/assets/js/global/charging.js" type="text/javascript">
 
