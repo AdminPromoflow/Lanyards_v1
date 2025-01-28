bc729095513d6fab4aac1468e5e22b6955d4939a
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-01-28 09:12:44 -0500

    “...”

--- views/assets/js/global/charging.js
+++ views/assets/js/global/charging.js
@@ -1,6 +1,16 @@
 class Charging {
   constructor() {
+    this.hideShowchargin(false);
+  }
+  hideShowchargin(action){
+    if (action) {
+      charging_background.style.display = "flex";
+    }
+    else {
+      charging_background.style.display = "none";
 
+    }
   }
 }
-const charging = new Charging();
+const charging_background = document.getElementById("charging_background");
+const chargingClass = new Charging();
--- views/global/charging.php
+++ views/global/charging.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="../../views/assets/css/global/charging.css">
-<div class="charging_background" id="">
+<div class="charging_background" id="charging_background">
   <img src="../../views/assets/img/charging/charge.gif" alt="">
   <p>Please wait, we are loading the data.</p>
 </div>
