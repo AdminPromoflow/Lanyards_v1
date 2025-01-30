b17f35e18afe5277086cea9962b49a897aa54d31
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-01-29 19:17:26 -0500

    “...”

--- views/assets/js/global/register.js
+++ views/assets/js/global/register.js
@@ -160,6 +160,7 @@ class Register {
       return response.json(); // Expecting JSON response
     })
     .then(data => {
+      alert(data);
       chargingClass.hideShowchargin(false); // Hide loading indicator
 
       switch (data.message) {
