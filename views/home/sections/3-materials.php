b0e50e95b80b978d9f6e1bce4799d26010c3ac4e
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-01-30 05:32:50 -0500

    “...”

--- views/home/sections/3-materials.php
+++ views/home/sections/3-materials.php
@@ -1,96 +1,69 @@
 <link rel="stylesheet" href="../assets/css/home/sectons/3-materials.css?v=<?php echo filemtime('../assets/css/home/sectons/3-materials.css'); ?>">
+
 <section class="materials">
   <h1>Create your own lanyard from scratch.</h1>
 
   <div class="containerMaterials">
-
     <div class="containerTextMaterials">
-      <!--<div class="closeMaterial">
-        <img src="../assets/img/home/3-materials/Close-Materials.png" alt="">
-      </div>-->
       <div class="containerTextMaterialsBox">
-        <img src="../../views/assets/img/home/3-materials/icon_lanyard.png" alt="">
+        <img src="../../views/assets/img/home/3-materials/icon_lanyard.png?v=<?php echo filemtime('../../views/assets/img/home/3-materials/icon_lanyard.png'); ?>" alt="">
       </div>
       <div class="containerTextMaterialsBox">
-        <h3 class="material-for-select">Create your our lanyard</h3>
+        <h3 class="material-for-select">Create your own lanyard</h3>
       </div>
       <div class="buttonMaterialsBox">
-        <button  class="openCustomizeLanyardFromMaterials" type="button" name="button">Select</button>
+        <button class="openCustomizeLanyardFromMaterials" type="button">Select</button>
       </div>
     </div>
 
+    <?php
+      $images = [
+        "Material-2-Tabular.jpg",
+        "Material-3-Natural-Bamboo.jpg",
+        "Material-4-Dye-sublimation.jpg",
+        "Material-5-Dye-sub-Recycled-PET.jpg"
+      ];
+      $titles = [
+        "Tubular",
+        "RPET Polyester",
+        "Dye Sub polyester",
+        "Dye Sub RPET"
+      ];
 
-    <div class="containerTextMaterials">
-      <div class="closeMaterial">
-        <img src="../assets/img/home/3-materials/Close-Materials.png" alt="">
+      for ($i = 0; $i < count($images); $i++):
+    ?>
+      <div class="containerTextMaterials">
+        <div class="closeMaterial">
+          <img src="../assets/img/home/3-materials/Close-Materials.png?v=<?php echo filemtime('../assets/img/home/3-materials/Close-Materials.png'); ?>" alt="">
+        </div>
+        <div class="containerTextMaterialsBox">
+          <img src="../assets/img/home/3-materials/<?php echo $images[$i]; ?>?v=<?php echo filemtime("../assets/img/home/3-materials/{$images[$i]}"); ?>" alt="">
+        </div>
+        <div class="containerTextMaterialsBox">
+          <h3 class="material-for-select"><?php echo $titles[$i]; ?></h3>
+        </div>
+        <div class="buttonMaterialsBox">
+          <button class="openCustomizeLanyardFromMaterials" type="button">Select</button>
+        </div>
       </div>
-      <div class="containerTextMaterialsBox">
-        <img src="../assets/img/home/3-materials/Material-2-Tabular.jpg" alt="">
+    <?php endfor; ?>
+
+    <?php
+      $extraImages = [
+        "FlatPolyester.png",
+        "2.png",
+        "3.png",
+        "4.png",
+        "6.png"
+      ];
+
+      foreach ($extraImages as $img):
+    ?>
+      <div class="itemMaterial">
+        <img src="../assets/img/home/3-materials/<?php echo $img; ?>?v=<?php echo filemtime("../assets/img/home/3-materials/{$img}"); ?>" alt="">
       </div>
-      <div class="containerTextMaterialsBox">
-        <h3 class="material-for-select">Tubular</h3>
-      </div>
-      <div class="buttonMaterialsBox">
-        <button  class="openCustomizeLanyardFromMaterials" type="button" name="button">Select</button>
-      </div>
-    </div>
-    <div class="containerTextMaterials">
-      <div class="closeMaterial">
-        <img src="../assets/img/home/3-materials/Close-Materials.png" alt="">
-      </div>
-      <div class="containerTextMaterialsBox">
-        <img src="../assets/img/home/3-materials/Material-3-Natural-Bamboo.jpg" alt="">
-      </div>
-      <div class="containerTextMaterialsBox">
-        <h3 class="material-for-select">RPET Polyester</h3>
-      </div>
-      <div class="buttonMaterialsBox">
-        <button  class="openCustomizeLanyardFromMaterials" type="button" name="button">Select</button>
-      </div>
-    </div>
-    <div class="containerTextMaterials">
-      <div class="closeMaterial">
-        <img src="../assets/img/home/3-materials/Close-Materials.png" alt="">
-      </div>
-      <div class="containerTextMaterialsBox">
-        <img src="../assets/img/home/3-materials/Material-4-Dye-sublimation.jpg" alt="">
-      </div>
-      <div class="containerTextMaterialsBox">
-        <h3 class="material-for-select">Dye Sub polyester</h3>
-      </div>
-      <div class="buttonMaterialsBox">
-        <button  class="openCustomizeLanyardFromMaterials" type="button" name="button">Select</button>
-      </div>
-    </div>
-    <div class="containerTextMaterials">
-      <div class="closeMaterial">
-        <img src="../assets/img/home/3-materials/Close-Materials.png" alt="">
-      </div>
-      <div class="containerTextMaterialsBox">
-        <img src="../assets/img/home/3-materials/Material-5-Dye-sub-Recycled-PET.jpg" alt="">
-      </div>
-      <div class="containerTextMaterialsBox">
-        <h3 class="material-for-select">Dye Sub RPET</h3>
-      </div>
-      <div class="buttonMaterialsBox">
-        <button  class="openCustomizeLanyardFromMaterials" type="button" name="button">Select</button>
-      </div>
-    </div>
-    <div class="itemMaterial">
-      <img src="../assets/img/home/3-materials/FlatPolyester.png" alt="">
-    </div>
-    <div class="itemMaterial">
-      <img src="../assets/img/home/3-materials/2.png" alt="">
-    </div>
-    <div class="itemMaterial">
-      <img src="../assets/img/home/3-materials/3.png" alt="">
-    </div>
-    <div class="itemMaterial">
-      <img src="../assets/img/home/3-materials/4.png" alt="">
-    </div>
-    <div class="itemMaterial">
-      <img src="../assets/img/home/3-materials/6.png" alt="">
-    </div>
+    <?php endforeach; ?>
   </div>
 </section>
+
 <script src="../assets/js/home/sectons/3-materials.js?v=<?php echo filemtime('../assets/js/home/sectons/3-materials.js'); ?>"></script>
