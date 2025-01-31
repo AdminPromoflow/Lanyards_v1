98b7717c5ee186eeb72ea454c3a4478a4c9ad2a9
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-01-30 23:10:39 -0500

    “...”

--- views/assets/js/customize-lanyard/app.js
+++ views/assets/js/customize-lanyard/app.js
@@ -16,7 +16,6 @@ class CustomizeLanyard {
   };
   // Make an AJAX request to fetch all materials.
   material.makeAjaxRequestGetAllMaterials(url, data);
-  alert("que raro");
   chargingClass.hideShowchargin(false);
 
   }
