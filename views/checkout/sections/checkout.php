da36ba5a78b0e161f1e3ea63fb5a18deb1576177
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-02-02 11:59:51 -0500

    “...”

--- views/checkout/sections/checkout.php
+++ views/checkout/sections/checkout.php
@@ -1,71 +1,72 @@
-<link rel="stylesheet" href="../../views/assets/css/shopping_cart/sections/shopping_cart.css">
-<section class="section_shopping_cart">
+<!-- Link to the checkout section stylesheet with a timestamp to force cache refresh -->
+<link rel="stylesheet" href="<?php echo '../../views/assets/css/checkout/sections/checkout.css?v=' . (file_exists($_SERVER['DOCUMENT_ROOT'] . '/views/assets/css/checkout/sections/checkout.css') ? filemtime($_SERVER['DOCUMENT_ROOT'] . '/views/assets/css/checkout/sections/checkout.css') : time()); ?>">
+<section class="section_checkout">
   <h1>Checkout</h1>
   <p>Hello, these are the products that you have added to your shopping cart.</p>
-  <div class="container_shopping_cart">
+  <div class="container_checkout">
 
-    <div class="boxes_shopping_cart">
-      <div class="items_shopping_cart">
-        <div class="product_items_shopping_cart">
+    <div class="boxes_checkout">
+      <div class="items_checkout">
+        <div class="product_items_checkout">
             <h3>hola2</h3>
             <h3>hola3</h3>
-            <img class="arrow_products_shopping_cart" src="../../views/assets/img/shopping_cart/sections/arrow_right.png" alt="">
+            <img class="arrow_products_checkout" src="../../views/assets/img/checkout/sections/arrow_right.png" alt="">
         </div>
-        <div class="descriptions_items_shopping_cart">
-          <div class="elements_descriptions_items_shopping_cart">
+        <div class="descriptions_items_checkout">
+          <div class="elements_descriptions_items_checkout">
             <h3>Material</h3>
             <h3>hola2</h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_descriptions_items_shopping_cart">
+          <div class="elements_descriptions_items_checkout">
             <h3>Lanyard type</h3>
             <h3>hola2</h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_descriptions_items_shopping_cart">
+          <div class="elements_descriptions_items_checkout">
             <h3>Width</h3>
             <h3>hola2</h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_descriptions_items_shopping_cart">
+          <div class="elements_descriptions_items_checkout">
             <h3>Side printed</h3>
             <h3>hola2</h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_descriptions_items_shopping_cart">
+          <div class="elements_descriptions_items_checkout">
             <h3>Clips</h3>
             <h3>hola2</h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_descriptions_items_shopping_cart">
+          <div class="elements_descriptions_items_checkout">
             <h3>Attachment</h3>
             <h3>hola2</h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_descriptions_items_shopping_cart">
+          <div class="elements_descriptions_items_checkout">
             <h3>Accessories</h3>
             <h3>hola2</h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_descriptions_items_shopping_cart">
+          <div class="elements_descriptions_items_checkout">
             <h3>Colour Quantity</h3>
             <h3>hola2</h3>
             <h3>hola3</h3>
           </div>
 
         </div>
-        <div class="summary_items_shopping_cart">
-          <div class="elements_summary_items_shopping_cart">
+        <div class="summary_items_checkout">
+          <div class="elements_summary_items_checkout">
             <h3>Cost per unit</h3>
             <h3></h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_summary_items_shopping_cart">
+          <div class="elements_summary_items_checkout">
             <h3>Amount</h3>
             <h3></h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_summary_items_shopping_cart">
+          <div class="elements_summary_items_checkout">
             <h3>Subtotal</h3>
             <h3></h3>
             <h3>hola3</h3>
@@ -73,67 +74,66 @@
 
         </div>
       </div>
-      <div class="items_shopping_cart">
-        <div class="product_items_shopping_cart">
+      <div class="items_checkout">
+        <div class="product_items_checkout">
             <h3>hola2</h3>
             <h3>hola3</h3>
-            <img class="arrow_products_shopping_cart" src="../../views/assets/img/shopping_cart/sections/arrow_right.png" alt="">
-        </div>
-        <div class="descriptions_items_shopping_cart">
-          <div class="elements_descriptions_items_shopping_cart">
+            <img class="arrow_products_checkout" src="<?php echo '../../views/assets/img/checkout/sections/arrow_right.png?v=' . (file_exists($_SERVER['DOCUMENT_ROOT'] . '/views/assets/img/checkout/sections/arrow_right.png') ? filemtime($_SERVER['DOCUMENT_ROOT'] . '/views/assets/img/checkout/sections/arrow_right.png') : time()); ?>" alt="">        </div>
+        <div class="descriptions_items_checkout">
+          <div class="elements_descriptions_items_checkout">
             <h3>Material</h3>
             <h3>hola2</h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_descriptions_items_shopping_cart">
+          <div class="elements_descriptions_items_checkout">
             <h3>Lanyard type</h3>
             <h3>hola2</h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_descriptions_items_shopping_cart">
+          <div class="elements_descriptions_items_checkout">
             <h3>Width</h3>
             <h3>hola2</h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_descriptions_items_shopping_cart">
+          <div class="elements_descriptions_items_checkout">
             <h3>Side printed</h3>
             <h3>hola2</h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_descriptions_items_shopping_cart">
+          <div class="elements_descriptions_items_checkout">
             <h3>Clips</h3>
             <h3>hola2</h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_descriptions_items_shopping_cart">
+          <div class="elements_descriptions_items_checkout">
             <h3>Attachment</h3>
             <h3>hola2</h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_descriptions_items_shopping_cart">
+          <div class="elements_descriptions_items_checkout">
             <h3>Accessories</h3>
             <h3>hola2</h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_descriptions_items_shopping_cart">
+          <div class="elements_descriptions_items_checkout">
             <h3>Colour Quantity</h3>
             <h3>hola2</h3>
             <h3>hola3</h3>
           </div>
 
         </div>
-        <div class="summary_items_shopping_cart">
-          <div class="elements_summary_items_shopping_cart">
+        <div class="summary_items_checkout">
+          <div class="elements_summary_items_checkout">
             <h3>Cost per unit</h3>
             <h3></h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_summary_items_shopping_cart">
+          <div class="elements_summary_items_checkout">
             <h3>Amount</h3>
             <h3></h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_summary_items_shopping_cart">
+          <div class="elements_summary_items_checkout">
             <h3>Subtotal</h3>
             <h3></h3>
             <h3>hola3</h3>
@@ -141,67 +141,66 @@
 
         </div>
       </div>
-      <div class="items_shopping_cart">
-        <div class="product_items_shopping_cart">
+      <div class="items_checkout">
+        <div class="product_items_checkout">
             <h3>hola2</h3>
             <h3>hola3</h3>
-            <img class="arrow_products_shopping_cart" src="../../views/assets/img/shopping_cart/sections/arrow_right.png" alt="">
-        </div>
-        <div class="descriptions_items_shopping_cart">
-          <div class="elements_descriptions_items_shopping_cart">
+            <img class="arrow_products_checkout" src="<?php echo '../../views/assets/img/checkout/sections/arrow_right.png?v=' . (file_exists($_SERVER['DOCUMENT_ROOT'] . '/views/assets/img/checkout/sections/arrow_right.png') ? filemtime($_SERVER['DOCUMENT_ROOT'] . '/views/assets/img/checkout/sections/arrow_right.png') : time()); ?>" alt="">        </div>
+        <div class="descriptions_items_checkout">
+          <div class="elements_descriptions_items_checkout">
             <h3>Material</h3>
             <h3>hola2</h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_descriptions_items_shopping_cart">
+          <div class="elements_descriptions_items_checkout">
             <h3>Lanyard type</h3>
             <h3>hola2</h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_descriptions_items_shopping_cart">
+          <div class="elements_descriptions_items_checkout">
             <h3>Width</h3>
             <h3>hola2</h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_descriptions_items_shopping_cart">
+          <div class="elements_descriptions_items_checkout">
             <h3>Side printed</h3>
             <h3>hola2</h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_descriptions_items_shopping_cart">
+          <div class="elements_descriptions_items_checkout">
             <h3>Clips</h3>
             <h3>hola2</h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_descriptions_items_shopping_cart">
+          <div class="elements_descriptions_items_checkout">
             <h3>Attachment</h3>
             <h3>hola2</h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_descriptions_items_shopping_cart">
+          <div class="elements_descriptions_items_checkout">
             <h3>Accessories</h3>
             <h3>hola2</h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_descriptions_items_shopping_cart">
+          <div class="elements_descriptions_items_checkout">
             <h3>Colour Quantity</h3>
             <h3>hola2</h3>
             <h3>hola3</h3>
           </div>
 
         </div>
-        <div class="summary_items_shopping_cart">
-          <div class="elements_summary_items_shopping_cart">
+        <div class="summary_items_checkout">
+          <div class="elements_summary_items_checkout">
             <h3>Cost per unit</h3>
             <h3></h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_summary_items_shopping_cart">
+          <div class="elements_summary_items_checkout">
             <h3>Amount</h3>
             <h3></h3>
             <h3>hola3</h3>
           </div>
-          <div class="elements_summary_items_shopping_cart">
+          <div class="elements_summary_items_checkout">
             <h3>Subtotal</h3>
             <h3></h3>
             <h3>hola3</h3>
@@ -210,43 +209,43 @@
         </div>
       </div>
     </div>
-    <div class="boxes_shopping_cart">
-      <h3 class="title_boxes_shopping_cart">You Orden</h3>
-      <div class="boxes_container_shopping_cart">
-        <div class="items2_shopping_cart">
+    <div class="boxes_checkout">
+      <h3 class="title_boxes_checkout">You Orden</h3>
+      <div class="boxes_container_checkout">
+        <div class="items2_checkout">
           <h3>Product</h3>
           <h3>Subtotal</h3>
         </div>
 
-        <div class="elements_boxes_container_shopping_cart">
+        <div class="elements_boxes_container_checkout">
           <h3>Material</h3>
           <h3>hola2</h3>
         </div>
-        <div class="elements_boxes_container_shopping_cart">
+        <div class="elements_boxes_container_checkout">
           <h3>Material</h3>
           <h3>hola2</h3>
         </div>
-        <div class="elements_boxes_container_shopping_cart">
+        <div class="elements_boxes_container_checkout">
           <h3>Material</h3>
           <h3>hola2</h3>
         </div>
       </div>
-      <div class="boxes2_container_shopping_cart">
+      <div class="boxes2_container_checkout">
 
-        <div class="elements_boxes2_container_shopping_cart">
+        <div class="elements_boxes2_container_checkout">
           <h3>Material</h3>
           <h3>hola2</h3>
         </div>
-        <div class="elements_boxes2_container_shopping_cart">
+        <div class="elements_boxes2_container_checkout">
           <h3>Material</h3>
           <h3>hola2</h3>
         </div>
-        <div class="elements_boxes2_container_shopping_cart">
+        <div class="elements_boxes2_container_checkout">
           <h3>Material</h3>
           <h3>hola2</h3>
         </div>
       </div>
-      <div class="boxes3_container_shopping_cart">
+      <div class="boxes3_container_checkout">
         <label>
             <input type="checkbox" name="option3" value="Option3">
             By clicking 'Place Order,' I confirm that I have read and agree to the website's terms and conditions.
@@ -258,6 +257,6 @@
   </div>
 
 </section>
-<script src="../../views/assets/js/shopping_cart/sections/shopping_cart.js" type="text/javascript">
-
-</script>
+<!-- Link to the checkout section stylesheet with a timestamp to force cache refresh -->
+<!-- JavaScript file with timestamp to ensure the latest version is always loaded -->
+<script src="<?php echo '../../views/assets/js/checkout/sections/checkout.js?v=' . (file_exists($_SERVER['DOCUMENT_ROOT'] . '/views/assets/js/checkout/sections/checkout.js') ? filemtime($_SERVER['DOCUMENT_ROOT'] . '/views/assets/js/checkout/sections/checkout.js') : time()); ?>" type="text/javascript"></script>
