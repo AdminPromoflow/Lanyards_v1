6923d99395aa7bd35a15ab93086bc8c698302b31
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-02-05 21:18:10 +0000

    ....

--- views/assets/js/home/sections/contact-us.js
+++ views/assets/js/home/sections/contact-us.js
@@ -1,10 +1,14 @@
 class ContactUsHome {
   constructor() {
     button_contact_us_from_home.addEventListener('click', function(){
-      alert('bueno');
+
     });
   }
 }
+const nameContactUsHome = document.getElementById('nameContactUsHome');
+const emailContactUsHome = document.getElementById('emailContactUsHome');
+const phoneContactUsHome = document.getElementById('phoneContactUsHome');
+const messageContactUsHome = document.getElementById('messageContactUsHome');
 
 const button_contact_us_from_home = document.getElementById('button_contact_us_from_home');
 const contactUsHome = new ContactUsHome();
