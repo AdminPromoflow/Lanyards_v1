1416deae739dfd889241107598bf40343363e691
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-02-05 04:01:45 +0000

    Update contact-us.php

--- views/home/sections/contact-us.php
+++ views/home/sections/contact-us.php
@@ -12,14 +12,14 @@
       <button class="button_contact_us" id="button_contact_us_from_home" type="button" name="button">Submit</button>
     </div>
     <div class="box_contact_us">
-      <div id="map3"></div>
-    </div> 
+    <!--  <div id="map3"></div>-->
+    </div>
   </div>
 
 </section>
 
 <script type="text/javascript">
-  var map = L.map('map3').setView([50.859644, -1.320230], 10);
+/*  var map = L.map('map3').setView([50.859644, -1.320230], 10);
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -30,7 +30,7 @@
 }).addTo(map);
 
 
-var marker = L.marker([50.859644, -1.320230]).addTo(map);
+var marker = L.marker([50.859644, -1.320230]).addTo(map);*/
 </script>
 
 <script src="../../views/assets/js/home/sectons/contact-us.js?v=<?= filemtime(__DIR__ . '/../../views/assets/js/home/sectons/contact-us.js'); ?>" type="text/javascript"></script>
