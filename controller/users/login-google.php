866eae4e6d2707ffa92b4dfdf95f908cc9caaa0d
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-02-05 02:48:03 +0000

    Update login-google.php

--- controller/users/login-google.php
+++ controller/users/login-google.php
@@ -58,8 +58,7 @@ class ApiHandlerLoginGoogle
 
 
     private function validateGoogleLogin() {
-      echo json_encode(array("google_login" => false));
-      exit
+
         // Configuración inicial de Google OAuth
         $clientID = '1022332881668-587bktseqso57k6m2dmpfao53vasg83b.apps.googleusercontent.com';
         $clientSecret = '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';
