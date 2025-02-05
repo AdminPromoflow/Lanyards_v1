b4c9de0c1721822dcd248be25c6dc14126285e91
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-02-05 20:57:07 +0000

    ..

--- views/contact_us/sections/contact-us.php
+++ views/contact_us/sections/contact-us.php
@@ -1,19 +1,26 @@
-<link rel="stylesheet" href="../../views/assets/css/contac_us/sections/contact-us.css">
+<?php
+// Validar el archivo CSS antes de usarlo
+$cssPath = '../../views/assets/css/contact_us/sections/contact-us.css';
+$cssVersion = file_exists($cssPath) ? filemtime($cssPath) : time();
+?>
+
+<!-- CSS -->
+<link rel="stylesheet" href="<?= $cssPath ?>?v=<?= $cssVersion; ?>">
 
 <section class="section_contact_us">
-  <h1 >Contact us</h1>
+  <h1>Contact us</h1>
   <div class="container_contact_us">
     <div class="box_contact_us">
-      <input type="text" name="" placeholder="Name" value="">
-      <input type="text" name="" placeholder="Email address" value="">
-      <input type="text" name="" placeholder="Contact number" value="">
-      <textarea name="name" placeholder="Tell us how we can assist you..." rows="3" ></textarea>
+      <input type="text" placeholder="Name">
+      <input type="text" placeholder="Email address">
+      <input type="text" placeholder="Contact number">
+      <textarea placeholder="Tell us how we can assist you..." rows="3"></textarea>
     </div>
     <div class="box_contact_us">
       <div id="map2"></div>
     </div>
   </div>
-  <button class="button_contact_us" id="button_contact_us" type="button" name="button">Submit</button>
+  <button class="button_contact_us" id="button_contact_us" type="button">Submit</button>
 </section>
 
 <script type="text/javascript">
@@ -25,11 +32,16 @@
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'your.mapbox.access.token'
-}).addTo(map);
+  }).addTo(map);
 
-
-var marker = L.marker([50.859644, -1.320230]).addTo(map);
+  var marker = L.marker([50.859644, -1.320230]).addTo(map);
 </script>
-<script src="../../views/assets/js/contact_us/sections/contact_us.js" type="text/javascript">
 
-</script>
+<?php
+// Validar el archivo JS antes de usarlo
+$jsPath = '../../views/assets/js/contact_us/sections/contact_us.js';
+$jsVersion = file_exists($jsPath) ? filemtime($jsPath) : time();
+?>
+
+<!-- JavaScript -->
+<script src="<?= $jsPath ?>?v=<?= $jsVersion; ?>" type="text/javascript"></script>
