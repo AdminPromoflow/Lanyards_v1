0de868934de4f8b6cf3444b12ef6ba3f92eff19d
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-02-08 19:01:16 +0000

    Update accessories.php

--- views/global/customize-lanyard/sections/accessories.php
+++ views/global/customize-lanyard/sections/accessories.php
@@ -1,143 +1,141 @@
-<link rel="stylesheet" href="../../views/assets/css/global/customize-lanyard/sections/accessories.css">
-<section class="accessoriesSection  section ">
-  <h2 class="name-section-customize-lanyard">Accessories</h2>
-  <br>
-  <p>Select one accessories</p>
-  <br>
+<?php
+function asset_version($filePath) {
+    $realPath = realpath(__DIR__ . $filePath);
+    return ($realPath && file_exists($realPath)) ? filemtime($realPath) : time();
+}
 
-  <div id="containers_boxes_accessories" class="containers_boxes_accessories">
+$basePath = "../../views/assets/";
+$cssFile_accessories = "{$basePath}css/global/customize-lanyard/sections/accessories.css";
+$jsFile_accessories = "{$basePath}js/customize-lanyard/sections/accessories.js";
 
-    <div class="container_boxes_accessories">
-      <h3 class="dataAccessories">None</h3>
-      <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/none.png" alt="">
-      <div class="arrow_accessories">
-        <img class="hideArrowAccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/arrow_top.png" alt="">
+$imgFiles = [
+    "none" => "{$basePath}img/global/customize-lanyard/sections/accessories/none.png",
+    "arrow" => "{$basePath}img/global/customize-lanyard/sections/accessories/arrow_top.png",
+    "6" => "{$basePath}img/global/customize-lanyard/sections/accessories/6.png",
+    "2" => "{$basePath}img/global/customize-lanyard/sections/accessories/2.png",
+    "1" => "{$basePath}img/global/customize-lanyard/sections/accessories/1.png",
+    "7" => "{$basePath}img/global/customize-lanyard/sections/accessories/7.png",
+    "12" => "{$basePath}img/global/customize-lanyard/sections/accessories/12.png",
+    "9" => "{$basePath}img/global/customize-lanyard/sections/accessories/9.png",
+    "11" => "{$basePath}img/global/customize-lanyard/sections/accessories/11.png",
+];
 
-      </div>
+function get_image($key) {
+    global $imgFiles;
+    return $imgFiles[$key] . "?v=" . asset_version($imgFiles[$key]);
+}
+?>
+
+<link rel="stylesheet" href="<?= $cssFile_accessories ?>?v=<?= asset_version($cssFile_accessories) ?>">
+
+<section class="accessoriesSection section">
+    <h2 class="name-section-customize-lanyard">Accessories 3</h2>
+    <p>Select one accessories</p>
+
+    <div id="containers_boxes_accessories" class="containers_boxes_accessories">
+
+        <div class="container_boxes_accessories">
+            <h3 class="dataAccessories">None</h3>
+            <img class="imgaccessories" src="<?= get_image('none') ?>" alt="">
+            <div class="arrow_accessories">
+                <img class="hideArrowAccessories" src="<?= get_image('arrow') ?>" alt="">
+            </div>
+        </div>
+
+        <div class="container_boxes_accessories">
+            <h3 class="dataAccessories">Rigid Card Holder</h3>
+            <div class="arrow_accessories">
+                <img src="<?= get_image('arrow') ?>" alt="">
+            </div>
+        </div>
+
+        <div class="subcontainers_boxes_accessories">
+            <?php
+            $accessories = [
+                ["Clear Plastic (closed face)", "6", "£0.3"],
+                ["Coloured Plastic (closed face)", "2", "£0.3"],
+                ["Coloured Plastic (open face)", "1", "£0.25"]
+            ];
+            foreach ($accessories as [$name, $img, $price]) : ?>
+                <div class="subcontainer_boxes_accessories">
+                    <h3 class="dataAccessories"><?= $name ?></h3>
+                    <img class="imgaccessories" src="<?= get_image($img) ?>" alt="">
+                    <h4 class="priceDataAccessories">+<?= $price ?> per unit</h4>
+                </div>
+            <?php endforeach; ?>
+        </div>
+
+        <div class="container_boxes_accessories">
+            <h3 class="dataAccessories">Plastic Wallet</h3>
+            <div class="arrow_accessories">
+                <img src="<?= get_image('arrow') ?>" alt="">
+            </div>
+        </div>
+
+        <div class="subcontainers_boxes_accessories">
+            <?php
+            $walletSizes = [
+                ["86 x 56mm", "7", "£0.12"],
+                ["103 x 70mm", "7", "£0.13"],
+                ["138 x 88mm", "7", "£0.14"]
+            ];
+            foreach ($walletSizes as [$size, $img, $price]) : ?>
+                <div class="subcontainer_boxes_accessories">
+                    <h3 class="dataAccessories"><?= $size ?></h3>
+                    <img class="imgaccessories" src="<?= get_image($img) ?>" alt="">
+                    <h4 class="priceDataAccessories">+<?= $price ?> per unit</h4>
+                </div>
+            <?php endforeach; ?>
+        </div>
+
+        <div class="container_boxes_accessories">
+            <h3 class="dataAccessories">Retractable Reel</h3>
+            <div class="arrow_accessories">
+                <img src="<?= get_image('arrow') ?>" alt="">
+            </div>
+        </div>
+
+        <div class="subcontainers_boxes_accessories">
+            <?php
+            $reels = [
+                ["Plastic", "12", "£0.5"],
+                ["Round", "12", "£0.85"],
+                ["Oval", "12", "£0.85"],
+                ["Metal", "12", "£1.10"],
+                ["Printing (per screen)", "12", "£25.0"]
+            ];
+            foreach ($reels as [$name, $img, $price]) : ?>
+                <div class="subcontainer_boxes_accessories">
+                    <h3 class="dataAccessories"><?= $name ?></h3>
+                    <img class="imgaccessories" src="<?= get_image($img) ?>" alt="">
+                    <h4 class="priceDataAccessories">+<?= $price ?> per unit</h4>
+                </div>
+            <?php endforeach; ?>
+        </div>
+
+        <div class="container_boxes_accessories">
+            <h3 class="dataAccessories">Other</h3>
+            <div class="arrow_accessories">
+                <img src="<?= get_image('arrow') ?>" alt="">
+            </div>
+        </div>
+
+        <div class="subcontainers_boxes_accessories">
+            <?php
+            $others = [
+                ["Cord Lock", "9", "£0.08"],
+                ["Whistle", "11", "£0.1"]
+            ];
+            foreach ($others as [$name, $img, $price]) : ?>
+                <div class="subcontainer_boxes_accessories">
+                    <h3 class="dataAccessories"><?= $name ?></h3>
+                    <img class="imgaccessories" src="<?= get_image($img) ?>" alt="">
+                    <h4 class="priceDataAccessories">+<?= $price ?> per unit</h4>
+                </div>
+            <?php endforeach; ?>
+        </div>
+
     </div>
-
-
-    <div class="subcontainers_boxes_accessories">
-    </div>
-
-    <div class="container_boxes_accessories">
-      <h3 class="dataAccessories">Rigid Card Holder</h3>
-      <div class="arrow_accessories">
-        <img class="" src="../../views/assets/img/global/customize-lanyard/sections/accessories/arrow_top.png" alt="">
-
-      </div>
-    </div>
-
-    <div class="subcontainers_boxes_accessories">
-      <div class="subcontainer_boxes_accessories">
-        <h3 class="dataAccessories">Clear Plastic (closed face) </h3>
-        <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/6.png" alt="">
-        <h4 class="priceDataAccessories">+£0.3 per unit</h4>
-      </div>
-      <div class="subcontainer_boxes_accessories">
-        <h3 class="dataAccessories">Coloured Plastic (closed face)</h3>
-        <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/2.png" alt="">
-        <h4 class="priceDataAccessories">+£0.3 per unit</h4>
-      </div>
-      <div class="subcontainer_boxes_accessories">
-        <h3 class="dataAccessories">Coloured Plastic (open face)</h3>
-        <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/1.png" alt="">
-        <h4 class="priceDataAccessories">+£0.25 per unit</h4>
-      </div>
-    </div>
-
-    <div class="container_boxes_accessories">
-      <h3 class="dataAccessories">Plastic Wallet</h3>
-      <div class="arrow_accessories">
-        <img class="" src="../../views/assets/img/global/customize-lanyard/sections/accessories/arrow_top.png" alt="">
-
-      </div>
-    </div>
-
-    <div class="subcontainers_boxes_accessories">
-
-      <div class="subcontainer_boxes_accessories plastic_wallet_small">
-        <h3 class="dataAccessories">86 x 56mm </h3>
-        <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/7.png" alt="">
-        <h4 class="priceDataAccessories">+£0.12 per unit</h4>
-      </div>
-
-      <div class="subcontainer_boxes_accessories plastic_wallet_medium">
-        <h3 class="dataAccessories">103 x 70mm</h3>
-        <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/7.png" alt="">
-        <h4 class="priceDataAccessories">+£0.13 per unit</h4>
-      </div>
-
-      <div class="subcontainer_boxes_accessories plastic_wallet_big">
-        <h3 class="dataAccessories">138 x 88mm</h3>
-        <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/7.png" alt="">
-        <h4 class="priceDataAccessories">+£0.14 per unit</h4>
-      </div>
-
-    </div>
-    <div class="container_boxes_accessories">
-      <h3 class="dataAccessories">Retractable Reel</h3>
-      <div class="arrow_accessories">
-        <img class="" src="../../views/assets/img/global/customize-lanyard/sections/accessories/arrow_top.png" alt="">
-
-      </div>
-    </div>
-
-    <div class="subcontainers_boxes_accessories">
-      <div class="subcontainer_boxes_accessories">
-        <h3 class="dataAccessories">Plastic</h3>
-        <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/12.png" alt="">
-        <h4 class="priceDataAccessories">+£0.5 per unit</h4>
-      </div>
-      <div class="subcontainer_boxes_accessories">
-        <h3 class="dataAccessories">Round</h3>
-        <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/12.png" alt="">
-        <h4 class="priceDataAccessories">+£0.85 per unit</h4>
-      </div>
-      <div class="subcontainer_boxes_accessories">
-        <h3 class="dataAccessories">Oval</h3>
-        <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/12.png" alt="">
-        <h4 class="priceDataAccessories">+£0.85 per unit</h4>
-      </div>
-      <div class="subcontainer_boxes_accessories">
-        <h3 class="dataAccessories">Metal</h3>
-        <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/12.png" alt="">
-        <h4 class="priceDataAccessories">+£1.10 per unit</h4>
-      </div>
-      <div class="subcontainer_boxes_accessories">
-        <h3 class="dataAccessories">Printing (per screen)</h3>
-        <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/12.png" alt="">
-        <h4 class="priceDataAccessories">+£25.0 per unit</h4>
-      </div>
-    </div>
-
-    <div class="container_boxes_accessories">
-      <h3 class="dataAccessories">Other</h3>
-      <div class="arrow_accessories">
-        <img class="" src="../../views/assets/img/global/customize-lanyard/sections/accessories/arrow_top.png" alt="">
-
-      </div>
-    </div>
-
-    <div class="subcontainers_boxes_accessories">
-      <div class="subcontainer_boxes_accessories">
-        <h3 class="dataAccessories">Cord Lock</h3>
-        <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/9.png" alt="">
-        <h4 class="priceDataAccessories">+£0.08 per unit</h4>
-      </div>
-
-      <div class="subcontainer_boxes_accessories">
-        <h3 class="dataAccessories">Whistle</h3>
-        <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/11.png" alt="">
-        <h4 class="priceDataAccessories">+£0.1 per unit</h4>
-      </div>
-
-
-    </div>
-
-  </div>
-
 </section>
 
-
-<script src="../../views/assets/js/customize-lanyard/sections/accessories.js"></script>
+<script src="<?= $jsFile_accessories ?>?v=<?= asset_version($jsFile_accessories) ?>" type="text/javascript"></script>
