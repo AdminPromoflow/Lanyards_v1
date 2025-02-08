37d7c7a55b491db1082bbef6da1c29f39e422981
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-02-08 19:34:25 +0000

    Update material.php

--- views/global/customize-lanyard/sections/material.php
+++ views/global/customize-lanyard/sections/material.php
@@ -1,27 +1,37 @@
-<link rel="stylesheet" href="../../views/assets/css/global/customize-lanyard/sections/material.css">
+<?php
+function asset_version_material($filePath) {
+    $realPath = realpath(__DIR__ . $filePath);
+    return ($realPath && file_exists($realPath)) ? filemtime($realPath) : time();
+}
+
+$cssFile_material = "../../views/assets/css/global/customize-lanyard/sections/material.css";
+$jsFile_material = "../../views/assets/js/customize-lanyard/sections/material.js";
+?>
+
+<link rel="stylesheet" href="<?= $cssFile_material ?>?v=<?= asset_version_material($cssFile_material) ?>">
+
 <section class="material section active">
-  <h2 class="name-section-customize-lanyard">Material</h2>
-  <br>
-  <p>Select the type of lanyard</p>
-  <br>
-  <div id="containers_boxes_material" class="containers_boxes_material">
-    <!--<div class="container_boxes_material">
-      <h4>Tubular</h4>
+    <h2 class="name-section-customize-lanyard">Material</h2>
+    <br>
+    <p>Select the type of lanyard</p>
+    <br>
+    <div id="containers_boxes_material" class="containers_boxes_material">
+        <!--<div class="container_boxes_material">
+            <h4>Tubular</h4>
+        </div>
+        <div class="container_boxes_material">
+            <h4>Dye Sub polyester</h4>
+        </div>
+        <div class="container_boxes_material">
+            <h4>Ribbed Polyester</h4>
+        </div>
+        <div class="container_boxes_material">
+            <h4>Dye Sub RPET</h4>
+        </div>
+        <div class="container_boxes_material">
+            <h4>Bamboo</h4>
+        </div>-->
     </div>
-    <div class="container_boxes_material">
-      <h4>Dye Sub polyester</h4>
-    </div>
-    <div class="container_boxes_material">
-      <h4>Ribbed Polyester</h4>
-    </div>
-    <div class="container_boxes_material">
-      <h4>Dye Sub RPET</h4>
-    </div>
-    <div class="container_boxes_material">
-      <h4>Bamboo</h4>
-    </div>-->
-  </div>
 </section>
 
-
-<script src="../../views/assets/js/customize-lanyard/sections/material.js"></script>
+<script src="<?= $jsFile_material ?>?v=<?= asset_version_material($jsFile_material) ?>" type="text/javascript"></script>
