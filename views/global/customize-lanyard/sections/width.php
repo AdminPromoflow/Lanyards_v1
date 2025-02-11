d09b97c092b8e0c83cf0c475c4c8890045d368c6
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-02-11 22:30:54 +0000

    Update width.php

--- views/global/customize-lanyard/sections/width.php
+++ views/global/customize-lanyard/sections/width.php
@@ -1,33 +1,22 @@
-<link rel="stylesheet" href="../../views/assets/css/global/customize-lanyard/sections/width.css">
+<?php
+// Corregir rutas absolutas para evitar problemas con __DIR__
+$cssFileWidth = realpath(__DIR__ . '/../../views/assets/css/global/customize-lanyard/sections/width.css');
+$jsFileWidth = realpath(__DIR__ . '/../../views/assets/js/customize-lanyard/sections/width.js');
+
+$cssVersionWidth = $cssFileWidth && file_exists($cssFileWidth) ? filemtime($cssFileWidth) : time();
+$jsVersionWidth = $jsFileWidth && file_exists($jsFileWidth) ? filemtime($jsFileWidth) : time();
+?>
+
+<!-- CSS con filemtime() -->
+<link rel="stylesheet" href="../../views/assets/css/global/customize-lanyard/sections/width.css?v=<?= $cssVersionWidth; ?>">
 
 <section class="width section">
   <h2 class="name-section-customize-lanyard">Width</h2>
   <br>
   <p>Select the width</p>
   <br>
-  <div id="containers_boxes_width" class="containers_boxes_width">
-<!-- <div class="container_boxes_width">
-      <h4 class="dataWidth">10mm</h4>
-      <img src="../../views/assets/img/global/customize-lanyard/sections/width/one-end/one-end-10mm.png" alt="">
-    </div>
-    <div class="container_boxes_width">
-      <h4 class="dataWidth">15mm</h4>
-      <img src="../../views/assets/img/global/customize-lanyard/sections/width/one-end/one-end-15mm.png" alt="">
-    </div>
-    <div class="container_boxes_width">
-      <h4 class="dataWidth">20mm</h4>
-      <img src="../../views/assets/img/global/customize-lanyard/sections/width/one-end/one-end-20mm.png" alt="">
-    </div>
-    <div class="container_boxes_width">
-      <h4 class="dataWidth">25mm</h4>
-      <img src="../../views/assets/img/global/customize-lanyard/sections/width/one-end/one-end-25mm.png" alt="">
-    </div>
-    <div class="container_boxes_width">
-      <h4 class="dataWidth">30mm</h4>
-      <img src="../../views/assets/img/global/customize-lanyard/sections/width/one-end/one-end-30mm.png" alt="">
-    </div>-->
-  </div>
+  <div id="containers_boxes_width" class="containers_boxes_width"></div>
 </section>
 
-
-<script src="../../views/assets/js/customize-lanyard/sections/width.js"></script>
+<!-- JavaScript con filemtime() -->
+<script src="../../views/assets/js/customize-lanyard/sections/width.js?v=<?= $jsVersionWidth; ?>"></script>
