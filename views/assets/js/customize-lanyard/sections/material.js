f8c77902b4511a7f3018e8f5b8e42c3b1e916a0e
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-02-11 03:35:52 +0000

    Update material.js

--- views/assets/js/customize-lanyard/sections/material.js
+++ views/assets/js/customize-lanyard/sections/material.js
@@ -186,7 +186,7 @@ class Material {
     // Get all elements with the class "pricesDataMaterial".
     const pricesDataMaterial = document.querySelectorAll(".pricesDataMaterial");
 
-    alert('Esto deberia crearse'+ pricesDataMaterial.length +' veces no mas');
+    alert('Esto deberia crearse'+ priceDataMaterialResult.length +' veces no mas');
 
     // Iterating through the priceDataMaterialResult array to update the HTML.
     for (var i = 0; i < priceDataMaterialResult.length; i++) {
