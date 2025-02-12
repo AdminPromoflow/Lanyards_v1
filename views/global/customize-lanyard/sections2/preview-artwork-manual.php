a0d381112af14453879abc8390cff1b26ba68777
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-02-12 22:02:04 +0000

    ...

--- views/global/customize-lanyard/sections2/preview-artwork-manual.php
+++ views/global/customize-lanyard/sections2/preview-artwork-manual.php
@@ -61,9 +61,7 @@ foreach ($imgFiles_previewArtworkManual as $key => $path) {
   <div id="preview-artwork-container" class="preview-artwork-container">
     <div class="preview-artwork-boxes">
       <h2>Artwork</h2>
-      <div class="artwork-boxes">
-        <h3>If you already have a design for your lanyard, select the artwork option</h3>
-      </div>
+      <h3>If you already have a design for your lanyard, select the artwork option</h3>
       <img src="<?= $imgFiles_previewArtworkManual['artwork'] ?>" alt="Artwork">
     </div>
   </div>
