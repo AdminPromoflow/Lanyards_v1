b2dc4823d7f2d68ac4d4ed304395b2f03ec53020
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-02-13 04:04:03 +0000

    ...

--- views/home/sections/accessories.php
+++ views/home/sections/accessories.php
@@ -10,7 +10,9 @@ $accessories_home_js = "../../views/assets/js/home/sections/accessories.js";
 <link rel="stylesheet" href="<?php echo $accessories_home_css . '?v=' . accessories_home_file_version($accessories_home_css); ?>">
 <section id="accessories_home" class="accessories_home">
   <div class="container_accessories_home">
-    <h1>Hola</h1>
+    <div class="box_accessories_home">
+
+    </div>
   </div>
 </section>
 <script src="<?php echo $accessories_home_js . '?v=' . accessories_home_file_version($accessories_home_js); ?>" type="text/javascript"></script>
