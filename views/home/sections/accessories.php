b355b32e3fd1b6f5f46519962871abe8aba270b8
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-02-13 03:54:31 +0000

    Update accessories.php

--- views/home/sections/accessories.php
+++ views/home/sections/accessories.php
@@ -1,7 +1,16 @@
-<link rel="stylesheet" href="../../views/assets/css/home/sections/accessories.css">
-<section id="accesories_home" class="accesories_home">
+<?php
+function accessories_home_file_version($file_path) {
+    return file_exists($file_path) ? filemtime($file_path) : time();
+}
+
+$accessories_home_css = "../../views/assets/css/home/sections/accessories.css";
+$accessories_home_js = "../../views/assets/js/home/sections/accessories.js";
+?>
+
+<link rel="stylesheet" href="<?php echo $accessories_home_css . '?v=' . accessories_home_file_version($accessories_home_css); ?>">
+<section id="accessories_home" class="accessories_home">
   <div class="container_accessories_home">
     <h1>Hola</h1>
   </div>
 </section>
-<scriptn src ="../../views/assets/js/home/sections/accessories.js" type="text/javascript"></script>
+<script src="<?php echo $accessories_home_js . '?v=' . accessories_home_file_version($accessories_home_js); ?>" type="text/javascript"></script>
