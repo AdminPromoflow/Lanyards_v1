93594d050334e01c8d02db60bc1718441910c7a5
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-02-13 03:49:28 +0000

    Update accessories.php

--- views/home/sections/accessories.php
+++ views/home/sections/accessories.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" href="./../views/assets/css/home/sections/accesories.css">
 <section id="accesories_home" class="accesories_home">
   <div class="container_accessories_home">
-
+    <h1>Hola</h1>
   </div>
 </section>
-<scriptn src ="views/assets/js/home/sections/accessories.js" type="text/javascript"></script>
+<scriptn src ="../../views/assets/js/home/sections/accessories.js" type="text/javascript"></script>
