6b01a861d675052104c6ac256db4c58f28470ce8
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-02-13 01:00:02 +0000

    Update text.php

--- views/global/customize-lanyard/sections/text.php
+++ views/global/customize-lanyard/sections/text.php
@@ -1,6 +1,20 @@
-<link rel="stylesheet" href="../../views/assets/css/global/customize-lanyard/sections/text.css">
-<section class="text  section ">
-  <h2>Text</h2>
+<?php
+function asset_version_text($filePath) {
+    $realPath = realpath(__DIR__ . $filePath);
+    return ($realPath && file_exists($realPath)) ? filemtime($realPath) : time();
+}
+
+$cssFile_text = "../../views/assets/css/global/customize-lanyard/sections/text.css";
+$jsFile_text = "../../views/assets/js/customize-lanyard/sections/text.js";
+$imgPath_text = "../../views/assets/img/global/customize-lanyard/sections/text/";
+
+?>
+
+<!-- CSS -->
+<link rel="stylesheet" href="<?= $cssFile_text ?>?v=<?= asset_version_text($cssFile_text) ?>">
+
+<section class="text section">
+  <h2>Text.</h2>
   <div id="containers-boxes-text" class="containers-boxes-text">
     <div class="containers-text1">
       <div class="item-text">
@@ -8,10 +22,10 @@
         <input id="textInput" type="text" name="" value="">
       </div>
       <div class="item-text">
-        <p id="label-repeat-text">Repeat text 1 times</p>
+        <p id="label-repeat-text">Repeat text 1 time</p>
         <div class="containr-repeat-text">
           <div class="repeat-text-box">
-          <h3>-</h3>
+            <h3>-</h3>
           </div>
           <div class="repeat-text-box">
             <h3>+</h3>
@@ -23,91 +37,54 @@
         <div class="colour-text-select" id="colour-text-select">
           <h3>Black</h3>
         </div>
-        <div class="colour-text-select-container" id="colour-text-select-container">
-        </div>
+        <div class="colour-text-select-container" id="colour-text-select-container"></div>
       </div>
-      <div  class="item-text">
+      <div class="item-text">
         <p>Click to choose a font style:</p>
         <div class="type-text-select" id="type-text-select">
           <h3>Arial</h3>
         </div>
-        <div class="type-text-select-container" id="type-text-select-container">
-
-
-        </div>
+        <div class="type-text-select-container" id="type-text-select-container"></div>
       </div>
       <div class="item-text">
         <p>Choose the text styling</p>
-
         <div class="containr-styling-text">
-
           <div class="styling-text-box">
-            <img src="../../views/assets/img/global/customize-lanyard/sections/text/Bold.png" alt="">
+            <img src="<?= $imgPath_text ?>Bold.png?v=<?= asset_version_text($imgPath_text . 'Bold.png') ?>" alt="">
           </div>
           <div class="styling-text-box">
-            <img src="../../views/assets/img/global/customize-lanyard/sections/text/Italic.png" alt="">
+            <img src="<?= $imgPath_text ?>Italic.png?v=<?= asset_version_text($imgPath_text . 'Italic.png') ?>" alt="">
           </div>
           <div class="styling-text-box">
-            <img src="../../views/assets/img/global/customize-lanyard/sections/text/Underline.png" alt="">
+            <img src="<?= $imgPath_text ?>Underline.png?v=<?= asset_version_text($imgPath_text . 'Underline.png') ?>" alt="">
           </div>
-
         </div>
       </div>
       <div class="item-text">
         <p>Adjust the text size</p>
-
         <div class="containr-size-text">
-
           <div class="size-text-box">
-            <img src="../../views/assets/img/global/customize-lanyard/sections/text/smaller.png" alt="">
+            <img src="<?= $imgPath_text ?>smaller.png?v=<?= asset_version_text($imgPath_text . 'smaller.png') ?>" alt="">
           </div>
-
           <div class="size-text-box">
-            <img src="../../views/assets/img/global/customize-lanyard/sections/text/bigger.png" alt="">
+            <img src="<?= $imgPath_text ?>bigger.png?v=<?= asset_version_text($imgPath_text . 'bigger.png') ?>" alt="">
           </div>
-
         </div>
       </div>
       <div class="item-text">
         <p>Adjust the spacing between the text</p>
-
         <div class="containr-space-between-text">
-
           <div class="space-between-text-box">
-            <img src="../../views/assets/img/global/customize-lanyard/sections/text/minus.png" alt="">
+            <img src="<?= $imgPath_text ?>minus.png?v=<?= asset_version_text($imgPath_text . 'minus.png') ?>" alt="">
           </div>
-
           <div class="space-between-text-box">
-            <img src="../../views/assets/img/global/customize-lanyard/sections/text/plus.png" alt="">
+            <img src="<?= $imgPath_text ?>plus.png?v=<?= asset_version_text($imgPath_text . 'plus.png') ?>" alt="">
           </div>
-
         </div>
       </div>
-<!--      <div class="item-text">
-        <p>Adjust the text position</p>
-
-        <div class="container-position-text">
-
-          <div class="position-text-box">
-            <img src="../../views/assets/img/global/customize-lanyard/sections/text/left.png" alt="">
-          </div>
-
-          <div class="position-text-box">
-            <img src="../../views/assets/img/global/customize-lanyard/sections/text/top.png" alt="">
-          </div>
-
-          <div class="position-text-box">
-            <img src="../../views/assets/img/global/customize-lanyard/sections/text/right.png" alt="">
-          </div>
-
-          <div class="position-text-box">
-            <img src="../../views/assets/img/global/customize-lanyard/sections/text/bottom.png" alt="">
-          </div>
-
-        </div>
-      </div>-->
-
     </div>
   </div>
 </section>
-<script src="../../views/assets/js/customize-lanyard/sections/text.js" type="text/javascript"></script>
+
+<!-- JavaScript -->
+<script src="<?= $jsFile_text ?>?v=<?= asset_version_text($jsFile_text) ?>" type="text/javascript"></script>
