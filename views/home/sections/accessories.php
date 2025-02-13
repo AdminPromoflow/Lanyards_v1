58bec094a3bf13722fc0fa25f873e3674905d09c
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-02-13 12:23:06 +0000

    ...

--- views/home/sections/accessories.php
+++ views/home/sections/accessories.php
@@ -13,11 +13,17 @@ $accessories_home_js = "../../views/assets/js/home/sections/accessories.js";
   <div class="container_accessories_home">
     <div class="box_accessories_home">
       <img src="../../views/assets/img/home/accessories/1.png" alt="">
-      <div class="subcontainer_accessories_home">
+      <div class="subcontainer_accessories_home_quantity">
         <div class="dropbox_accessories_home">
-          <h3>Select</h3>
+          <h3>Quantity</h3>
         </div>
         <input type="text" name="" value="">
+      </div>
+      <div class="subcontainer_accessories_home_quantity">
+        <div class="box_accessories_home_quantity">
+`         <h3>Description</h3>`
+          <h3>£3 per unit</h3>
+        </div>
       </div>
     </div>
     <div class="box_accessories_home">
