8c55b443e224bd59a1863105096a34434bac76aa
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-02-19 00:46:46 +0000

    ...

--- views/global/customize-lanyard/sections2/preview-colour.php
+++ views/global/customize-lanyard/sections2/preview-colour.php
@@ -1,4 +1,18 @@
-<link rel="stylesheet" href="../../views/assets/css/global/customize-lanyard/sections2/preview-colour.css">
+<?php
+// Function to get the last modified timestamp of the file
+function get_customize_lanyard_file_version($filePath) {
+    if (file_exists($filePath)) {
+        return filemtime($filePath);
+    }
+    return time(); // If the file doesn't exist, return current timestamp
+}
+
+$customize_lanyard_css = "../../views/assets/css/global/customize-lanyard/sections2/preview-colour.css";
+$customize_lanyard_js = "../../views/assets/js/customize-lanyard/sections2/preview-colour.js";
+?>
+
+<!-- Link CSS with versioning -->
+<link rel="stylesheet" href="<?php echo $customize_lanyard_css . '?v=' . get_customize_lanyard_file_version($customize_lanyard_css); ?>">
 
 <section id="preview-colour-container" class="preview-colour-container">
   <!-- <h3>Tubular</h3>
@@ -8,7 +22,8 @@
   </div>
   <div class="description-preview-material">
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.   </p>
-  </div>-->
+  </div> -->
 </section>
-<script src="../../views/assets/js/customize-lanyard/sections2/preview-colour.js" type="text/javascript">
-</script>
+
+<!-- Include JS with versioning -->
+<script src="<?php echo $customize_lanyard_js . '?v=' . get_customize_lanyard_file_version($customize_lanyard_js); ?>" type="text/javascript"></script>
