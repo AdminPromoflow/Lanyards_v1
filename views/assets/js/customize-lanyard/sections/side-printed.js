0406e3373edcdc2860706661d401d5218e894b09
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-02-19 18:28:48 +0000

    Update side-printed.js

--- views/assets/js/customize-lanyard/sections/side-printed.js
+++ views/assets/js/customize-lanyard/sections/side-printed.js
@@ -16,6 +16,7 @@ class SidePrinted {
 
     // Get the available side printed options
     let sidePrintedAvailable = this.updatePriceSidePrinted();
+    alert(JSON.stringify(sidePrintedAvailable));
 
     // Iterate through the available side printed options and draw them
     for (var i = 0; i < sidePrintedAvailable.length; i++) {
