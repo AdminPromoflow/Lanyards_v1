672fac6ea15d252a04657f2026cec19074b60e0e
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-02-24 15:41:34 -0500

    ef

--- views/assets/js/terms_conditions/sections/teams_conditions.js
+++ views/assets/js/terms_conditions/sections/teams_conditions.js
@@ -5,5 +5,5 @@ class TermsConditions {
     })
   }
 }
-const termsConditions = new TermsConditions();
 const back_home_from_terms_conditions = document.getElementById('back_home_from_terms_conditions');
+const termsConditions = new TermsConditions();
