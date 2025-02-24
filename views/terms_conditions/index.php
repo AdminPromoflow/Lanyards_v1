e971b32803850a850a5993e0a8b943d07f115ec1
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-02-24 10:31:32 -0500

    ..

--- views/terms_conditions/index.php
+++ views/terms_conditions/index.php
@@ -6,5 +6,39 @@
   </head>
   <body>
     <?php include '../../views/terms_conditions/sections/terms_conditions.php'; ?>
+    <section>
+      <?php // include "../../views/global/test_alert.php"; ?>
+      <?php include "../../views/global/charging.php"; ?>
+
+      <!-- Background section -->
+      <div class="background">
+
+        <!-- Container for content -->
+        <div class="container" id="dad-customize-lanyard">
+
+          <!-- Content section -->
+          <div class="content">
+
+            <!-- Include the menu component -->
+            <?php include "../../views/global/menu.php"; ?>
+            <div class="bgLanyards">
+
+              <!-- Include the slider component for the index page -->
+
+              <?php include "../../views/global/footer.php"; ?>
+
+            </div>
+          </div>
+
+          <!-- Include the login component -->
+          <?php include "../../views/global/login.php"; ?>
+          <?php include "../../views/global/password_forgotten.php"; ?>
+
+          <!-- Include the register component -->
+          <?php include "../../views/global/register.php"; ?>
+
+        </div>
+      </div>
+    </section>
   </body>
 </html>
