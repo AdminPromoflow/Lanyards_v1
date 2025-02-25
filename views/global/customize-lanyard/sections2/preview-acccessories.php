26cd3260e8ad4540fbdc0fce432494bafb355bb7
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-02-25 15:09:25 -0500

    fde

--- views/global/customize-lanyard/sections2/preview-acccessories.php
+++ views/global/customize-lanyard/sections2/preview-acccessories.php
@@ -1,31 +1,32 @@
 <?php
-function preview_accessories_version($file) {
+function previewAccessories_filemtime($file) {
     return file_exists($file) ? filemtime($file) : time();
 }
 
-$preview_accessories_css = "../../views/assets/css/global/customize-lanyard/sections2/preview-accessories.css";
-$preview_accessories_js = "../../views/assets/js/customize-lanyard/sections2/preview-accessories.js";
+$previewAccessories_css = "../../views/assets/css/global/customize-lanyard/sections2/preview-accessories.css";
+$previewAccessories_js = "../../views/assets/js/customize-lanyard/sections2/preview-accessories.js";
 
-// Array de imágenes con sus rutas
-$preview_accessories_images = [
-    "../../views/assets/img/global/customize-lanyard/sections/accessories/6.png",
-    "../../views/assets/img/global/customize-lanyard/sections/accessories/2.png",
-    "../../views/assets/img/global/customize-lanyard/sections/accessories/1.png",
-    "../../views/assets/img/global/customize-lanyard/sections/accessories/7.png",
-    "../../views/assets/img/global/customize-lanyard/sections/accessories/12.png",
-    "../../views/assets/img/global/customize-lanyard/sections/accessories/9.png",
-    "../../views/assets/img/global/customize-lanyard/sections/accessories/11.png"
+$previewAccessories_images = [
+    "6.png", "2.png", "1.png", "7.png", "7.png", "7.png",
+    "12.png", "12.png", "12.png", "12.png", "12.png",
+    "9.png", "11.png"
 ];
+
+$img_path_base = "../../views/assets/img/global/customize-lanyard/sections/accessories/";
 ?>
 
-<link rel="stylesheet" href="<?php echo $preview_accessories_css . '?v=' . preview_accessories_version($preview_accessories_css); ?>">
+<link rel="stylesheet" href="<?php echo $previewAccessories_css . '?v=' . previewAccessories_filemtime($previewAccessories_css); ?>">
 
 <section class="previewAccessories" id="preview_accessories">
-  <?php foreach ($preview_accessories_images as $image): ?>
+
+  <?php foreach ($previewAccessories_images as $img) :
+      $img_path = $img_path_base . $img;
+  ?>
     <div class="accessories_clear_plastic_closed_face">
-      <img src="<?php echo $image . '?v=' . preview_accessories_version($image); ?>" alt="">
+      <img src="<?php echo $img_path . '?v=' . previewAccessories_filemtime($img_path); ?>" alt="">
     </div>
   <?php endforeach; ?>
+
 </section>
 
-<script src="<?php echo $preview_accessories_js . '?v=' . preview_accessories_version($preview_accessories_js); ?>" type="text/javascript"></script>
+<script src="<?php echo $previewAccessories_js . '?v=' . previewAccessories_filemtime($previewAccessories_js); ?>" type="text/javascript"></script>
