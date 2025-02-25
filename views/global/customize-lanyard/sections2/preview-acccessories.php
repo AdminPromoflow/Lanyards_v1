7769975404d4ed6b6e093c569bddcd4c882a6149
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-02-25 15:10:57 -0500

    fde

--- views/global/customize-lanyard/sections2/preview-acccessories.php
+++ views/global/customize-lanyard/sections2/preview-acccessories.php
@@ -1,32 +1,57 @@
-<?php
-function previewAccessories_filemtime($file) {
-    return file_exists($file) ? filemtime($file) : time();
-}
-
-$previewAccessories_css = "../../views/assets/css/global/customize-lanyard/sections2/preview-accessories.css";
-$previewAccessories_js = "../../views/assets/js/customize-lanyard/sections2/preview-accessories.js";
-
-$previewAccessories_images = [
-    "6.png", "2.png", "1.png", "7.png", "7.png", "7.png",
-    "12.png", "12.png", "12.png", "12.png", "12.png",
-    "9.png", "11.png"
-];
-
-$img_path_base = "../../views/assets/img/global/customize-lanyard/sections/accessories/";
-?>
-
-<link rel="stylesheet" href="<?php echo $previewAccessories_css . '?v=' . previewAccessories_filemtime($previewAccessories_css); ?>">
-
+<link rel="stylesheet" href="../../views/assets/css/global/customize-lanyard/sections2/preview-accessories.css">
 <section class="previewAccessories" id="preview_accessories">
 
-  <?php foreach ($previewAccessories_images as $img) :
-      $img_path = $img_path_base . $img;
-  ?>
-    <div class="accessories_clear_plastic_closed_face">
-      <img src="<?php echo $img_path . '?v=' . previewAccessories_filemtime($img_path); ?>" alt="">
-    </div>
-  <?php endforeach; ?>
+  <div class="accessories_clear_plastic_closed_face big_accessory">
+    <img src="../../views/assets/img/global/customize-lanyard/sections/accessories/6.png" alt="">
+  </div>
+
+  <div class="accessories_clear_plastic_closed_face big_accessory">
+    <img src="../../views/assets/img/global/customize-lanyard/sections/accessories/2.png" alt="">
+  </div>
+
+  <div class="accessories_clear_plastic_closed_face big_accessory">
+    <img src="../../views/assets/img/global/customize-lanyard/sections/accessories/1.png" alt="">
+  </div>
+
+  <div class="accessories_clear_plastic_closed_face small_accessory">
+    <img src="../../views/assets/img/global/customize-lanyard/sections/accessories/7.png" alt="">
+  </div>
+
+  <div class="accessories_clear_plastic_closed_face medium_accessory">
+    <img src="../../views/assets/img/global/customize-lanyard/sections/accessories/7.png" alt="">
+  </div>
+
+  <div class="accessories_clear_plastic_closed_face big_accessory">
+    <img src="../../views/assets/img/global/customize-lanyard/sections/accessories/7.png" alt="">
+  </div>
+
+  <div class="accessories_clear_plastic_closed_face small_accessory_other">
+    <img src="../../views/assets/img/global/customize-lanyard/sections/accessories/12.png" alt="">
+  </div>
+
+  <div class="accessories_clear_plastic_closed_face small_accessory_other">
+    <img src="../../views/assets/img/global/customize-lanyard/sections/accessories/12.png" alt="">
+  </div>
+
+  <div class="accessories_clear_plastic_closed_face small_accessory_other">
+    <img src="../../views/assets/img/global/customize-lanyard/sections/accessories/12.png" alt="">
+  </div>
+
+  <div class="accessories_clear_plastic_closed_face small_accessory_other">
+    <img src="../../views/assets/img/global/customize-lanyard/sections/accessories/12.png" alt="">
+  </div>
+
+  <div class="accessories_clear_plastic_closed_face small_accessory_other">
+    <img src="../../views/assets/img/global/customize-lanyard/sections/accessories/12.png" alt="">
+  </div>
+
+  <div class="accessories_clear_plastic_closed_face small_accessory_other">
+    <img src="../../views/assets/img/global/customize-lanyard/sections/accessories/9.png" alt="">
+  </div>
+
+  <div class="accessories_clear_plastic_closed_face medium_accessory_other">
+    <img src="../../views/assets/img/global/customize-lanyard/sections/accessories/11.png" alt="">
+  </div>
 
 </section>
-
-<script src="<?php echo $previewAccessories_js . '?v=' . previewAccessories_filemtime($previewAccessories_js); ?>" type="text/javascript"></script>
+<script src="../../views/assets/js/customize-lanyard/sections2/preview-accessories.js" type="text/javascript"></script>
