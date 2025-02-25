80ca8af8a32057a156ce52c96cc997490a18ab6d
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2025-02-25 15:03:49 -0500

    dfds

--- views/global/customize-lanyard/sections2/preview-acccessories.php
+++ views/global/customize-lanyard/sections2/preview-acccessories.php
@@ -1,57 +1,31 @@
-<link rel="stylesheet" href="../../views/assets/css/global/customize-lanyard/sections2/preview-accessories.css">
+<?php
+function preview_accessories_version($file) {
+    return file_exists($file) ? filemtime($file) : time();
+}
+
+$preview_accessories_css = "../../views/assets/css/global/customize-lanyard/sections2/preview-accessories.css";
+$preview_accessories_js = "../../views/assets/js/customize-lanyard/sections2/preview-accessories.js";
+
+// Array de imágenes con sus rutas
+$preview_accessories_images = [
+    "../../views/assets/img/global/customize-lanyard/sections/accessories/6.png",
+    "../../views/assets/img/global/customize-lanyard/sections/accessories/2.png",
+    "../../views/assets/img/global/customize-lanyard/sections/accessories/1.png",
+    "../../views/assets/img/global/customize-lanyard/sections/accessories/7.png",
+    "../../views/assets/img/global/customize-lanyard/sections/accessories/12.png",
+    "../../views/assets/img/global/customize-lanyard/sections/accessories/9.png",
+    "../../views/assets/img/global/customize-lanyard/sections/accessories/11.png"
+];
+?>
+
+<link rel="stylesheet" href="<?php echo $preview_accessories_css . '?v=' . preview_accessories_version($preview_accessories_css); ?>">
+
 <section class="previewAccessories" id="preview_accessories">
-
-  <div class="accessories_clear_plastic_closed_face big_accessory">
-    <img src="../../views/assets/img/global/customize-lanyard/sections/accessories/6.png" alt="">
-  </div>
-
-  <div class="accessories_clear_plastic_closed_face big_accessory">
-    <img src="../../views/assets/img/global/customize-lanyard/sections/accessories/2.png" alt="">
-  </div>
-
-  <div class="accessories_clear_plastic_closed_face big_accessory">
-    <img src="../../views/assets/img/global/customize-lanyard/sections/accessories/1.png" alt="">
-  </div>
-
-  <div class="accessories_clear_plastic_closed_face small_accessory">
-    <img src="../../views/assets/img/global/customize-lanyard/sections/accessories/7.png" alt="">
-  </div>
-
-  <div class="accessories_clear_plastic_closed_face medium_accessory">
-    <img src="../../views/assets/img/global/customize-lanyard/sections/accessories/7.png" alt="">
-  </div>
-
-  <div class="accessories_clear_plastic_closed_face big_accessory">
-    <img src="../../views/assets/img/global/customize-lanyard/sections/accessories/7.png" alt="">
-  </div>
-
-  <div class="accessories_clear_plastic_closed_face small_accessory_other">
-    <img src="../../views/assets/img/global/customize-lanyard/sections/accessories/12.png" alt="">
-  </div>
-
-  <div class="accessories_clear_plastic_closed_face small_accessory_other">
-    <img src="../../views/assets/img/global/customize-lanyard/sections/accessories/12.png" alt="">
-  </div>
-
-  <div class="accessories_clear_plastic_closed_face small_accessory_other">
-    <img src="../../views/assets/img/global/customize-lanyard/sections/accessories/12.png" alt="">
-  </div>
-
-  <div class="accessories_clear_plastic_closed_face small_accessory_other">
-    <img src="../../views/assets/img/global/customize-lanyard/sections/accessories/12.png" alt="">
-  </div>
-
-  <div class="accessories_clear_plastic_closed_face small_accessory_other">
-    <img src="../../views/assets/img/global/customize-lanyard/sections/accessories/12.png" alt="">
-  </div>
-
-  <div class="accessories_clear_plastic_closed_face small_accessory_other">
-    <img src="../../views/assets/img/global/customize-lanyard/sections/accessories/9.png" alt="">
-  </div>
-
-  <div class="accessories_clear_plastic_closed_face medium_accessory_other">
-    <img src="../../views/assets/img/global/customize-lanyard/sections/accessories/11.png" alt="">
-  </div>
-
+  <?php foreach ($preview_accessories_images as $image): ?>
+    <div class="accessories_clear_plastic_closed_face">
+      <img src="<?php echo $image . '?v=' . preview_accessories_version($image); ?>" alt="">
+    </div>
+  <?php endforeach; ?>
 </section>
-<script src="../../views/assets/js/customize-lanyard/sections2/preview-accessories.js" type="text/javascript"></script>
+
+<script src="<?php echo $preview_accessories_js . '?v=' . preview_accessories_version($preview_accessories_js); ?>" type="text/javascript"></script>
