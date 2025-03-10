28a7bd0e28c1c6778da4fccab48f1f36abf1646c
Aleina Rossui <admin@promoflow.net>
2025-03-10 04:33:57 -0700

    “…”

--- views/assets/js/global/login.js
+++ views/assets/js/global/login.js
@@ -136,7 +136,8 @@ class Login {
           return response.json(); // Parse the response as JSON
       })
       .then(data => {
-        alert(data.google_login + 'hi');
+        JSON.parse(data);
+      //  alert(data.google_login + 'hi');
           // Check if 'google_login' is true
           if (data.google_login) {
               // Handle successful login
