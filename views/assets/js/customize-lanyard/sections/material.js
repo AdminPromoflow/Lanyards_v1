177c409047886b9c56f037a8f3ad01dc0d301e4a
Aleina Rossui <admin@promoflow.net>
2025-03-14 15:58:41 -0700

    “…”

--- views/assets/js/customize-lanyard/sections/material.js
+++ views/assets/js/customize-lanyard/sections/material.js
@@ -291,7 +291,7 @@ class Material {
     sidePrintedClass.refreshSidePrintedData();
     colourClass.updateColourQuantity();
 
-  //  alert("6");
+    alert("7");
 
     clipClass.updateClip();
   }
