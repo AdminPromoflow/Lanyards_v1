4c8b4e969cdf37a571cd074fabf7b22ade5bbd95
Laura  Rozo <laurarozo@MacBook-Pro-de-Laura.local>
2025-03-24 10:44:07 -0500

    “...”

--- views/assets/js/customize-lanyard/sections/width.js
+++ views/assets/js/customize-lanyard/sections/width.js
@@ -92,23 +92,6 @@ class Width {
     //this.showSelectedWidth();
     // this.updatePriceWidthIndividual(index);
 
-    //  alert("individual" + index);
-      const priceDataWidth = document.querySelectorAll(".priceDataWidth");
-
-      for (var i = 0; i < priceDataWidth.length; i++) {
-          if (i == index) {
-              let text = priceDataWidth[i].innerHTML + "";
-              let number = +text.match(/-?\d+\.\d+|\d+/); // Finds the first number (float or integer), which can be negative.
-
-              if (number >= 0) {
-                  let result = number.toFixed(2);
-                  priceClass.setPriceWidth(result); // Displays the positive float number with two decimals.
-                  priceClass.changePricePerLanyard();
-              } else {
-                  throw new Error("The number width is negative or no numbers were found.");
-              }
-          }
-      }
     this.refreshWidth();
     oneTwoEndsClass.refreshLanyardType();
     sidePrintedClass.refreshSidePrintedData()
@@ -219,7 +202,7 @@ class Width {
 
       // Get the elements to display price data.
       const priceDataWidth = document.querySelectorAll(".priceDataWidth");
-    //  alert(JSON.stringify(priceDataWidth));
+      alert(JSON.stringify(priceDataWidthResult));
 
 
       // Update the price display for each element.
