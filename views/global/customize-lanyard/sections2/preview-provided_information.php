ceac8276c754d0bec555330cb7d0b4a4363dcf1e
Laura  Rozo <laurarozo@MacBook-Pro-de-Laura.local>
2025-03-25 10:27:43 -0500

    “...”

--- views/global/customize-lanyard/sections2/preview-provided_information.php
+++ views/global/customize-lanyard/sections2/preview-provided_information.php
@@ -1,12 +1,17 @@
-  <link rel="stylesheet" href="../../views/assets/css/global/customize-lanyard/sections2/preview-provided-information.css">
-  <script src='https://api.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.js'></script>
-  <link href='https://api.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.css' rel='stylesheet' />
-  <section  class="preview-provided-information-container" id="preview-provided-information-container">
+<?php
+function previewProvidedInformation($filePath) {
+    return $filePath . '?v=' . filemtime($_SERVER['DOCUMENT_ROOT'] . $filePath);
+}
+?>
+
+<link rel="stylesheet" href="<?php echo previewProvidedInformation('/views/assets/css/global/customize-lanyard/sections2/preview-provided-information.css'); ?>">
+<script src='https://api.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.js'></script>
+<link href='https://api.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.css' rel='stylesheet' />
+
+<section class="preview-provided-information-container" id="preview-provided-information-container">
     <h2>Location</h2>
     <br>
-
     <div id="map"></div>
+</section>
 
-  </section>
-
-  <script src="../../views/assets/js/customize-lanyard/sections2/preview-provided-information.js" type="text/javascript"></script>
+<script src="<?php echo previewProvidedInformation('/views/assets/js/customize-lanyard/sections2/preview-provided-information.js'); ?>" type="text/javascript"></script>
