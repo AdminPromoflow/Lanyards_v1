2cfd5416f6e20527791774be9e0951f519124058
Laura  Rozo <laurarozo@MacBook-Pro-de-Laura.local>
2025-03-26 06:41:34 -0500

    “...”

--- controller/users/login-google.php
+++ controller/users/login-google.php
@@ -174,8 +174,8 @@ class ApiHandlerLoginGoogle
     //  $data->emailRegister = "juan@example.com";
     //  $data->passwordRegister = "123456";
       //$data->signupCategory = "normal";
-      $apiHandler = new ApiHandlerRegister();
-      $apiHandler->ejemplo();
+      //$apiHandler = new ApiHandlerRegister();
+      //$apiHandler->ejemplo();
       }
       else {
         header('Content-Type: application/json');
