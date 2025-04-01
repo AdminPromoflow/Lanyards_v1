776b353d2662f9357a16700f5f060e7f6d9b7e20
Aleina Rossui <admin@promoflow.net>
2025-04-01 14:54:38 -0700

    “…”

---
+++ views/user_deletion_request/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Request to Delete User Account</h1>
+    <p>Please provide the email address you wish to delete.</p>
+    <input type="email" name="" value="">
+    <button type="button" name="button">submit</button>
+  </body>
+</html>
