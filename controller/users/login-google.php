ae29599e239cd3506198e8a857240f1efd8edd93
Aleina Rossui <admin@promoflow.net>
2025-03-31 21:24:19 -0700

    “…”

--- controller/users/login-google.php
+++ controller/users/login-google.php
@@ -153,7 +153,9 @@ class ApiHandlerLoginGoogle
                           $data->passwordRegister
                       );
 
-
+                      header('Content-Type: application/json');
+                      echo json_encode(array("google_login" => true, "message" => "The user has successfully registered and logged in", "Validate database" => "antes del if".$validatedData));
+                      exit;
 
                       if ($validatedData == false) {
                         //false
