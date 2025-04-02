f2bb17cc2c76108f3e0c729a0ffdc2b18e62e548
Aleina Rossui <admin@promoflow.net>
2025-04-02 12:36:03 -0700

    “…”

--- views/assets/js/global/login.js
+++ views/assets/js/global/login.js
@@ -155,7 +155,7 @@ class Login {
 
       })
       .catch(error => {
-        //  alert("Error: " + error.message); // Maneja errores de red o análisis
+          alert("Error: " + error.message); // Maneja errores de red o análisis
       });
   }
 
