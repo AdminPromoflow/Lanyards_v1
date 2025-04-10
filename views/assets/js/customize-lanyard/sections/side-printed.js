84ccdf4ba39e4df0b073728b6fff7236c2772c6d
Aleina Rossui <admin@promoflow.net>
2025-04-09 18:13:06 -0700

    “…”

--- views/assets/js/customize-lanyard/sections/side-printed.js
+++ views/assets/js/customize-lanyard/sections/side-printed.js
@@ -259,18 +259,12 @@ class SidePrinted {
   }
 
   drawSidePrintedAvailable(data, index){
-    var imgLink;
-    if (this.getSidePrintedSelected() == "one-side") {
-      imgLink = "views/assets/img/global/customize-lanyard/sections/side-printed/one-side.png";
-    }
-    else {
-      imgLink = "views/assets/img/global/customize-lanyard/sections/side-printed/two-side.png";
-    }
+
     containerBoxSidePrinted.innerHTML +=
     '<div class="container_boxes_side_printed" onclick="sidePrintedClass.searchDataSidePrintedSelected(\'' + data.noSides + '\', \' '+ index +'  \');">' +
         '<h3 class="priceDataSidePrinted">+£'+data["price"]+' per unit</h3>' +
         '<h4 class="data_side_printed">'+ data["noSides"]+'</h4>' +
-        '<img src="../../'+ imgLink +'" alt="">' +
+        '<img src="../../views/assets/img/global/customize-lanyard/sections/side-printed/'+ data["noSides"]+'".png alt="">' +
       '</div>'
     ;
   }
