7274c3614fb4c42340c7a1dfdeaf2ad41a121b27
Aleina Rossui <admin@promoflow.net>
2025-04-10 22:51:15 -0700

    “…”

--- views/global/customize-lanyard/sections2/preview-attachment.php
+++ views/global/customize-lanyard/sections2/preview-attachment.php
@@ -1,5 +1,12 @@
-<link rel="stylesheet" href="../../views/assets/css/global/customize-lanyard/sections2/preview-attachment.css">
+<?php
+  $preview_attachment_css = '../../views/assets/css/global/customize-lanyard/sections2/preview-attachment.css';
+  $preview_attachment_js = '../../views/assets/js/customize-lanyard/sections2/preview-attachment.js';
+?>
+
+<link rel="stylesheet" href="<?= $preview_attachment_css ?>?<?= filemtime($preview_attachment_css) ?>">
+
 <section>
 
 </section>
-<script src="../../views/assets/js/customize-lanyard/sections2/preview-attachment.js" type="text/javascript"></script>
+
+<script src="<?= $preview_attachment_js ?>?<?= filemtime($preview_attachment_js) ?>" type="text/javascript"></script>
