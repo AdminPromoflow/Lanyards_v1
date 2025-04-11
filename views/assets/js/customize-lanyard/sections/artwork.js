7747c45394b02c8147ca43eee5b4450e7a97e500
Aleina Rossui <admin@promoflow.net>
2025-04-11 11:40:19 -0700

    “…”

--- views/assets/js/customize-lanyard/sections/artwork.js
+++ views/assets/js/customize-lanyard/sections/artwork.js
@@ -34,6 +34,9 @@ class Artwork {
                     console.log('Dimensiones de la imagen:', img.width, img.height);
                     // Validar dimensiones mínimas de la imagen
                     //if (img.width >= 1287 && img.height >= 54) {
+                    alert("Las dimensiones de la imágen son: " + img.width +
+                  "de ancho y " + img.height + "de alto");
+
                       if (img.width >= 12 && img.height >= 4) {
 
                         // Si las dimensiones son correctas o mayores, establecer la imagen como fondo
