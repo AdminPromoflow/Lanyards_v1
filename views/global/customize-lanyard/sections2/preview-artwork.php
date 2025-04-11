d0c3079dc9fc352f33b664d701a4fd62dfd47421
Aleina Rossui <admin@promoflow.net>
2025-04-11 12:34:27 -0700

    “…”

--- views/global/customize-lanyard/sections2/preview-artwork.php
+++ views/global/customize-lanyard/sections2/preview-artwork.php
@@ -1,55 +1,58 @@
-  <link rel="stylesheet" href="../../views/assets/css/global/customize-lanyard/sections2/preview-artwork.css">
-  <section class="previewArtwork" id="previewArtwork" >
-    <div class="container-previewArtwork">
-      <div class="box-previewArtwork">
-        <div class="measures-previewArtwork">
-          <div class="measures-previewArtwork-long">
-            <h4>39mm</h4>
-          </div>
-          <div class="measures-previewArtwork-width">
-            <h4 class="width-preview-artwork">8mm</h4>
-          </div>
-        </div>
-        <h2>Front left</h2>
-        <div class="lanyard-artwork">
+<?php
+  $css_preview_artwork = '../../views/assets/css/global/customize-lanyard/sections2/preview-artwork.css';
+  $js_preview_artwork = '../../views/assets/js/customize-lanyard/sections2/preview-artwork.js';
+?>
 
-        </div>
-      </div>
-      <div class="box-previewArtwork">
-        <h2>Front right</h2>
+<link rel="stylesheet" href="<?php echo $css_preview_artwork . '?v=' . filemtime($css_preview_artwork); ?>">
+
+<section class="previewArtwork" id="previewArtwork">
+  <div class="container-previewArtwork">
+    <div class="box-previewArtwork">
+      <div class="measures-previewArtwork">
         <div class="measures-previewArtwork-long">
           <h4>39mm</h4>
         </div>
         <div class="measures-previewArtwork-width">
           <h4 class="width-preview-artwork">8mm</h4>
         </div>
-        <div class="lanyard-artwork">
-        </div>
       </div>
-      <div class="box-previewArtwork previewArtworkBackSide">
-        <h2>Back left</h2>
-        <div class="measures-previewArtwork-long">
-          <h4>39mm</h4>
-        </div>
-        <div class="measures-previewArtwork-width">
-          <h4 class="width-preview-artwork">8mm</h4>
-        </div>
-        <div class="lanyard-artwork">
-
-        </div>
-      </div>
-      <div class="box-previewArtwork previewArtworkBackSide">
-        <h2>Back right</h2>
-        <div class="measures-previewArtwork-long">
-          <h4>39mm</h4>
-        </div>
-        <div class="measures-previewArtwork-width">
-          <h4 class="width-preview-artwork">8mm</h4>
-        </div>
-        <div class="lanyard-artwork">
-
-        </div>
-      </div>
+      <h2>Front left</h2>
+      <div class="lanyard-artwork"></div>
     </div>
-  </section>
-  <script src="../../views/assets/js/customize-lanyard/sections2/preview-artwork.js" type="text/javascript"></script>
+
+    <div class="box-previewArtwork">
+      <h2>Front right</h2>
+      <div class="measures-previewArtwork-long">
+        <h4>39mm</h4>
+      </div>
+      <div class="measures-previewArtwork-width">
+        <h4 class="width-preview-artwork">8mm</h4>
+      </div>
+      <div class="lanyard-artwork"></div>
+    </div>
+
+    <div class="box-previewArtwork previewArtworkBackSide">
+      <h2>Back left</h2>
+      <div class="measures-previewArtwork-long">
+        <h4>39mm</h4>
+      </div>
+      <div class="measures-previewArtwork-width">
+        <h4 class="width-preview-artwork">8mm</h4>
+      </div>
+      <div class="lanyard-artwork"></div>
+    </div>
+
+    <div class="box-previewArtwork previewArtworkBackSide">
+      <h2>Back right</h2>
+      <div class="measures-previewArtwork-long">
+        <h4>39mm</h4>
+      </div>
+      <div class="measures-previewArtwork-width">
+        <h4 class="width-preview-artwork">8mm</h4>
+      </div>
+      <div class="lanyard-artwork"></div>
+    </div>
+  </div>
+</section>
+
+<script src="<?php echo $js_preview_artwork . '?v=' . filemtime($js_preview_artwork); ?>" type="text/javascript"></script>
