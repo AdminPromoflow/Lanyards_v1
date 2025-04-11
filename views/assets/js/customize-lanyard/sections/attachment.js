47884ca5a3ea12ebe2194720cfb9de7a0a39be46
Aleina Rossui <admin@promoflow.net>
2025-04-10 22:43:55 -0700

    “…”

--- views/assets/js/customize-lanyard/sections/attachment.js
+++ views/assets/js/customize-lanyard/sections/attachment.js
@@ -5,7 +5,7 @@ class AttachmentClass {
   searchDataAttachmentSelected(attachment, index, price){
     attachmentClass.setAttachmentSelected(attachment);
     attachmentClass.showSelectedAttachment();
-    //attachmentPreviewClass.showSelectedPreviewTemplate();
+    showSelectedPreviewTemplate.showSelectedPreviewTemplate();
     priceClass.setPriceAttachment(price);
     priceClass.changePricePerLanyard();
   }
