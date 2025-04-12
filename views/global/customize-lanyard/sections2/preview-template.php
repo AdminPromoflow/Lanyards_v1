7c45fcb5f54925f7cf0eba3b9746a988b058cbaa
Aleina Rossui <admin@promoflow.net>
2025-04-12 09:34:40 -0700

    “…”

--- views/global/customize-lanyard/sections2/preview-template.php
+++ views/global/customize-lanyard/sections2/preview-template.php
@@ -30,7 +30,7 @@ $jsFilemtime = getPreviewTemplateFilemtime($jsFilePath);
     <div class="left-super-lanyard left-super-lanyard-one-end-10mm">
 
     </div>
-    <div class="right-super-lanyard">
+    <div class="right-super-lanyard right-super-lanyard-one-end-10mm">
 
     </div>
     <div class="center-super-lanyard">
