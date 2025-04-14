28b5d64125e5d355107dbc8355a3d8f6f7a1d7d2
Aleina Rossui <admin@promoflow.net>
2025-04-13 19:23:38 -0700

    “…”

--- views/global/customize-lanyard/sections2/preview-template.php
+++ views/global/customize-lanyard/sections2/preview-template.php
@@ -30,15 +30,15 @@ $jsFilemtime = getPreviewTemplateFilemtime($jsFilePath);
     <!-- Standard lanyard preview layout -->
     <div class="super-lanyard">
       <!-- Left part of the standard lanyard (for one-end, 10mm type) -->
-      <div class="left-super-lanyard left-super-lanyard-one-end-20mm-attachment">
+      <div class="left-super-lanyard left-super-lanyard-one-end-25mm-attachment">
         <!-- Rendered preview for the left segment -->
       </div>
       <!-- Right part of the standard lanyard (for one-end, 10mm type) -->
-      <div class="right-super-lanyard right-super-lanyard-one-end-20mm-attachment">
+      <div class="right-super-lanyard right-super-lanyard-one-end-25mm-attachment">
         <!-- Rendered preview for the right segment -->
       </div>
       <!-- Center part of the standard lanyard (typically the neck area) -->
-      <div class="center-super-lanyard center-super-lanyard-one-end-20mm-attachment">
+      <div class="center-super-lanyard center-super-lanyard-one-end-25mm-attachment">
         <!-- Rendered preview for the center segment -->
       </div>
     </div>
