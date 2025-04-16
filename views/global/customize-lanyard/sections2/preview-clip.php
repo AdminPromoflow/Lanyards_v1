0640974b94e33d58aef3360c8704e1cfde0dd1a3
Aleina Rossui <admin@promoflow.net>
2025-04-16 15:48:03 -0700

    “…”

--- views/global/customize-lanyard/sections2/preview-clip.php
+++ views/global/customize-lanyard/sections2/preview-clip.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="../../views/assets/css/global/customize-lanyard/sections2/preview-clip.css">
 <section class="preview-clip-class" id="preview-clip-class">
   <div class="container-preview-clip">
-
+    <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h2>
   </div>
 </section>
 <script src="../../views/assets/js/customize-lanyard/sections2/preview-clip.js" type="text/javascript"></script>
