97c6571c39cd98b18ac60a546843f2214bda31fe
Aleina Rossui <admin@promoflow.net>
2025-04-17 13:05:24 -0700

    “…”

--- views/global/customize-lanyard/sections2/preview-template.php
+++ views/global/customize-lanyard/sections2/preview-template.php
@@ -1,90 +1,67 @@
 <?php
 // Function to get the last modified time (timestamp) of a file
 function getPreviewTemplateFilemtime($filePath) {
-    return filemtime($filePath); // Returns the file's last modification time
+    return file_exists($filePath) ? filemtime($filePath) : time(); // fallback a time() si no existe
 }
 
-// File paths for CSS and JS used in the lanyard preview
+// File paths for CSS, JS and images
 $cssFilePath = '../../views/assets/css/global/customize-lanyard/sections2/preview-template.css';
 $cssFilePathArtwork = '../../views/assets/css/global/customize-lanyard/sections2/preview-template-artwork.css';
 $cssFilePathManual = '../../views/assets/css/global/customize-lanyard/sections2/preview-template-manual.css';
 $jsFilePath = '../../views/assets/js/customize-lanyard/sections2/preview-template.js';
 
-// Get the last modified times for each file
+// File paths for images
+$imgLeftPath = '../../views/assets/img/global/customize-lanyard/sections2/preview_template/clip-neck-left.png';
+$imgRightPath = '../../views/assets/img/global/customize-lanyard/sections2/preview_template/clip-neck-right.png';
+
+// Get the last modified times
 $cssFilemtime = getPreviewTemplateFilemtime($cssFilePath);
 $cssFilemtimeArtwork = getPreviewTemplateFilemtime($cssFilePathArtwork);
 $cssFilemtimeManual = getPreviewTemplateFilemtime($cssFilePathManual);
 $jsFilemtime = getPreviewTemplateFilemtime($jsFilePath);
+$imgLeftMtime = getPreviewTemplateFilemtime($imgLeftPath);
+$imgRightMtime = getPreviewTemplateFilemtime($imgRightPath);
 ?>
 
-<!-- Include CSS files with versioning based on file modification time (prevents browser caching issues) -->
+<!-- Include CSS files -->
 <link rel="stylesheet" href="<?php echo $cssFilePath; ?>?v=<?php echo $cssFilemtime; ?>">
 <link rel="stylesheet" href="<?php echo $cssFilePathArtwork; ?>?v=<?php echo $cssFilemtimeArtwork; ?>">
 <link rel="stylesheet" href="<?php echo $cssFilePathManual; ?>?v=<?php echo $cssFilemtimeManual; ?>">
 
-<!-- Section that holds all the lanyard preview templates -->
+<!-- Preview Template Section -->
 <section id="preview-template-class" class="preview-template-class">
-
   <div class="container-preview-template">
 
     <!-- Standard lanyard preview layout -->
     <div class="super-lanyard" id="super-lanyard">
       <div class="top_left_clip top_left_clip-one-end-10mm">
-        <img src="../../views/assets/img/global/customize-lanyard/sections2/preview_template/clip-neck-left.png" alt="">
+        <img src="<?php echo $imgLeftPath . '?v=' . $imgLeftMtime; ?>" alt="">
       </div>
       <div class="top_right_clip top_right_clip-one-end-10mm">
-        <img src="../../views/assets/img/global/customize-lanyard/sections2/preview_template/clip-neck-right.png" alt="">
+        <img src="<?php echo $imgRightPath . '?v=' . $imgRightMtime; ?>" alt="">
       </div>
-      <!-- Left part of the standard lanyard (for one-end, 10mm type) -->
-      <div class="left-super-lanyard-one-end-30mm" id="left-super-lanyard">
-        <!-- Rendered preview for the left segment -->
-      </div>
-      <!-- Right part of the standard lanyard (for one-end, 10mm type) -->
-      <div class="right-super-lanyard-one-end-30mm" id="right-super-lanyard">
-        <!-- Rendered preview for the right segment -->
-      </div>
-      <!-- Center part of the standard lanyard (typically the neck area) -->
-      <div class="center-super-lanyard-one-end-30mm" id="center-super-lanyard">
-        <!-- Rendered preview for the center segment -->
 
-      </div>
+      <div class="left-super-lanyard-one-end-30mm" id="left-super-lanyard"></div>
+      <div class="right-super-lanyard-one-end-30mm" id="right-super-lanyard"></div>
+      <div class="center-super-lanyard-one-end-30mm" id="center-super-lanyard"></div>
     </div>
 
-    <!-- Manual mode lanyard preview layout (used when user adjusts elements manually) -->
+    <!-- Manual lanyard layout -->
     <div class="super-lanyard-manual" id="super-lanyard-manual">
-      <!-- Left part of the manual-mode lanyard -->
-      <div class="left-super-lanyard-one-end-30mm-manual" id="left-super-lanyard-manual">
-        <!-- Rendered manual preview for the left segment -->
-      </div>
-      <!-- Right part of the manual-mode lanyard -->
-      <div class="right-super-lanyard-one-end-30mm-manual" id="right-super-lanyard-manual">
-        <!-- Rendered manual preview for the right segment -->
-      </div>
-      <!-- Center part of the manual-mode lanyard -->
-      <div class="center-super-lanyard-one-end-30mm-manual" id="center-super-lanyard-manual">
-        <!-- Rendered manual preview for the center segment -->
-      </div>
+      <div class="left-super-lanyard-one-end-30mm-manual" id="left-super-lanyard-manual"></div>
+      <div class="right-super-lanyard-one-end-30mm-manual" id="right-super-lanyard-manual"></div>
+      <div class="center-super-lanyard-one-end-30mm-manual" id="center-super-lanyard-manual"></div>
     </div>
 
-    <!-- Artwork mode lanyard preview layout (used when uploading custom artwork) -->
+    <!-- Artwork lanyard layout -->
     <div class="super-lanyard-artwork" id="super-lanyard-artwork">
-      <!-- Left part of the artwork-mode lanyard -->
-      <div class=" " id="left-super-lanyard-artwork">
-        <!-- Rendered artwork preview for the left segment -->
-      </div>
-      <!-- Right part of the artwork-mode lanyard -->
-      <div class=" " id="right-super-lanyard-artwork">
-        <!-- Rendered artwork preview for the right segment -->
-      </div>
-      <!-- Center part of the artwork-mode lanyard -->
-      <div class=" " id="center-super-lanyard-artwork">
-        <!-- Rendered artwork preview for the center segment -->
-      </div>
+      <div id="left-super-lanyard-artwork"></div>
+      <div id="right-super-lanyard-artwork"></div>
+      <div id="center-super-lanyard-artwork"></div>
     </div>
 
   </div>
-
 </section>
 
-<!-- Include JavaScript file with versioning based on file modification time (prevents browser caching issues) -->
+<!-- Include JS file -->
 <script src="<?php echo $jsFilePath; ?>?v=<?php echo $jsFilemtime; ?>" type="text/javascript"></script>
