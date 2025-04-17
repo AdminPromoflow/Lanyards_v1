546a50efdba97b204afed98e267376fa5f7ea664
Aleina Rossui <admin@promoflow.net>
2025-04-17 13:50:22 -0700

    “…”

--- views/global/customize-lanyard/sections2/preview-template.php
+++ views/global/customize-lanyard/sections2/preview-template.php
@@ -34,10 +34,10 @@ $imgRightMtime = getPreviewTemplateFilemtime($imgRightPath);
 
     <!-- Standard lanyard preview layout -->
     <div class="super-lanyard" id="super-lanyard">
-      <div class="top_left_clip top_left_clip-one-end-25mm">
+      <div class="top_left_clip top_left_clip-one-end-30mm">
         <img src="<?php echo $imgLeftPath . '?v=' . $imgLeftMtime; ?>" alt="">
       </div>
-      <div class="top_right_clip top_right_clip-one-end-25mm">
+      <div class="top_right_clip top_right_clip-one-end-30mm">
         <img src="<?php echo $imgRightPath . '?v=' . $imgRightMtime; ?>" alt="">
       </div>
 
