94b8cc65db79e93b5758acab187a3e870a7527f3
Aleina Rossui <admin@promoflow.net>
2025-04-16 21:38:24 -0700

    “…”

--- views/global/customize-lanyard/sections2/preview-clip.php
+++ views/global/customize-lanyard/sections2/preview-clip.php
@@ -16,10 +16,10 @@ function preview_clip_asset_version($relativePath) {
     <div class="clip_one_end_10mm clip" id="clip">
       <img src="<?php echo preview_clip_asset_version('/views/assets/img/global/customize-lanyard/sections2/clips/one-end/dog_clip.png'); ?>" alt="">
     </div>
-    <div class="clip_two_ends_20mm_left clips" >
+    <div class="clip_two_ends_25mm_left clips" >
       <img src="<?php echo preview_clip_asset_version('/views/assets/img/global/customize-lanyard/sections2/clips/one-end/dog_clip.png'); ?>" alt="">
     </div>
-    <div class="clip_two_ends_20mm_right clips" >
+    <div class="clip_two_ends_25mm_right clips" >
       <img src="<?php echo preview_clip_asset_version('/views/assets/img/global/customize-lanyard/sections2/clips/one-end/dog_clip0.png'); ?>" alt="">
     </div>
 
