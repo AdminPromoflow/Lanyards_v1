69e608d8f22d515bc93c9469a1c8544e17845574
Aleina Rossui <admin@promoflow.net>
2025-04-21 14:29:37 -0700

    “…”

--- views/assets/js/customize-lanyard/sections2/preview-artwork-manual.js
+++ views/assets/js/customize-lanyard/sections2/preview-artwork-manual.js
@@ -30,12 +30,13 @@ class PreviewArtworkManualClass {
     const materialSelected = material.getMaterialSelected();
 
     if (materialSelected == "Tubular" || materialSelected == "Ribbed Polyester" || materialSelected == "RPET Polyester" ) {
-      description_preview_artwork_manual[0].innerHTML = printingOptions.sublimation_option.manual;
-      description_preview_artwork_manual[1].innerHTML = printingOptions.sublimation_option.artwork;
-    }
-    else if (materialSelected == "Dye Sub polyester" || materialSelected == "Dye Sub RPET" ) {
       description_preview_artwork_manual[0].innerHTML = printingOptions.screen_print_option.manual;
       description_preview_artwork_manual[1].innerHTML = printingOptions.screen_print_option.artwork;
+    }
+    else if (materialSelected == "Dye Sub polyester" || materialSelected == "Dye Sub RPET" ) {
+      description_preview_artwork_manual[0].innerHTML = printingOptions.sublimation_option.manual;
+      description_preview_artwork_manual[1].innerHTML = printingOptions.sublimation_option.artwork;
+
     }
 
   }
