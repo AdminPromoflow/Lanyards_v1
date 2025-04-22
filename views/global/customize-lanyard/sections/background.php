adcb45ce182094dad644913d66d66ba5cbcd55a3
Aleina Rossui <admin@promoflow.net>
2025-04-21 20:50:10 -0700

    “…”

--- views/global/customize-lanyard/sections/background.php
+++ views/global/customize-lanyard/sections/background.php
@@ -13,24 +13,30 @@ $jsFile_background = "../../views/assets/js/customize-lanyard/sections/backgroun
 
 <section class="backgroundSection section">
   <h2 class="name-section-customize-lanyard">Background</h2>
-
-  <!-- Radio option for selecting preset background color -->
-  <input type="radio" class="select-background-colour-option" id="backgroundColourFirstOption" name="option" checked>
-  <label for="backgroundColourFirstOption">Select colour of the background:</label>
-  <div class="opacity-background-option-first-option opacity-option-background-selected"></div>
-  <div id="containers_boxes_background" class="containers_boxes_background">
-    <div class="container_boxes_background" id="container_boxes_background">
-      <!-- Container where dynamic background colors will be appended -->
+  <div class="box_background_colour">
+    <!-- Radio option for selecting preset background color -->
+    <input type="radio" class="select-background-colour-option" id="backgroundColourFirstOption" name="option" checked>
+    <label for="backgroundColourFirstOption">Select colour of the background:</label>
+    <div class="opacity-background-option-first-option opacity-option-background-selected"></div>
+    <div id="containers_boxes_background" class="containers_boxes_background">
+      <div class="container_boxes_background" id="container_boxes_background">
+        <!-- Container where dynamic background colors will be appended -->
+      </div>
     </div>
   </div>
+
   <br>
 
-  <!-- Radio option for customizing background color if not in stock -->
-  <input type="radio" class="select-background-colour-option  screen_print_background_colour" id="backgroundColourSecondOption" name="option">
-  <label class="screen_print_background_colour" for="backgroundColourSecondOption">If your color isn't in stock, customize it for £25 more at checkout:</label>
-  <br>
-  <div class="opacity-background-option-second-option opacity-option-background-selected screen_print_background_colour"></div>
-  <input  class="select-colour-second-option screen_print_background_colour" id="optionColourBackground" type="color" value="#FFFFFF">
+  <div class="box_background_colour">
+    <!-- Radio option for customizing background color if not in stock -->
+    <input type="radio" class="select-background-colour-option  screen_print_background_colour" id="backgroundColourSecondOption" name="option">
+    <label class="screen_print_background_colour" for="backgroundColourSecondOption">If your color isn't in stock, customize it for £25 more at checkout:</label>
+    <br>
+    <div class="opacity-background-option-second-option opacity-option-background-selected screen_print_background_colour"></div>
+    <input  class="select-colour-second-option screen_print_background_colour" id="optionColourBackground" type="color" value="#FFFFFF">
+
+  </div>
+
 </section>
 
 <!-- JavaScript -->
