aa2d7fb6c16997656793a874f4765f2e229ea3ca
Aleina Rossui <admin@promoflow.net>
2025-04-23 11:59:44 -0700

    “…”

--- views/assets/js/customize-lanyard/sections2/preview-template.js
+++ views/assets/js/customize-lanyard/sections2/preview-template.js
@@ -10,6 +10,13 @@ class PreviewTemplate {
         top_right_clip: document.getElementById("top_right_clip")
     };
 
+    this.manualElements = {
+        left: document.getElementById("left-super-lanyard"),
+        right: document.getElementById("right-super-lanyard"),
+        center: document.getElementById("center-super-lanyard")
+    };
+
+
     // Store the main preview container element
     this.previewContainer = document.getElementById("preview-template-class");
   }
@@ -82,79 +89,88 @@ class PreviewTemplate {
 
   // Apply styles for the "no-attachment" case based on width
   applyNoAttachmentStyles(width) {
-
-    // Add appropriate classes based on the width
     switch (width) {
       case "10mm":
         this.applyClassToGroup("left-super-lanyard-one-end-10mm", "right-super-lanyard-one-end-10mm", "", "top_left_clip-one-end-10mm", "top_right_clip-one-end-10mm");
+        this.applyClassToManualGroup("left-super-lanyard-one-end-10mm-manual", "right-super-lanyard-one-end-10mm-manual", "center-super-lanyard-one-end-10mm-manual");
         break;
       case "15mm":
         this.applyClassToGroup("left-super-lanyard-one-end-15mm", "right-super-lanyard-one-end-15mm", "", "top_left_clip-one-end-15mm", "top_right_clip-one-end-15mm");
+        this.applyClassToManualGroup("left-super-lanyard-one-end-15mm-manual", "right-super-lanyard-one-end-15mm-manual", "center-super-lanyard-one-end-15mm-manual");
         break;
       case "20mm":
         this.applyClassToGroup("left-super-lanyard-one-end-20mm", "right-super-lanyard-one-end-20mm", "", "top_left_clip-one-end-20mm", "top_right_clip-one-end-20mm");
+        this.applyClassToManualGroup("left-super-lanyard-one-end-20mm-manual", "right-super-lanyard-one-end-20mm-manual", "center-super-lanyard-one-end-20mm-manual");
         break;
       case "25mm":
         this.applyClassToGroup("left-super-lanyard-one-end-25mm", "right-super-lanyard-one-end-25mm", "", "top_left_clip-one-end-25mm", "top_right_clip-one-end-25mm");
+        this.applyClassToManualGroup("left-super-lanyard-one-end-25mm-manual", "right-super-lanyard-one-end-25mm-manual", "center-super-lanyard-one-end-25mm-manual");
         break;
       case "30mm":
         this.applyClassToGroup("left-super-lanyard-one-end-30mm", "right-super-lanyard-one-end-30mm", "", "top_left_clip-one-end-30mm", "top_right_clip-one-end-30mm");
+        this.applyClassToManualGroup("left-super-lanyard-one-end-30mm-manual", "right-super-lanyard-one-end-30mm-manual", "center-super-lanyard-one-end-30mm-manual");
         break;
       default:
         console.warn("Unrecognised width in 'no-attachment' mode:", width);
     }
 
-    console.log(`Styles applied: no-attachment - ${width} `);
+    console.log(`Styles applied: no-attachment - ${width}`);
   }
+
 
   // Apply styles for the "with-attachment" case based on width
   applyWithAttachmentStyles(width) {
-
-
-    // Add appropriate classes based on the width
     switch (width) {
       case "10mm":
         this.applyClassToGroup("left-super-lanyard-one-end-10mm-attachment", "right-super-lanyard-one-end-10mm-attachment", "center-super-lanyard-one-end-10mm-attachment", "top_left_clip-one-end-10mm-attachment", "top_right_clip-one-end-10mm-attachment");
+        this.applyClassToManualGroup("left-super-lanyard-one-end-10mm-attachment-manual", "right-super-lanyard-one-end-10mm-attachment-manual", "center-super-lanyard-one-end-10mm-attachment-manual");
         break;
       case "15mm":
         this.applyClassToGroup("left-super-lanyard-one-end-15mm-attachment", "right-super-lanyard-one-end-15mm-attachment", "center-super-lanyard-one-end-15mm-attachment", "top_left_clip-one-end-15mm-attachment", "top_right_clip-one-end-15mm-attachment");
+        this.applyClassToManualGroup("left-super-lanyard-one-end-15mm-attachment-manual", "right-super-lanyard-one-end-15mm-attachment-manual", "center-super-lanyard-one-end-15mm-attachment-manual");
         break;
       case "20mm":
         this.applyClassToGroup("left-super-lanyard-one-end-20mm-attachment", "right-super-lanyard-one-end-20mm-attachment", "center-super-lanyard-one-end-20mm-attachment", "top_left_clip-one-end-20mm-attachment", "top_right_clip-one-end-20mm-attachment");
+        this.applyClassToManualGroup("left-super-lanyard-one-end-20mm-attachment-manual", "right-super-lanyard-one-end-20mm-attachment-manual", "center-super-lanyard-one-end-20mm-attachment-manual");
         break;
       case "25mm":
         this.applyClassToGroup("left-super-lanyard-one-end-25mm-attachment", "right-super-lanyard-one-end-25mm-attachment", "center-super-lanyard-one-end-25mm-attachment", "top_left_clip-one-end-25mm-attachment", "top_right_clip-one-end-25mm-attachment");
+        this.applyClassToManualGroup("left-super-lanyard-one-end-25mm-attachment-manual", "right-super-lanyard-one-end-25mm-attachment-manual", "center-super-lanyard-one-end-25mm-attachment-manual");
         break;
       case "30mm":
         this.applyClassToGroup("left-super-lanyard-one-end-30mm-attachment", "right-super-lanyard-one-end-30mm-attachment", "center-super-lanyard-one-end-30mm-attachment", "top_left_clip-one-end-30mm-attachment", "top_right_clip-one-end-30mm-attachment");
+        this.applyClassToManualGroup("left-super-lanyard-one-end-30mm-attachment-manual", "right-super-lanyard-one-end-30mm-attachment-manual", "center-super-lanyard-one-end-30mm-attachment-manual");
         break;
       default:
         console.warn("Unrecognised width in 'with-attachment' mode:", width);
     }
 
-
-    console.log(`Styles applied: with-attachment - ${width} `);
+    console.log(`Styles applied: with-attachment - ${width}`);
   }
+
 
   // Apply styles for the "two-end" case based on width
   applyTwoEndStyles(width) {
-
-    // Add appropriate classes for both left and right elements based on width
     switch (width) {
       case "10mm":
         this.applyClassToGroup("left-super-lanyard-two-end-10mm", "right-super-lanyard-two-end-10mm", "", "top_left_clip-two-end-10mm", "top_right_clip-two-end-10mm");
+        this.applyClassToManualGroup("left-super-lanyard-two-end-10mm-manual", "right-super-lanyard-two-end-10mm-manual", "center-super-lanyard-two-end-10mm-manual");
         break;
       case "15mm":
         this.applyClassToGroup("left-super-lanyard-two-end-15mm", "right-super-lanyard-two-end-15mm", "", "top_left_clip-two-end-15mm", "top_right_clip-two-end-15mm");
+        this.applyClassToManualGroup("left-super-lanyard-two-end-15mm-manual", "right-super-lanyard-two-end-15mm-manual", "center-super-lanyard-two-end-15mm-manual");
         break;
       case "20mm":
         this.applyClassToGroup("left-super-lanyard-two-end-20mm", "right-super-lanyard-two-end-20mm", "", "top_left_clip-two-end-20mm", "top_right_clip-two-end-20mm");
+        this.applyClassToManualGroup("left-super-lanyard-two-end-20mm-manual", "right-super-lanyard-two-end-20mm-manual", "center-super-lanyard-two-end-20mm-manual");
         break;
       case "25mm":
         this.applyClassToGroup("left-super-lanyard-two-end-25mm", "right-super-lanyard-two-end-25mm", "", "top_left_clip-two-end-25mm", "top_right_clip-two-end-25mm");
+        this.applyClassToManualGroup("left-super-lanyard-two-end-25mm-manual", "right-super-lanyard-two-end-25mm-manual", "center-super-lanyard-two-end-25mm-manual");
         break;
       case "30mm":
         this.applyClassToGroup("left-super-lanyard-two-end-30mm", "right-super-lanyard-two-end-30mm", "", "top_left_clip-two-end-30mm", "top_right_clip-two-end-30mm");
+        this.applyClassToManualGroup("left-super-lanyard-two-end-30mm-manual", "right-super-lanyard-two-end-30mm-manual", "center-super-lanyard-two-end-30mm-manual");
         break;
       default:
         console.warn("Unrecognised width in 'two-end' mode:", width);
@@ -162,6 +178,7 @@ class PreviewTemplate {
 
     console.log(`Styles applied: two-end - ${width}`);
   }
+
 
   // Helper function to add CSS classes to left, right, and centre elements
   applyClassToGroup(leftClass = "", rightClass = "", centerClass = "", topLeftClip = "", topRightClip = "") {
@@ -174,5 +191,14 @@ class PreviewTemplate {
     if (topLeftClip) el.top_left_clip.classList.add(topLeftClip);
     if (topRightClip) el.top_right_clip.classList.add(topRightClip);
   }
+  applyClassToManualGroup(leftClass = "", rightClass = "", centerClass = "") {
+    const el = this.manualElements;
+
+    // Add classes to left, right, and center if provided
+    if (leftClass) el.left.classList.add(leftClass);
+    if (rightClass) el.right.classList.add(rightClass);
+    if (centerClass) el.center.classList.add(centerClass);
+  }
+
 }
 const previewTemplate = new PreviewTemplate();
