cdd134fa34528ae67c5705ba0adfa602139c0586
Aleina Rossui <admin@promoflow.net>
2025-04-23 12:07:49 -0700

    “…”

--- views/assets/js/customize-lanyard/sections2/preview-template.js
+++ views/assets/js/customize-lanyard/sections2/preview-template.js
@@ -194,10 +194,10 @@ class PreviewTemplate {
   applyClassToManualGroup(leftClass = "", rightClass = "", centerClass = "") {
     const el = this.manualElements;
 
-    // Add classes to left, right, and center if provided
-    if (leftClass) el.left_manual.classList.add(leftClass);
-    if (rightClass) el.right_manual.classList.add(rightClass);
-    if (centerClass) el.center_manual.classList.add(centerClass);
+
+    if (leftClass && el.left_manual) el.left_manual.classList.add(leftClass);
+    if (rightClass && el.right_manual) el.right_manual.classList.add(rightClass);
+    if (centerClass && el.center_manual) el.center_manual.classList.add(centerClass);
   }
 
 }
