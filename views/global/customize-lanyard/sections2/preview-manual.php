745b5458673d1b22b0cfdf1e78f5b93eb2342a0b
Aleina Rossui <admin@promoflow.net>
2025-04-23 17:14:02 -0700

    “…”

--- views/global/customize-lanyard/sections2/preview-manual.php
+++ views/global/customize-lanyard/sections2/preview-manual.php
@@ -17,8 +17,9 @@ function get_preview_manual_css_version() {
   <div class="super-lanyard-manual" id="super-lanyard-manual">
     <div class="" id="left-super-lanyard-manual">
       <div class="text-lanyard">
-        <h1>hola</h1>
-        <h1>hola</h1>
+        <h1>hola1</h1>
+        <h1>hola2</h1>
+        <h1>hola3</h1>
       </div>
     </div>
     <div class="" id="right-super-lanyard-manual"></div>
