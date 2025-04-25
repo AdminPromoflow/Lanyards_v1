f0ebf4d9dbc257d6acb9a4510ed1fbd4c4ea5d4f
Aleina Rossui <admin@promoflow.net>
2025-04-24 19:02:30 -0700

    “…”

--- views/assets/js/customize-lanyard/sections/background.js
+++ views/assets/js/customize-lanyard/sections/background.js
@@ -7,11 +7,61 @@ class BackgroundClass {
 
     containers_boxes_background[0].style.display = "block";
 
-
     for (let i = 0; i < option_background_colour.length; i++) {
       option_background_colour[i].addEventListener("click", function(){
         backgroundClass.activateContainerBackground(i);
       })
+    }
+
+
+    const pantoneColors = [
+     { pantone: "White", html: "#FFFFFF" },
+     { pantone: "Black", html: "#2D2926" },
+     { pantone: "425", html: "#585C5F" },
+     { pantone: "Cool Gray 6", html: "#A8AAAB" },
+     { pantone: "Yellow", html: "#FCE300" },
+     { pantone: "809", html: "#E4E400" },
+     { pantone: "1235", html: "#FFBF29" },
+     { pantone: "1355", html: "#FFCA84" },
+     { pantone: "142", html: "#F9C26D" },
+     { pantone: "Orange 021", html: "#FE5000" },
+     { pantone: "Bright Orange", html: "#FF7F2A" },
+     { pantone: "485", html: "#DA291C" },
+     { pantone: "1795", html: "#D93600" },
+     { pantone: "186", html: "#E4002B" },
+     { pantone: "Red 032", html: "#EF3340" },
+     { pantone: "201", html: "#920021" },
+     { pantone: "193", html: "#BF003D" },
+     { pantone: "208", html: "#930032" },
+     { pantone: "212", html: "#FF5C8A" },
+     { pantone: "226", html: "#FF006E" },
+     { pantone: "249", html: "#80004F" },
+     { pantone: "252", html: "#FF66CC" },
+     { pantone: "Purple", html: "#BB29BB" },
+     { pantone: "2607", html: "#8700BF" },
+     { pantone: "281", html: "#002147" },
+     { pantone: "Reflex Blue", html: "#001489" },
+     { pantone: "293", html: "#0033A0" },
+     { pantone: "289", html: "#001B3A" },
+     { pantone: "3015", html: "#005387" },
+     { pantone: "Process Blue", html: "#0084D1" },
+     { pantone: "3302", html: "#005258" },
+     { pantone: "3278", html: "#008C93" },
+     { pantone: "348", html: "#00A370" },
+     { pantone: "354", html: "#00B35A" },
+     { pantone: "361", html: "#00BF26" },
+     { pantone: "7481", html: "#1AB87A" },
+     { pantone: "382", html: "#AEDD00" },
+     { pantone: "478", html: "#6B3300" },
+     { pantone: "471", html: "#E6CF6B" }
+ ];
+ 
+    for (let i = 0; i < pantoneColors.length; i++) {
+      containerBoxesBackground.innerHTML +=
+          '<div class="pantoneColours" style="background-color:' + pantoneColors[i].html + ';" ' +
+          'onclick="backgroundClass.handleClickBackgroundColour(\'' + pantoneColors[i].html + '\', ' + i + ')">' +
+          '<h3>' + pantoneColors[i].pantone + '</h3>' +
+          '</div>';
     }
 //option.selected = true;
 
@@ -117,21 +167,29 @@ class BackgroundClass {
     }*/
 
   }
-  activateContainerBackground(container){
-    const containers_boxes_background = document.querySelectorAll(".containers_boxes_background");
 
-    var index;
 
-    for (var i = 0; i < containers_boxes_background.length; i++) {
-      containers_boxes_background[i].style.display = "none";
-      if (container == i) {
-        index = i;
-      }
+
+  /**
+   * Displays the background container at the specified index and hides all others.
+   *
+   * @param {number} containerIndex - The index of the background container to be displayed.
+   */
+   activateContainerBackground(containerIndex) {
+    // Select all elements with the class 'containers_boxes_background'.
+    const backgroundContainers = document.querySelectorAll(".containers_boxes_background");
+
+    // Loop through all containers and hide them.
+    for (let i = 0; i < backgroundContainers.length; i++) {
+      backgroundContainers[i].style.display = "none";
     }
 
-    containers_boxes_background[index].style.display = "block";
-
+    // If the specified index is valid, display the corresponding container.
+    if (backgroundContainers[containerIndex]) {
+      backgroundContainers[containerIndex].style.display = "block";
+    }
   }
+
 
   // Set the first selected background color.
   setBackgroundColourSelectedFirstOption(value) {
