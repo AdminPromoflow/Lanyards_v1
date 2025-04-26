4936b763bce40fffaedcef72190e3538d77d309e
Aleina Rossui <admin@promoflow.net>
2025-04-26 13:14:14 -0700

    “…”

--- views/global/customize-lanyard/sections2/preview-manual.php
+++ views/global/customize-lanyard/sections2/preview-manual.php
@@ -12,14 +12,14 @@ function get_file_version($relative_path) {
   <div class="super-lanyard-manual" id="super-lanyard-manual">
     <div class="" id="left-super-lanyard-manual">
       <div class="text_lanyard_left" id="text_lanyard_left">
-
       </div>
     </div>
     <div class="" id="right-super-lanyard-manual">
       <div class="text_lanyard_right" id="text_lanyard_right">
       </div>
     </div>
-    <div class="" id="center-super-lanyard-manual"></div>
+    <div class="" id="center-super-lanyard-manual">
+    </div>
   </div>
 </section>
 
