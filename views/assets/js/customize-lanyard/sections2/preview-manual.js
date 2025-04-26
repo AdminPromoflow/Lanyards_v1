cde043da8c6d2dc57cd3d818778fdf63710518d3
Aleina Rossui <admin@promoflow.net>
2025-04-26 09:49:20 -0700

    “…”

--- views/assets/js/customize-lanyard/sections2/preview-manual.js
+++ views/assets/js/customize-lanyard/sections2/preview-manual.js
@@ -3,18 +3,20 @@ class PreviewManual {
     this.text_lanyard = document.querySelectorAll(".text-lanyard");
   }
   addTextToLanyard() {
-    this.text_lanyard = document.querySelectorAll(".text-lanyard");
-    var textLanyard = textClass.getContentText();
-    var times = 3; //getTimesText(); // obtenemos el valor desde la función get
+      this.text_lanyard = document.querySelectorAll(".text-lanyard");
+      var textLanyard = textClass.getContentText();
+      var times = 3; // o usar getTimesText()
 
-    this.text_lanyard.forEach(el => {
-        el.innerHTML = Array(times).fill(`
-            <div class="wrap_ex">
-                <h1>${textLanyard}</h1>
-            </div>
-        `).join('');
-    });
+      this.text_lanyard.forEach(el => {
+          el.innerHTML = ""; // 🔹 limpiar contenido previo
+          el.innerHTML = Array(times).fill(`
+              <div class="wrap_ex">
+                  <h1>${textLanyard}</h1>
+              </div>
+          `).join('');
+      });
   }
+
 
 
 
