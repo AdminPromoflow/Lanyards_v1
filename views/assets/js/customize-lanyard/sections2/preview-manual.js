467e7c195c8237fe1b5c677de3e6e98979243fdd
Aleina Rossui <admin@promoflow.net>
2025-04-27 14:43:32 -0700

    “…”

--- views/assets/js/customize-lanyard/sections2/preview-manual.js
+++ views/assets/js/customize-lanyard/sections2/preview-manual.js
@@ -49,13 +49,13 @@ class PreviewManual {
     this.text_lanyard_right.style.gap = textClass.getSpaceBetweenText() + "px";
     const wrap_text_0 = document.querySelectorAll(".wrap_text_0");
 
-  //  alert(wrap_text_0[0].offsetHeight + "  " + wrap_text_0[0].getBoundingClientRect().width);
+    alert(wrap_text_0[0].offsetHeight + "  " + wrap_text_0[0].offsetWidth);
 
     for (var i = 0; i < wrap_text_0.length; i++) {
       wrap_text_0[i].style.height = wrap_text_0[i].getBoundingClientRect().width;
     }
 
-    alert(wrap_text_0[0].offsetHeight + "  " + wrap_text_0[0].offsetWidth);
+    //alert(wrap_text_0[0].offsetHeight + "  " + wrap_text_0[0].offsetWidth);
 
   }
 
