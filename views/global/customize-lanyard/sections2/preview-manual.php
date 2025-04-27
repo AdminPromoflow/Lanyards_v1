4835af847b4f41a71067b160c7cf7f7b1fd4dd70
Aleina Rossui <admin@promoflow.net>
2025-04-27 13:24:42 -0700

    “…”

--- views/global/customize-lanyard/sections2/preview-manual.php
+++ views/global/customize-lanyard/sections2/preview-manual.php
@@ -12,12 +12,31 @@ function get_file_version($relative_path) {
   <div class="super-lanyard-manual" id="super-lanyard-manual">
     <div class="" id="left-super-lanyard-manual">
       <div class="text_lanyard_left" id="text_lanyard_left">
-
+        <div class="wrap_img_0 1">
+          <img src="../../views/assets/img/Test/arrow2.png" alt="">
+        </div>
+        <div class="wrap_img_0 2">
+          <img src="../../views/assets/img/Test/arrow2.png" alt="">
+        </div>
+        <div class="wrap_img_0 3">
+          <img src="../../views/assets/img/Test/arrow2.png" alt="">
+        </div>
+        <div class="wrap_img_0 4">
+          <img src="../../views/assets/img/Test/arrow2.png" alt="">
+        </div>
+        <div class="wrap_img_0 5">
+          <img src="../../views/assets/img/Test/arrow2.png" alt="">
+        </div>
       </div>
     </div>
     <div class="" id="right-super-lanyard-manual">
       <div class="text_lanyard_right" id="text_lanyard_right">
-
+        <div class="wrap_img_1">
+          <img src="../../views/assets/img/Test/arrow2.png" alt="">
+        </div>
+        <div class="wrap_img_1">
+          <img src="../../views/assets/img/Test/arrow2.png" alt="">
+        </div>
 
       </div>
     </div>
