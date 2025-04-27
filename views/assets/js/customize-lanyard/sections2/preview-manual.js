1567c17161a3fb49a67f21adc617921c9f9df27d
Aleina Rossui <admin@promoflow.net>
2025-04-26 18:55:29 -0700

    “…”

--- views/assets/js/customize-lanyard/sections2/preview-manual.js
+++ views/assets/js/customize-lanyard/sections2/preview-manual.js
@@ -40,7 +40,7 @@ class PreviewManual {
     const wrapElement = document.querySelectorAll('.wrap_ex_0');
 
     for (var i = 0; i < wrapElement.length; i++) {
-      alert(text_lanyard_right.offsetWidth/2 + "  " + -wrapElement[i].offsetHeight / 2);
+      //alert(text_lanyard_right.offsetWidth/2 + "  " + -wrapElement[i].offsetHeight / 2);
       // Obtener el ancho de text_lanyard_right
       const rightWidth = text_lanyard_right.offsetWidth;
 
@@ -48,7 +48,7 @@ class PreviewManual {
       wrapElement[i].style.left = `${rightWidth / 2}px`; // Convertir a px para unidades correctas
 
       // Calcular y establecer la propiedad 'transform' para centrar el elemento
-      wrapElement[i].style.transform = `rotate(90deg)  translateX(0px, calc(-100% + ${-wrapElement[i].offsetHeight / 2}))`; // Ajustar la posición
+      wrapElement[i].style.transform = `rotate(90deg)  translateX(0px, calc(-100% + ${wrapElement[i].offsetHeight / 2}px))`; // Ajustar la posición
     }
 //rotate(90deg) translateY(-100%)
 
