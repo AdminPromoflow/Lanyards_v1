7faf7441c2b148cc873df743ea8a1927fe2e8fb3
Aleina Rossui <admin@promoflow.net>
2025-04-29 07:13:33 -0700

    “…”

--- views/global/customize-lanyard/sections/text.php
+++ views/global/customize-lanyard/sections/text.php
@@ -29,6 +29,17 @@ $imgPath_text = "../../views/assets/img/global/customize-lanyard/sections/text/"
       <div class="item-text">
         <p>Adjust the spacing between the text</p>
         <div class="containr-space-between-text">
+          <div class="space-between-text-box" id="move_down_text">
+            <img src="<?= $imgPath_text ?>minus.png?v=<?= asset_version_text($imgPath_text . 'bottom.png') ?>" alt="">
+          </div>
+          <div class="space-between-text-box" id="move_up_text">
+            <img src="<?= $imgPath_text ?>plus.png?v=<?= asset_version_text($imgPath_text . 'top.png') ?>" alt="">
+          </div>
+        </div>
+      </div>
+      <div class="item-text">
+        <p>Adjust the spacing between the text</p>
+        <div class="containr-space-between-text">
           <div class="space-between-text-box" id="decrease_space_between_text">
             <img src="<?= $imgPath_text ?>minus.png?v=<?= asset_version_text($imgPath_text . 'minus.png') ?>" alt="">
           </div>
