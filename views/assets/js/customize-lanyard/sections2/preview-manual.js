6ea490931ecd816ef58c321cf4308bfc89e5df3e
Aleina Rossui <admin@promoflow.net>
2025-04-29 07:43:54 -0700

    “…”

--- views/assets/js/customize-lanyard/sections2/preview-manual.js
+++ views/assets/js/customize-lanyard/sections2/preview-manual.js
@@ -88,8 +88,8 @@ class PreviewManual {
       padding_text_top[i].style.background = backgroundClass.getBackground() ;
       padding_text_bottom[i].style.background = backgroundClass.getBackground();
 
-      padding_text_top[i].style.top =  -textClass.getTextPosition()  +"%" ;
-      padding_text_bottom[i].style.bottom = -textClass.getTextPosition()  +"%" ;
+      padding_text_top[i].style.top =  textClass.getTextPosition()  +"%" ;
+      padding_text_bottom[i].style.bottom = textClass.getTextPosition()  +"%" ;
 
     }
   }
