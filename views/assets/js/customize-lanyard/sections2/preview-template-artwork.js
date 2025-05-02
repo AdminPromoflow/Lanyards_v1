bff9693d505f10f0b5bf1858595a6e196546fc1d
Aleina Rossui <admin@promoflow.net>
2025-05-02 14:44:22 -0700

    “…”

--- views/assets/js/customize-lanyard/sections2/preview-template-artwork.js
+++ views/assets/js/customize-lanyard/sections2/preview-template-artwork.js
@@ -14,14 +14,18 @@ class PreviewTemplateArtworkClass {
 
     leftSuperLanyardTemplateArtwork.innerHTML = "";
     rightSuperLanyardTemplateArtwork.innerHTML = "";
-
-
-    if (side == "left") {
-       leftSuperLanyardTemplateArtwork.innerHTML = '<img src="' + image.src + '" alt="">';
+    if (sizeAproval) {
+      if (side == "left") {
+         leftSuperLanyardTemplateArtwork.innerHTML = '<img src="' + image.src + '" alt="">';
+      }
+      else if (side == "right") {
+        rightSuperLanyardTemplateArtwork.innerHTML = '<img src="' + image.src + '" alt="">';
+      }
     }
-    else if (side == "right") {
-      rightSuperLanyardTemplateArtwork.innerHTML = '<img src="' + image.src + '" alt="">';
+    else {
+      alert("Please re-upload the image.");
     }
+
   }
 
   confirmImageMessures(image){
@@ -29,7 +33,7 @@ class PreviewTemplateArtworkClass {
     const widthSelected = widthClass.getWidthSelectedJ();
 
     if (widthSelected == "10mm") {
-      if (image.height == 42520 && img.width == 945) {
+      if (image.height == 42520 && image.width == 945) {
         var sizeAproval = true;
       }
       else {
@@ -37,38 +41,38 @@ class PreviewTemplateArtworkClass {
       }
     }
     else if (widthSelected == "15mm") {
-      if (image.height == 42520 && img.width == 1417) {
+      if (image.height == 42520 && image.width == 1417) {
       var sizeAproval = true;
       }
       else {
-        alert("The lanyard artwork image must measure 42,520 pixels high by 945 pixels wide.");
+        alert("The lanyard artwork image must measure 42,520 pixels high by 1417 pixels wide.");
       }
     }
 
     else if (widthSelected == "20mm") {
-      if (image.height == 42520 && img.width == 1890) {
+      if (image.height == 42520 && image.width == 1890) {
       var sizeAproval = true;
       }
       else {
-        alert("The lanyard artwork image must measure 42,520 pixels high by 945 pixels wide.");
+        alert("The lanyard artwork image must measure 42,520 pixels high by 1890 pixels wide.");
       }
     }
 
     else if (widthSelected == "25mm") {
-      if (image.height == 42520 && img.width == 2362) {
+      if (image.height == 42520 && image.width == 2362) {
       var sizeAproval = true;
       }
       else {
-        alert("The lanyard artwork image must measure 42,520 pixels high by 945 pixels wide.");
+        alert("The lanyard artwork image must measure 42,520 pixels high by 2362 pixels wide.");
       }
     }
 
     else if (widthSelected == "30mm") {
-      if (image.height == 42520 && img.width == 2835) {
+      if (image.height == 42520 && image.width == 2835) {
       var sizeAproval = true;
       }
       else {
-        alert("The lanyard artwork image must measure 42,520 pixels high by 945 pixels wide.");
+        alert("The lanyard artwork image must measure 42,520 pixels high by 2835 pixels wide.");
       }
     }
 
