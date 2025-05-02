0cef99403ea6cfeb84fc702714dd73eade32cef8
Aleina Rossui <admin@promoflow.net>
2025-05-01 22:23:50 -0700

    “…”

--- views/assets/js/customize-lanyard/sections2/preview-template-artwork.js
+++ views/assets/js/customize-lanyard/sections2/preview-template-artwork.js
@@ -1,13 +1,15 @@
 class PreviewTemplateArtworkClass {
   constructor() {
+    previewTemplateArtworkSection.style.display = "none";
 
   }
   togglePreviewTemplateArtworkClass(action) {
-      const previewTemplateArtworkSection = document.getElementById("preview-template-artwork-section");
       previewTemplateArtworkSection.style.display = action;
       alert(JSON.stringify(previewTemplateArtworkSection.id + action));
 
 
   }
 }
+const previewTemplateArtworkSection = document.getElementById("preview-template-artwork-section");
+
 const previewTemplateArtworkClass = new PreviewTemplateArtworkClass();
