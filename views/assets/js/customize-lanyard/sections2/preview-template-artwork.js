85376a7779190af3933862789206f4bf0da30add
Aleina Rossui <admin@promoflow.net>
2025-05-02 14:48:25 -0700

    “…”

--- views/assets/js/customize-lanyard/sections2/preview-template-artwork.js
+++ views/assets/js/customize-lanyard/sections2/preview-template-artwork.js
@@ -31,7 +31,7 @@ class PreviewTemplateArtworkClass {
   confirmImageMessures(image){
     var sizeAproval = false;
     const widthSelected = widthClass.getWidthSelected();
-    alert(image.height + "  " + image.widt);
+    alert(image.height + "  " + image.width);
 
     if (widthSelected == "10mm") {
       if (image.height == 42520 && image.width == 945) {
