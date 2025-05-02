25077bc1e4d18d1ab483c58628725cd3e3e300ee
Aleina Rossui <admin@promoflow.net>
2025-05-01 22:14:59 -0700

    “…”

--- views/global/customize-lanyard/customize-lanyard.php
+++ views/global/customize-lanyard/customize-lanyard.php
@@ -41,9 +41,9 @@ $customize_lanyard_js = "../../views/assets/js/customize-lanyard/app.js";
       <?php include "../../views/global/customize-lanyard/sections2/preview-login.php"; ?>
       <?php include "../../views/global/customize-lanyard/sections2/preview-provided_information.php"; ?>
       <?php include "../../views/global/customize-lanyard/sections2/preview-checkout.php"; ?>
+      <?php include "../../views/global/customize-lanyard/sections2/preview-template-artwork.php" ?>
       <?php include "../../views/global/customize-lanyard/sections2/preview-template.php" ?>
       <?php include "../../views/global/customize-lanyard/sections2/preview-manual.php" ?>
-      <?php include "../../views/global/customize-lanyard/sections2/preview-template-artwork.php" ?>
     </div>
 
     <div id="options-customize-lanyard" class="options-customize-lanyard">
