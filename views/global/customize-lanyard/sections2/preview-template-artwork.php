e4fdcd71e063cf5cb0d7676a0d35e31c43171e5c
Aleina Rossui <admin@promoflow.net>
2025-05-01 21:34:06 -0700

    “…”

---
+++ views/global/customize-lanyard/sections2/preview-template-artwork.php
@@ -0,0 +1,6 @@
+<?php
+function get_file_version($relative_path) {
+    $absolute_path = __DIR__ . '/' . $relative_path;
+    return file_exists($absolute_path) ? filemtime($absolute_path) : time();
+}
+?>
