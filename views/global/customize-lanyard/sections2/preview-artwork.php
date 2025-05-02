e8fb7aa83367663417751c3db2d7576fa1f58ff6
Aleina Rossui <admin@promoflow.net>
2025-05-01 19:02:03 -0700

    “…”

--- views/global/customize-lanyard/sections2/preview-artwork.php
+++ views/global/customize-lanyard/sections2/preview-artwork.php
@@ -4,11 +4,10 @@ function get_file_version($relative_path) {
     return file_exists($absolute_path) ? filemtime($absolute_path) : time();
 }
 ?>
-
+<!--
 <link rel="stylesheet" href="../../views/assets/css/global/customize-lanyard/sections2/preview-artwork.css?v=<?php echo get_file_version('../../views/assets/css/global/customize-lanyard/sections2/preview-artwork.css'); ?>">
 
 <section id="preview-artwork-section">
-  <!-- artwork lanyard layout -->
   <div class="super-lanyard-artwork" id="super-lanyard-artwork">
 
     <div class="" id="left-super-lanyard-artwork">
@@ -22,7 +21,8 @@ function get_file_version($relative_path) {
     <div class="" id="center-super-lanyard-artwork">
 
     </div>
-    
+
 </section>
 
 <script src="../../views/assets/js/customize-lanyard/sections2/preview-artwork.js?v=<?php echo get_file_version('../../views/assets/js/customize-lanyard/sections2/preview-artwork.js'); ?>" type="text/javascript"></script>
+-->
