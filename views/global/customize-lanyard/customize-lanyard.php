f9480235983ff4be06526dcf947ae02368251759
Aleina Rossui <admin@promoflow.net>
2025-05-02 10:24:22 -0700

    “…”

--- views/global/customize-lanyard/customize-lanyard.php
+++ views/global/customize-lanyard/customize-lanyard.php
@@ -10,6 +10,13 @@ function get_customize_lanyard_file_version($filePath) {
 $customize_lanyard_css = "../assets/css/global/customize-lanyard/style.css";
 $customize_lanyard_js = "../../views/assets/js/customize-lanyard/app.js";
 ?>
+<?php
+header("Expires: Tue, 01 Jan 2000 00:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+?>
 
 <!-- Link CSS with versioning -->
 <link rel="stylesheet" href="<?php echo $customize_lanyard_css . '?v=' . get_customize_lanyard_file_version($customize_lanyard_css); ?>">
