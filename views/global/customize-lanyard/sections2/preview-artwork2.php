ea8fd952ccf6a3f41c6730e3ca891fe02b869c9d
Aleina Rossui <admin@promoflow.net>
2025-05-01 19:51:15 -0700

    “…”

---
+++ views/global/customize-lanyard/sections2/preview-artwork2.php
@@ -0,0 +1,42 @@
+<?php
+function get_file_version($relative_path) {
+    $absolute_path = __DIR__ . '/' . $relative_path;
+    return file_exists($absolute_path) ? filemtime($absolute_path) : time();
+}
+?>
+
+<link rel="stylesheet" href="../../views/assets/css/global/customize-lanyard/sections2/preview-artwork2.css?v=<?php echo get_file_version('../../views/assets/css/global/customize-lanyard/sections2/preview-artwork2.css'); ?>">
+
+<section id="preview-artwork2-section">
+  <!-- Artwork2 lanyard layout -->
+  <div class="super-lanyard-artwork2" id="super-lanyard-artwork2">
+
+    <div class="" id="left-super-lanyard-artwork2">
+
+      <div class="text_lanyard_left" id="text_lanyard_left">
+      </div>
+      <div class="img_lanyard_left" id="img_lanyard_left">
+      </div>
+
+    </div>
+
+    <div class="" id="right-super-lanyard-artwork2">
+
+      <div class="text_lanyard_right" id="text_lanyard_right">
+      </div>
+      <div class="img_lanyard_right" id="img_lanyard_right">
+      </div>
+
+  </div>
+
+  <div class="" id="center-super-lanyard-artwork2">
+
+    <div class="text_lanyard_center" id="text_lanyard_center">
+    </div>
+    <div class="img_lanyard_center" id="img_lanyard_center">
+    </div>
+
+  </div>
+</section>
+
+<script src="../../views/assets/js/customize-lanyard/sections2/preview-artwork2.js?v=<?php echo get_file_version('../../views/assets/js/customize-lanyard/sections2/preview-artwork2.js'); ?>" type="text/javascript"></script>
