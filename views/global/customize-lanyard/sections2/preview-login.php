4c5a3257d85d28f9f194f66044d77c899874e334
Aleina Rossui <admin@promoflow.net>
2025-05-03 15:11:19 -0700

    “…”

--- views/global/customize-lanyard/sections2/preview-login.php
+++ views/global/customize-lanyard/sections2/preview-login.php
@@ -1,9 +1,19 @@
-<link rel="stylesheet" href="../../views/assets/css/global/customize-lanyard/sections2/preview-login.css">
+<?php
+$cssPath = '../../views/assets/css/global/customize-lanyard/sections2/preview-login.css';
+$jsPath = '../../views/assets/js/customize-lanyard/sections2/preview-login.js';
+$imgPath = '../../views/assets/img/global/customize-lanyard/sections2/preview-login/avatars.png';
+
+$cssVersion = file_exists($cssPath) ? filemtime($cssPath) : time();
+$jsVersion = file_exists($jsPath) ? filemtime($jsPath) : time();
+$imgVersion = file_exists($imgPath) ? filemtime($imgPath) : time();
+?>
+
+<link rel="stylesheet" href="<?= $cssPath ?>?v=<?= $cssVersion ?>">
 
 <section id="preview-login-container" class="preview-login-container">
   <div class="preview-login-container-image">
-    <img id="preview-login-container-image" src="../../views/assets/img/global/customize-lanyard/sections2/preview-login/avatars.png" alt="">
+    <img id="preview-login-container-image" src="<?= $imgPath ?>?v=<?= $imgVersion ?>" alt="">
   </div>
 </section>
-<script src="../../views/assets/js/customize-lanyard/sections2/preview-login.js" type="text/javascript">
-</script>
+
+<script src="<?= $jsPath ?>?v=<?= $jsVersion ?>" type="text/javascript"></script>
