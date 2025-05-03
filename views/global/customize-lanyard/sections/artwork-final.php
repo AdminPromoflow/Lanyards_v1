47fc2175187e82dd8725c1a38f3fa9ada5c2ee65
Aleina Rossui <admin@promoflow.net>
2025-05-02 20:31:02 -0700

    “…”

--- views/global/customize-lanyard/sections/artwork-final.php
+++ views/global/customize-lanyard/sections/artwork-final.php
@@ -7,37 +7,37 @@
   <div id="containers_boxes_artwork-final" class="containers_boxes_artwork-final">
  <div class="container_boxes_artwork-final">
       <h4 id="artwork-final-material"></h4>
-      <h3 id="" class="buttom_artwork_final">Change</h3>
+
 
     </div>
     <div class="container_boxes_artwork-final">
       <h4 id="artwork-final-lanyard-type"></h4>
-      <h3 id="" class="buttom_artwork_final">Change</h3>
+
     </div>
     <div class="container_boxes_artwork-final">
       <h4 id="artwork-final-width"></h4>
-      <h3 id="" class="buttom_artwork_final">Change</h3>
+
     </div>
     <div class="container_boxes_artwork-final">
       <h4 id="artwork-final-side-printed"></h4>
-      <h3 id="" class="buttom_artwork_final">Change</h3>
+
     </div>
     <div class="container_boxes_artwork-final">
       <h4 id="artwork-final-clips"></h4>
-      <h3 id="" class="buttom_artwork_final">Change</h3>
+
     </div>
     <div class="container_boxes_artwork-final">
       <h4 id="artwork-final-attachment"></h4>
-      <h3 id="" class="buttom_artwork_final">Change</h3>
+
     </div>
     <div class="container_boxes_artwork-final">
       <h4 id="artwork-final-colour-quantity"></h4>
-      <h3 id="" class="buttom_artwork_final">Change</h3>
+
     </div>
     <div class="container_boxes_artwork-final">
 
       <h4 id="artwork-final-artwork-manual"></h4>
-      <h3 id="" class="buttom_artwork_final">Change</h3>
+
     </div>
 
   </div>
