77b5062e9924177f9bdc2e3b9b8917cfda3ef46b
Aleina Rossui <admin@promoflow.net>
2025-05-03 15:34:43 -0700

    “…”

--- views/global/customize-lanyard/sections/login.php
+++ views/global/customize-lanyard/sections/login.php
@@ -1,4 +1,19 @@
-<link rel="stylesheet" href="../../views/assets/css/global/customize-lanyard/sections/login.css">
+<?php
+$cssPath = '../../views/assets/css/global/customize-lanyard/sections/login.css';
+$jsPath = '../../views/assets/js/customize-lanyard/sections/login.js';
+$imgGoogle = '../../views/assets/img/global/login/google-icon.png';
+$imgFacebook = '../../views/assets/img/global/login/facebook-icon.png';
+$imgApple = '../../views/assets/img/global/login/apple-icon.png';
+
+$cssVersion = file_exists($cssPath) ? filemtime($cssPath) : time();
+$jsVersion = file_exists($jsPath) ? filemtime($jsPath) : time();
+$googleVersion = file_exists($imgGoogle) ? filemtime($imgGoogle) : time();
+$facebookVersion = file_exists($imgFacebook) ? filemtime($imgFacebook) : time();
+$appleVersion = file_exists($imgApple) ? filemtime($imgApple) : time();
+?>
+
+<link rel="stylesheet" href="<?= $cssPath ?>?v=<?= $cssVersion ?>">
+
 <section class="login2 section" id="login2">
 
   <h2 class="name-section-customize-lanyard">Login</h2>
@@ -7,28 +22,27 @@
     <label for="login2Email">Please enter your login details:</label>
     <input id="login2Email" type="text" name="" value="" placeholder="Please provide your email">
     <input id="login2Password" type="text" name="" value="" placeholder="and your password">
-    <button id="enterLogin2" type="button" name="button"><strong class="containers_boxes_login_button">Login</strong></button>
+    <button id="enterLogin2" type="button" name="button">
+      <strong class="containers_boxes_login_button">Login</strong>
+    </button>
 
     <h3>or login with:</h3>
     <div class="login2OptionsContainer">
       <div id="loginWithGoogle2" class="login2Options">
-        <img src="../../views/assets/img/global/login/google-icon.png" alt="">
+        <img src="<?= $imgGoogle ?>?v=<?= $googleVersion ?>" alt="">
       </div>
       <div id="loginWithFacebook2" class="login2Options">
-        <img src="../../views/assets/img/global/login/facebook-icon.png" alt="">
+        <img src="<?= $imgFacebook ?>?v=<?= $facebookVersion ?>" alt="">
       </div>
       <div id="loginWithApple2" class="login2Options">
-        <img src="../../views/assets/img/global/login/apple-icon.png" alt="">
+        <img src="<?= $imgApple ?>?v=<?= $appleVersion ?>" alt="">
       </div>
     </div>
     <p id="openRegister2">No account yet? Register here.</p>
-
   </div>
 
-
 </section>
-<?php include "../../views/global/customize-lanyard/sections/register.php" ?>
 
+<?php include "../../views/global/customize-lanyard/sections/register.php"; ?>
 
-
-<script src="../../views/assets/js/customize-lanyard/sections/login.js"></script>
+<script src="<?= $jsPath ?>?v=<?= $jsVersion ?>"></script>
