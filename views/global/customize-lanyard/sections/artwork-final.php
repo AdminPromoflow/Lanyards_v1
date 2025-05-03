61d4e20fec95b516312b89a4d6355e2efa83c7c7
Aleina Rossui <admin@promoflow.net>
2025-05-02 20:38:24 -0700

    “…”

--- views/global/customize-lanyard/sections/artwork-final.php
+++ views/global/customize-lanyard/sections/artwork-final.php
@@ -1,47 +1,34 @@
-<link rel="stylesheet" href="../../views/assets/css/global/customize-lanyard/sections/artwork-final.css">
+<link rel="stylesheet" href="../../views/assets/css/global/customize-lanyard/sections/artwork-final.css?v=<?= filemtime('../../views/assets/css/global/customize-lanyard/sections/artwork-final.css') ?>">
 <section class="artwork-final section">
   <h2 class="name-section-customize-lanyard">Final lanyard</h2>
   <br>
   <p>These are the options you’ve chosen for your lanyard. Click ‘Next’ or ‘Preview’ if you’d like to make any changes:</p>
   <br>
   <div id="containers_boxes_artwork-final" class="containers_boxes_artwork-final">
- <div class="container_boxes_artwork-final">
+    <div class="container_boxes_artwork-final">
       <h4 id="artwork-final-material"></h4>
-
-
     </div>
     <div class="container_boxes_artwork-final">
       <h4 id="artwork-final-lanyard-type"></h4>
-
     </div>
     <div class="container_boxes_artwork-final">
       <h4 id="artwork-final-width"></h4>
-
     </div>
     <div class="container_boxes_artwork-final">
       <h4 id="artwork-final-side-printed"></h4>
-
     </div>
     <div class="container_boxes_artwork-final">
       <h4 id="artwork-final-clips"></h4>
-
     </div>
     <div class="container_boxes_artwork-final">
       <h4 id="artwork-final-attachment"></h4>
-
     </div>
     <div class="container_boxes_artwork-final">
       <h4 id="artwork-final-colour-quantity"></h4>
-
     </div>
     <div class="container_boxes_artwork-final">
-
       <h4 id="artwork-final-artwork-manual"></h4>
-
     </div>
-
   </div>
-
-
 </section>
-<script src="../../views/assets/js/customize-lanyard/sections/artwork-final.js" type="text/javascript"></script>
+<script src="../../views/assets/js/customize-lanyard/sections/artwork-final.js?v=<?= filemtime('../../views/assets/js/customize-lanyard/sections/artwork-final.js') ?>" type="text/javascript"></script>
