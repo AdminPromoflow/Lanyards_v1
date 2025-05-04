09415efe20ad37f0f427e26e34ed089688db4ced
Aleina Rossui <admin@promoflow.net>
2025-05-04 12:54:36 -0700

    “…”

--- views/global/customize-lanyard/sections/login.php
+++ views/global/customize-lanyard/sections/login.php
@@ -29,7 +29,7 @@ $appleVersion = file_exists($imgApple) ? filemtime($imgApple) : time();
     </button>
 
     <h3>or login with:</h3>
-  <!--  <div class="login2OptionsContainer">
+    <div class="login2OptionsContainer">
     <!--  <div id="loginWithGoogle2" class="login2Options">
         <img src="<?= $imgGoogle ?>?v=<?= $googleVersion ?>" alt="">
       </div>-->
@@ -39,7 +39,7 @@ $appleVersion = file_exists($imgApple) ? filemtime($imgApple) : time();
       <div id="loginWithApple2" class="login2Options">
         <img src="<?= $imgApple ?>?v=<?= $appleVersion ?>" alt="">
       </div>-->
-    </div>-->
+    </div>
     <p id="openRegister2">No account yet? Register here.</p>
   </div>
 
