87e6536734df45b7d994e13681e344642db4cb3d
Aleina Rossui <admin@promoflow.net>
2025-05-07 01:09:06 -0700

    “…”

--- views/assets/js/customize-lanyard/sections/artwork-final.js
+++ views/assets/js/customize-lanyard/sections/artwork-final.js
@@ -3,7 +3,23 @@ class ArtworkFinal {
     this.updateItems();
   }
 updateItems(){
+  artworkFinalMaterial.innerHTML = "<strong>Material: </strong>&nbsp;&nbsp;&nbsp;" + material.getMaterialSelected();
+  artworkFinalLanyardLype.innerHTML = "<strong>Lanyard type: </strong>&nbsp;&nbsp;&nbsp;" + oneTwoEndsClass.getTypeLanyardSelected();
+  artworkFinalWidth.innerHTML = "<strong>Width: </strong>&nbsp;&nbsp;&nbsp;" + widthClass.getWidthSelected();
+  artworkFinalSidePrinted.innerHTML = "<strong>Side printed: </strong>&nbsp;&nbsp;&nbsp;" + sidePrintedClass.getSidePrintedSelected();
+  artworkFinalClips.innerHTML = "<strong>Clips: </strong>&nbsp;&nbsp;&nbsp;" + clipClass.getClipSelected();
+  artworkFinalAttachment.innerHTML = "<strong>Attachment: </strong>&nbsp;&nbsp;&nbsp;" + attachmentClass.getAttachmentSelected();
+  artworkFinalColourQuantity.innerHTML = "<strong>Colour Quantity: </strong>&nbsp;&nbsp;&nbsp;" + colourClass.getColourSelected();
+  artworkFinalArtworkManual.innerHTML = "<strong>Artwork - Manual: </strong>&nbsp;&nbsp;&nbsp;" + artworkManualClass.getArtworkManual();
 }
 }
+const artworkFinalMaterial = document.getElementById("artwork-final-material");
+const artworkFinalLanyardLype = document.getElementById("artwork-final-lanyard-type");
+const artworkFinalWidth = document.getElementById("artwork-final-width");
+const artworkFinalSidePrinted = document.getElementById("artwork-final-side-printed");
+const artworkFinalClips = document.getElementById("artwork-final-clips");
+const artworkFinalAttachment = document.getElementById("artwork-final-attachment");
+const artworkFinalColourQuantity = document.getElementById("artwork-final-colour-quantity");
+const artworkFinalArtworkManual = document.getElementById("artwork-final-artwork-manual");
 
 const artworkClassFinal = new ArtworkFinal();
--- views/global/customize-lanyard/sections/artwork-final.php
+++ views/global/customize-lanyard/sections/artwork-final.php
@@ -5,54 +5,29 @@
   <p>These are the options you’ve chosen for your lanyard. Click ‘Next’ or ‘Preview’ if you’d like to make any changes:</p>
   <br>
   <div id="containers_boxes_artwork-final" class="containers_boxes_artwork-final">
-
-
-    <div class="subcontainer_boxes_final_lanyard">
-      <h3 class=""><strong>Material</strong> </h3>
-      <h3 id="final_lanyard_material">Material </h3>
-      <h4 id="price_final_lanyard_material">+£0.3 per unit</h4>
+    <div class="container_boxes_artwork-final">
+      <h4 id="artwork-final-material"></h4>
     </div>
-
-    <div class="subcontainer_boxes_final_lanyard">
-      <h3 class=""><strong>Lanyard type</strong></h3>
-      <h3 id="final_lanyard_lanyard_type">Lanyard type</h3>
-      <h4 id="price_final_lanyard_lanyard_type">+£0.3 per unit</h4>
+    <div class="container_boxes_artwork-final">
+      <h4 id="artwork-final-lanyard-type"></h4>
     </div>
-
-    <div class="subcontainer_boxes_final_lanyard">
-      <h3 class="" ><strong>Width</strong></h3>
-      <h3 id="final_lanyard_width">Width</h3>
-      <h4 id="price_final_lanyard_width">+£0.25 per unit</h4>
+    <div class="container_boxes_artwork-final">
+      <h4 id="artwork-final-width"></h4>
     </div>
-
-    <div class="subcontainer_boxes_final_lanyard">
-      <h3 class=""><strong>Side Printed</strong></h3>
-      <h3 id="final_lanyard_side_printed">Side Printed</h3>
-      <h4 id="price_final_lanyard_side_printed">+£0.25 per unit</h4>
+    <div class="container_boxes_artwork-final">
+      <h4 id="artwork-final-side-printed"></h4>
     </div>
-
-    <div class="subcontainer_boxes_final_lanyard">
-      <h3 class=""><strong>Clips</strong></h3>
-      <h3 id="final_lanyard_clips">Clips</h3>
-      <h4 id="price_final_lanyard_clips">+£0.25 per unit</h4>
+    <div class="container_boxes_artwork-final">
+      <h4 id="artwork-final-clips"></h4>
     </div>
-
-    <div class="subcontainer_boxes_final_lanyard">
-      <h3 class=""><strong>Attachment</strong></h3>
-      <h3 id="final_lanyard_attachment">Attachment</h3>
-      <h4 id="price_final_lanyard_attachment">+£0.25 per unit</h4>
+    <div class="container_boxes_artwork-final">
+      <h4 id="artwork-final-attachment"></h4>
     </div>
-
-    <div class="subcontainer_boxes_final_lanyard">
-      <h3 class=""><strong>Colour quantity</strong></h3>
-      <h3 id="final_lanyard_colour_quantity">Colour quantity</h3>
-      <h4 id="price_final_lanyard_colour_quantity">+£0.25 per unit</h4>
+    <div class="container_boxes_artwork-final">
+      <h4 id="artwork-final-colour-quantity"></h4>
     </div>
-
-    <div class="subcontainer_boxes_final_lanyard">
-      <h3 class=""><strong>Accessories</strong></h3>
-      <h3 id="final_lanyard_accessories">Accessories</h3>
-      <h4 id="price_final_lanyard_accessories">+£0.25 per unit</h4>
+    <div class="container_boxes_artwork-final">
+      <h4 id="artwork-final-artwork-manual"></h4>
     </div>
   </div>
 </section>
