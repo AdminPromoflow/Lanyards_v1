27161ae8ba4153102f65b2227a209311627f6af2
Aleina Rossui <admin@promoflow.net>
2025-05-06 18:21:50 -0700

    “…”

--- views/global/customize-lanyard/sections2/preview-checkout.php
+++ views/global/customize-lanyard/sections2/preview-checkout.php
@@ -1,6 +1,7 @@
-  <link rel="stylesheet" href="../../views/assets/css/global/customize-lanyard/sections2/preview-checkout.css">
-  <section class="preview-checkout-container" id="preview-checkout-container" >
-    <h2>Summary</h2>
+<link rel="stylesheet" href="../../views/assets/css/global/customize-lanyard/sections2/preview-checkout.css?v=<?= filemtime('../../views/assets/css/global/customize-lanyard/sections2/preview-checkout.css') ?>">
 
-  </section>
-  <script src="../../views/assets/js/customize-lanyard/sections2/preview-checkout.js" type="text/javascript"></script>
+<section class="preview-checkout-container" id="preview-checkout-container">
+  <h2>Summary</h2>
+</section>
+
+<script src="../../views/assets/js/customize-lanyard/sections2/preview-checkout.js?v=<?= filemtime('../../views/assets/js/customize-lanyard/sections2/preview-checkout.js') ?>" type="text/javascript"></script>
