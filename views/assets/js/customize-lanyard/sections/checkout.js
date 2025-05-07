cbc66a0c7a31cb99b8c84c8f724c3d59b286ead4
Aleina Rossui <admin@promoflow.net>
2025-05-06 21:49:12 -0700

    “…”

--- views/assets/js/customize-lanyard/sections/checkout.js
+++ views/assets/js/customize-lanyard/sections/checkout.js
@@ -40,36 +40,29 @@ const subcontainerBoxes = document.getElementsByClassName('subcontainer_boxes_pr
 
 const arrowPreviewCheckout = document.getElementById('arrow_preview_checkout');
 
-// Material
-const material = document.getElementById('buy_cart_material');
-const priceMaterial = document.getElementById('price_buy_cart_material');
+const buy_cart_material = document.getElementById('buy_cart_material');
+const price_buy_cart_material = document.getElementById('price_buy_cart_material');
 
-// Lanyard type
-const lanyardType = document.getElementById('buy_cart_lanyard_type');
-const priceLanyardType = document.getElementById('price_buy_cart_lanyard_type');
+const buy_cart_lanyard_type = document.getElementById('buy_cart_lanyard_type');
+const price_buy_cart_lanyard_type = document.getElementById('price_buy_cart_lanyard_type');
 
-// Width
-const width = document.getElementById('buy_cart_width');
-const priceWidth = document.getElementById('price_buy_cart_width');
+const buy_cart_width = document.getElementById('buy_cart_width');
+const price_buy_cart_width = document.getElementById('price_buy_cart_width');
 
-// Side Printed
-const sidePrinted = document.getElementById('buy_cart_side_printed');
-const priceSidePrinted = document.getElementById('price_buy_cart_side_printed');
+const buy_cart_side_printed = document.getElementById('buy_cart_side_printed');
+const price_buy_cart_side_printed = document.getElementById('price_buy_cart_side_printed');
 
-// Clips
-const clips = document.getElementById('buy_cart_clips');
-const priceClips = document.getElementById('price_buy_cart_clips');
+const buy_cart_clips = document.getElementById('buy_cart_clips');
+const price_buy_cart_clips = document.getElementById('price_buy_cart_clips');
 
-// Attachment
-const attachment = document.getElementById('buy_cart_attachment');
-const priceAttachment = document.getElementById('price_buy_cart_attachment');
+const buy_cart_attachment = document.getElementById('buy_cart_attachment');
+const price_buy_cart_attachment = document.getElementById('price_buy_cart_attachment');
 
-// Accessories
-const accessories = document.getElementById('buy_cart_accessories');
-const priceAccessories = document.getElementById('price_buy_cart_accessories');
+const buy_cart_accessories = document.getElementById('buy_cart_accessories');
+const price_buy_cart_accessories = document.getElementById('price_buy_cart_accessories');
 
-// Colour quantity
-const colourQuantity = document.getElementById('buy_cart_colour_quantity');
-const priceColourQuantity = document.getElementById('price_buy_cart_colour_quantity');
+const buy_cart_colour_quantity = document.getElementById('buy_cart_colour_quantity');
+const price_buy_cart_colour_quantity = document.getElementById('price_buy_cart_colour_quantity');
+
 
 const checkout = new Checkout();
