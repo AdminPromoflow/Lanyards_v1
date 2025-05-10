2e839f219b8dcb56bfa3e23c3ee196b018eab244
Aleina Rossui <admin@promoflow.net>
2025-05-10 14:08:02 -0700

    “…”

--- models/orders.php
+++ models/orders.php
@@ -59,7 +59,7 @@ class Order_Model {
             $sql = $this->connection->getConnection()->prepare("INSERT INTO `Orders` (`idUser`, `order_date`, `status`, `total`)
                 VALUES (:idUser, :order_date, :status, :total)
             ");
-            echo json_encode($this->status);exit;
+            echo json_encode($this->total);exit;
 
 
             $sql->bindParam(':idUser', $this->idUser, PDO::PARAM_INT);
