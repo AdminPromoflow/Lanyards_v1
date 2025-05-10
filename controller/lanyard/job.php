896de368b7ea4d4efe4571af0b8738503ff2a97f
Aleina Rossui <admin@promoflow.net>
2025-05-10 13:48:58 -0700

    “…”

--- controller/lanyard/job.php
+++ controller/lanyard/job.php
@@ -90,10 +90,10 @@ class Job {
             session_start();
         }
 
-
+        echo json_encode($_SESSION['orden_in_process']);
+        exit;
         if (!isset($_SESSION['orden_in_process'])) {
-          echo json_encode("Vamos lograndolo 3");
-          exit;
+
             $connection = new Database();
             $order_model = new Order_Model($connection);
             $order_model->setEmail($_SESSION['email']);
