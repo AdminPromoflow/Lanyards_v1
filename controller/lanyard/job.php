6b036b494a700f3516f0593966ce6108554742b4
Aleina Rossui <admin@promoflow.net>
2025-05-10 10:38:36 -0700

    “…”

--- controller/lanyard/job.php
+++ controller/lanyard/job.php
@@ -57,7 +57,7 @@ class Job {
         // Recuperar el idOrder desde la sesión
         $job_model->setIdOrder($_SESSION['orden_in_process']);
 
-        $amount_Models = new Amount_Models();
+        $amount_Models = new AmountModel();
         echo json_encode("hola tu");exit;
 
         $amount_Models->setDescription($data->description);
