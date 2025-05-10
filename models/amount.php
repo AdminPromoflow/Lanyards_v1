480b8aeb3b1552ce02f13a0b4cd308e005518ffa
Aleina Rossui <admin@promoflow.net>
2025-05-10 09:01:33 -0700

    “…”

--- models/amount.php
+++ models/amount.php
@@ -157,10 +157,6 @@ class Amount_Models {
         throw new Exception("Error in the user verification query.");
     }
   }
-
-  public getIdAmount(){
-
-  }
     /*
    * Get a list of all Materia's lanyards from the database.
    *
--- views/assets/js/customize-lanyard/sections/checkout.js
+++ views/assets/js/customize-lanyard/sections/checkout.js
@@ -105,7 +105,7 @@ class Checkout {
     const data = {
       action: "createJob",
       product: this.getProduct(),
-      description: JSON.stringify(this.getDescription()),
+      description: this.getDescription(),
       price_per_unit: this.getPricePerUnit(),
       amount: this.getAmount(),
       total: this.getTotal()
