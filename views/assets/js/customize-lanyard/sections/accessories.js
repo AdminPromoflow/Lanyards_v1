181c7f121c7ad9ffbecbeb3cb52ee4f7136b0cb6
Aleina Rossui <admin@promoflow.net>
2025-05-11 00:12:28 -0700

    “…”

--- views/assets/js/customize-lanyard/sections/accessories.js
+++ views/assets/js/customize-lanyard/sections/accessories.js
@@ -70,12 +70,12 @@ class Accessories {
 
   }
   selectAccessory(i){
+    alert(i);
 
     const text = priceDataAccessories[i].textContent;
     const number = parseFloat(text.match(/[-+]?[0-9]*\.?[0-9]+/)[0]);
     priceClass.setPriceAccessory(number);
     priceClass.changePricePerLanyard();
-    alert(text);
 
     // Restablece el borde de todos los elementos a transparente
     for (let j = 0; j < subContainerBoxesAccessories.length; j++) {
