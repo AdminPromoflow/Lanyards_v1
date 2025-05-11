9dae99e92ef2ebe0ab27e9e02747413e0efa23a5
Aleina Rossui <admin@promoflow.net>
2025-05-11 09:07:25 -0700

    “…”

--- views/global/customize-lanyard/sections/accessories.php
+++ views/global/customize-lanyard/sections/accessories.php
@@ -35,31 +35,30 @@ $jsVersion  = file_exists($jsPath)  ? filemtime($jsPath)  : time();
 
       </div>
     </div>
+
+
     <div class="subcontainer_boxes_accessories">
       <h3 class="dataAccessories">None </h3>
       <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/none.png" alt="">
       <h4 class="priceDataAccessories">+£0 per unit</h4>
     </div>
-
-    <div class="subcontainers_boxes_accessories">
-
-
-      <div class="subcontainer_boxes_accessories">
-        <h3 class="dataAccessories">Clear Plastic (closed face) </h3>
-        <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/6.png" alt="">
-        <h4 class="priceDataAccessories">+£0.3 per unit</h4>
-      </div>
-      <div class="subcontainer_boxes_accessories">
-        <h3 class="dataAccessories">Coloured Plastic (closed face)</h3>
-        <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/2.png" alt="">
-        <h4 class="priceDataAccessories">+£0.3 per unit</h4>
-      </div>
-      <div class="subcontainer_boxes_accessories">
-        <h3 class="dataAccessories">Coloured Plastic (open face)</h3>
-        <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/1.png" alt="">
-        <h4 class="priceDataAccessories">+£0.25 per unit</h4>
-      </div>
+    <div class="subcontainer_boxes_accessories">
+      <h3 class="dataAccessories">Clear Plastic (closed face) </h3>
+      <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/6.png" alt="">
+      <h4 class="priceDataAccessories">+£0.3 per unit</h4>
     </div>
+    <div class="subcontainer_boxes_accessories">
+      <h3 class="dataAccessories">Coloured Plastic (closed face)</h3>
+      <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/2.png" alt="">
+      <h4 class="priceDataAccessories">+£0.3 per unit</h4>
+    </div>
+    <div class="subcontainer_boxes_accessories">
+      <h3 class="dataAccessories">Coloured Plastic (open face)</h3>
+      <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/1.png" alt="">
+      <h4 class="priceDataAccessories">+£0.25 per unit</h4>
+    </div>
+
+
 
     <div class="container_boxes_accessories">
       <h3 class="">Plastic Wallet</h3>
@@ -69,27 +68,29 @@ $jsVersion  = file_exists($jsPath)  ? filemtime($jsPath)  : time();
       </div>
     </div>
 
-    <div class="subcontainers_boxes_accessories">
 
-      <div class="subcontainer_boxes_accessories plastic_wallet_small">
-        <h3 class="dataAccessories">86 x 56mm </h3>
-        <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/7.png" alt="">
-        <h4 class="priceDataAccessories">+£0.12 per unit</h4>
-      </div>
-
-      <div class="subcontainer_boxes_accessories plastic_wallet_medium">
-        <h3 class="dataAccessories">103 x 70mm</h3>
-        <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/7.png" alt="">
-        <h4 class="priceDataAccessories">+£0.13 per unit</h4>
-      </div>
-
-      <div class="subcontainer_boxes_accessories plastic_wallet_big">
-        <h3 class="dataAccessories">138 x 88mm</h3>
-        <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/7.png" alt="">
-        <h4 class="priceDataAccessories">+£0.14 per unit</h4>
-      </div>
-
+    <div class="subcontainer_boxes_accessories plastic_wallet_small">
+      <h3 class="dataAccessories">86 x 56mm </h3>
+      <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/7.png" alt="">
+      <h4 class="priceDataAccessories">+£0.12 per unit</h4>
     </div>
+
+    <div class="subcontainer_boxes_accessories plastic_wallet_medium">
+      <h3 class="dataAccessories">103 x 70mm</h3>
+      <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/7.png" alt="">
+      <h4 class="priceDataAccessories">+£0.13 per unit</h4>
+    </div>
+
+    <div class="subcontainer_boxes_accessories plastic_wallet_big">
+      <h3 class="dataAccessories">138 x 88mm</h3>
+      <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/7.png" alt="">
+      <h4 class="priceDataAccessories">+£0.14 per unit</h4>
+    </div>
+
+
+
+
+
     <div class="container_boxes_accessories">
       <h3 class="">Retractable Reel</h3>
       <div class="arrow_accessories">
@@ -98,33 +99,39 @@ $jsVersion  = file_exists($jsPath)  ? filemtime($jsPath)  : time();
       </div>
     </div>
 
-    <div class="subcontainers_boxes_accessories">
-      <div class="subcontainer_boxes_accessories">
-        <h3 class="dataAccessories">Plastic</h3>
-        <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/12.png" alt="">
-        <h4 class="priceDataAccessories">+£0.5 per unit</h4>
-      </div>
-      <div class="subcontainer_boxes_accessories">
-        <h3 class="dataAccessories">Round</h3>
-        <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/12.png" alt="">
-        <h4 class="priceDataAccessories">+£0.85 per unit</h4>
-      </div>
-      <div class="subcontainer_boxes_accessories">
-        <h3 class="dataAccessories">Oval</h3>
-        <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/12.png" alt="">
-        <h4 class="priceDataAccessories">+£0.85 per unit</h4>
-      </div>
-      <div class="subcontainer_boxes_accessories">
-        <h3 class="dataAccessories">Metal</h3>
-        <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/12.png" alt="">
-        <h4 class="priceDataAccessories">+£1.10 per unit</h4>
-      </div>
-      <div class="subcontainer_boxes_accessories">
-        <h3 class="dataAccessories">Printing (per screen)</h3>
-        <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/12.png" alt="">
-        <h4 class="priceDataAccessories">+£25.0 per unit</h4>
-      </div>
+
+
+    <div class="subcontainer_boxes_accessories">
+      <h3 class="dataAccessories">Plastic</h3>
+      <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/12.png" alt="">
+      <h4 class="priceDataAccessories">+£0.5 per unit</h4>
     </div>
+    <div class="subcontainer_boxes_accessories">
+      <h3 class="dataAccessories">Round</h3>
+      <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/12.png" alt="">
+      <h4 class="priceDataAccessories">+£0.85 per unit</h4>
+    </div>
+    <div class="subcontainer_boxes_accessories">
+      <h3 class="dataAccessories">Oval</h3>
+      <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/12.png" alt="">
+      <h4 class="priceDataAccessories">+£0.85 per unit</h4>
+    </div>
+    <div class="subcontainer_boxes_accessories">
+      <h3 class="dataAccessories">Metal</h3>
+      <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/12.png" alt="">
+      <h4 class="priceDataAccessories">+£1.10 per unit</h4>
+    </div>
+    <div class="subcontainer_boxes_accessories">
+      <h3 class="dataAccessories">Printing (per screen)</h3>
+      <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/12.png" alt="">
+      <h4 class="priceDataAccessories">+£25.0 per unit</h4>
+    </div>
+
+
+
+
+
+
 
     <div class="container_boxes_accessories">
       <h3 class="">Other</h3>
@@ -134,20 +141,17 @@ $jsVersion  = file_exists($jsPath)  ? filemtime($jsPath)  : time();
       </div>
     </div>
 
-    <div class="subcontainers_boxes_accessories">
-      <div class="subcontainer_boxes_accessories">
-        <h3 class="dataAccessories">Cord Lock</h3>
-        <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/9.png" alt="">
-        <h4 class="priceDataAccessories">+£0.08 per unit</h4>
-      </div>
 
-      <div class="subcontainer_boxes_accessories">
-        <h3 class="dataAccessories">Whistle</h3>
-        <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/11.png" alt="">
-        <h4 class="priceDataAccessories">+£0.1 per unit</h4>
-      </div>
+    <div class="subcontainer_boxes_accessories">
+      <h3 class="dataAccessories">Cord Lock</h3>
+      <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/9.png" alt="">
+      <h4 class="priceDataAccessories">+£0.08 per unit</h4>
+    </div>
 
-
+    <div class="subcontainer_boxes_accessories">
+      <h3 class="dataAccessories">Whistle</h3>
+      <img class="imgaccessories" src="../../views/assets/img/global/customize-lanyard/sections/accessories/11.png" alt="">
+      <h4 class="priceDataAccessories">+£0.1 per unit</h4>
     </div>
 
   </div>
