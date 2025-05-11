512e2fbc662f4e54eda26c781f6ea99707b8303e
Aleina Rossui <admin@promoflow.net>
2025-05-11 11:56:32 -0700

    “…”

--- controller/users/register.php
+++ controller/users/register.php
@@ -21,7 +21,7 @@ class ApiHandlerRegister {
                 // Perform actions based on the request
                 switch ($action) {
                     case "register":
-                    echo json_encode($data);
+                    echo json_encode($data->nameRegister);
                     exit;
 
                         $this->handleRegistration($data);
