147d1d5399159aad3dd165f42cc70bbab489eac5
Aleina Rossui <admin@promoflow.net>
2025-05-12 22:37:40 -0700

    “…”

--- views/assets/js/shopping_cart/sections/shopping_cart.js
+++ views/assets/js/shopping_cart/sections/shopping_cart.js
@@ -58,9 +58,10 @@ class ShoppingCart {
     }*/
     })
     .catch(error => {
+      alert("error");
       // Handle specific errors (from throw in the .then block)
-      console.error("Error:", error.message);
-      alert(error.message); // Show the error message in an alert
+    //  console.error("Error:", error.message);
+      //alert(error.message); // Show the error message in an alert
     });
   }
 
