c8a227ba303fbd4cb8e3044fc5d47a2fb56d8094
Aleina Rossui <admin@promoflow.net>
2025-05-14 14:57:12 -0700

    “…”

--- views/assets/js/shopping_cart/sections/shopping_cart.js
+++ views/assets/js/shopping_cart/sections/shopping_cart.js
@@ -66,13 +66,89 @@ class ShoppingCart {
 
 
 
-  addJobstoOrder(data){
-    for (var i = 0; i < data.length; i++) {
+   addJobsToOrder(data) {
+    if (!Array.isArray(data)) return;
 
-      alert(JSON.stringify(data[i]["name"]));
+    // Limpiar contenido anterior
+    container_draw_items_shopping_cart.innerHTML = "";
 
-    }
+    data.forEach((item) => {
+      const name = item["name"] || "No name";
+
+      const itemHTML = `
+        <div class="items_shopping_cart">
+          <div class="product_items_shopping_cart">
+              <h3>${name}</h3>
+              <h3></h3>
+              <img class="arrow_products_shopping_cart" src="../../views/assets/img/shopping_cart/sections/arrow_right.png" alt="">
+          </div>
+          <div class="descriptions_items_shopping_cart">
+            <div class="elements_descriptions_items_shopping_cart">
+              <h3>Material</h3>
+              <h3>hola2</h3>
+              <h3>hola3</h3>
+            </div>
+            <div class="elements_descriptions_items_shopping_cart">
+              <h3>Lanyard type</h3>
+              <h3>hola2</h3>
+              <h3>hola3</h3>
+            </div>
+            <div class="elements_descriptions_items_shopping_cart">
+              <h3>Width</h3>
+              <h3>hola2</h3>
+              <h3>hola3</h3>
+            </div>
+            <div class="elements_descriptions_items_shopping_cart">
+              <h3>Side printed</h3>
+              <h3>hola2</h3>
+              <h3>hola3</h3>
+            </div>
+            <div class="elements_descriptions_items_shopping_cart">
+              <h3>Clips</h3>
+              <h3>hola2</h3>
+              <h3>hola3</h3>
+            </div>
+            <div class="elements_descriptions_items_shopping_cart">
+              <h3>Attachment</h3>
+              <h3>hola2</h3>
+              <h3>hola3</h3>
+            </div>
+            <div class="elements_descriptions_items_shopping_cart">
+              <h3>Accessories</h3>
+              <h3>hola2</h3>
+              <h3>hola3</h3>
+            </div>
+            <div class="elements_descriptions_items_shopping_cart">
+              <h3>Colour Quantity</h3>
+              <h3>hola2</h3>
+              <h3>hola3</h3>
+            </div>
+          </div>
+
+          <div class="summary_items_shopping_cart">
+            <div class="elements_summary_items_shopping_cart">
+              <h3>Cost per unit</h3>
+              <h3></h3>
+              <h3>hola3</h3>
+            </div>
+            <div class="elements_summary_items_shopping_cart">
+              <h3>Amount</h3>
+              <h3></h3>
+              <h3>hola3</h3>
+            </div>
+            <div class="elements_summary_items_shopping_cart">
+              <h3>Subtotal</h3>
+              <h3></h3>
+              <h3>hola3</h3>
+            </div>
+          </div>
+        </div>
+      `;
+
+      container_draw_items_shopping_cart.innerHTML += itemHTML;
+    });
   }
+
 
 
 
