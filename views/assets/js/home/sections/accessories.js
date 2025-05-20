32150bfd2386de337884dcdceabcd9b92b06f9d5
Aleina Rossui <admin@promoflow.net>
2025-05-19 23:15:26 -0700

    “…”

--- views/assets/js/home/sections/accessories.js
+++ views/assets/js/home/sections/accessories.js
@@ -95,9 +95,9 @@ class AccessoriesHome {
   // Function to add event listener to 'Add to Cart Accessory' buttons
   addToCartEvent(selector) {
     const cartButtons = document.querySelectorAll(selector);
-    cartButtons.forEach(button => {
+    cartButtons.forEach((button, index) => {
       button.addEventListener("click", () => {
-        alert("Your product has been added to the basket");
+        alert(index);
       });
     });
   }
