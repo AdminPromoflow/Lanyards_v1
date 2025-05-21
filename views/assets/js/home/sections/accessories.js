ade4e5a38e4d172603806fbb00bb5e5c55bdbf66
Aleina Rossui <admin@promoflow.net>
2025-05-20 20:37:18 -0700

    “…”

--- views/assets/js/home/sections/accessories.js
+++ views/assets/js/home/sections/accessories.js
@@ -41,7 +41,7 @@ class AccessoriesHome {
            const title = document.querySelectorAll(".name_accessory_item")[index].innerHTML;
 
         //   alert(title + box_accessories_home_description[i].innerHTML);
-        alert(i + " " + index);
+        alert(index  + " " + i);
 
       })
     }
