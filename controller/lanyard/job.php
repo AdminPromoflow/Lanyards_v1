76e9b26d490ca5d2b39af6b9bcb115d8fd05203e
Aleina Rossui <admin@promoflow.net>
2025-05-21 23:04:48 -0700

    “…”

--- controller/lanyard/job.php
+++ controller/lanyard/job.php
@@ -50,7 +50,7 @@ class Job {
 
     // 🛠️ Crea un nuevo trabajo (job)
     private function createJob($data) {
-      echo json_encode("hi");exit;
+    //  echo json_encode("hi");exit;
 
         $idOrder = $this->verifyOrden($data);
 
