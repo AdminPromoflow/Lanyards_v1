808590a42300c838e1176e642c28c69844455eee
Aleina Rossui <admin@promoflow.net>
2025-05-21 22:43:21 -0700

    “…”

--- views/assets/js/home/sections/accessories.js
+++ views/assets/js/home/sections/accessories.js
@@ -237,9 +237,58 @@ class AccessoriesHome {
 
 
 
-          alert(this.getPosicionChildSelected(index)  + "  " +   this.getProduct(index) + "  " + JSON.stringify(this.getDescription(index)) + "  " +  this.getPricePerUnit(index)
+        /*  alert(this.getPosicionChildSelected(index)  + "  " +   this.getProduct(index) + "  " + JSON.stringify(this.getDescription(index)) + "  " +  this.getPricePerUnit(index)
            + "  " +  this.getAmount(index) + "  " +  this.getTotal(index)
-        );
+        );*/
+
+
+
+
+        makeAjaxRequestGetAllMaterials() {
+          const url = "../../controller/lanyard/job.php";
+
+          const data = {
+            action: "createJob",
+            product: this.getProduct(index),
+            description: this.getDescription(index),
+            price_per_unit: this.getPricePerUnit(index),
+            amount: this.getAmount(index),
+            total: this.getTotal(index)
+          };
+
+          fetch(url, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+          })
+            .then(response => {
+              if (response.ok) {
+                return response.text();
+              }
+              throw new Error("Network error.");
+            })
+            .then(data => {
+
+
+              data = JSON.parse(data);
+
+              if (data["status"]) {
+                alert(data["message"]);
+                window.location.href = "../../views/shopping_cart/index.php";
+              }
+              else {
+
+              }
+
+
+
+            })
+            .catch(error => {
+              console.error("Error:", error);
+            });
+        }
 
 
 
