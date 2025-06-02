3861ff3e4b4104ede3a8aeeac61668a0909aaf0e
Aleina Rossui <admin@promoflow.net>
2025-06-02 04:37:44 -0700

    “…”

--- controller/lanyard/addresses.php
+++ controller/lanyard/addresses.php
@@ -74,7 +74,6 @@ class Addresses {
         }
     }
     private function updateAddresses($data) {
-      echo json_encode("uffff");exit;
         if (session_status() === PHP_SESSION_NONE) {
             session_start();
         }
