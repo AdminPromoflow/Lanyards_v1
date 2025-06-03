1597133dee64c75e91b8ed7a0bda2c54bf3d281e
Aleina Rossui <admin@promoflow.net>
2025-06-02 21:28:34 -0700

    “…”

--- controller/webhook/webhook.php
+++ controller/webhook/webhook.php
@@ -1,7 +1,7 @@
 <?php
 require '../../vendor/autoload.php';
-require_once '../../models/orders.php';
-require_once '../../controller/config/database.php';
+//require_once '../../models/orders.php';
+//require_once '../../controller/config/database.php';
 
 \Stripe\Stripe::setApiKey('sk_test_51RVWm7Iy7ZwkjsYRhmh4hsLctFV3lGr2HlAK5qn8eb7yAOTc9z2BTYRc2DVzvyRhLrndFR4MYMWBe6Kw2PA9Od3Z00UpRTyB8P');
 $endpoint_secret = 'whsec_5966d1384d72bd6d255e3ee1cce732be54436717c95630ac4bcdc96f968f64f1';
@@ -35,20 +35,20 @@ if ($event->type === 'checkout.session.completed') {
     }
 
     try {
-        $connection = new Database();
+      /*  $connection = new Database();
         $orderModel = new Order_Model($connection);
         $orderModel->setIdOrder($orderId);
         $orderModel->setStatus("processing");
 
-        $status = $orderModel->updateOrderStatus();
+        $status = $orderModel->updateOrderStatus();*/
 
-        $logMsg = date('Y-m-d H:i:s') . " | Order ID: $orderId | Status actualizado: " . ($status ? 'true' : 'false') . PHP_EOL;
+      /*  $logMsg = date('Y-m-d H:i:s') . " | Order ID: $orderId | Status actualizado: " . ($status ? 'true' : 'false') . PHP_EOL;
         file_put_contents(__DIR__ . '/stripe_webhook.log', $logMsg, FILE_APPEND);
 
         if (!$status) {
             http_response_code(500);
             exit('No se pudo actualizar el estado de la orden');
-        }
+        }*/
 
     } catch (Exception $e) {
         file_put_contents(__DIR__ . '/stripe_webhook.log', date('Y-m-d H:i:s') . " | Error de servidor: " . $e->getMessage() . PHP_EOL, FILE_APPEND);
