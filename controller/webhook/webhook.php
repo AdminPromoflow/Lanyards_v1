b199e01e409655de8a7618fa0203d3fa997f260e
Aleina Rossui <admin@promoflow.net>
2025-06-02 17:37:28 -0700

    “…”

--- controller/webhook/webhook.php
+++ controller/webhook/webhook.php
@@ -25,7 +25,7 @@ try {
   http_response_code(400);
   exit();
 }
-$_SESSION['success_payment'] = null;
+$_SESSION['success_payment'] = false;
 
 
 if ($event->type === 'checkout.session.completed') {
