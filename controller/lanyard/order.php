d60ce066e6c71f22bf42fc1751fbeeeeea78c9b8
Aleina Rossui <admin@promoflow.net>
2025-06-04 18:17:18 -0700

    “…”

--- controller/lanyard/order.php
+++ controller/lanyard/order.php
@@ -195,13 +195,13 @@ class Order {
 
       //  echo json_encode(['url' => $session->url]);
 
-    //  echo json_encode("buenas");exit;
 
 
         \Stripe\Stripe::setApiKey('sk_test_51RVWm7Iy7ZwkjsYRhmh4hsLctFV3lGr2HlAK5qn8eb7yAOTc9z2BTYRc2DVzvyRhLrndFR4MYMWBe6Kw2PA9Od3Z00UpRTyB8P');
         header('Content-Type: application/json');
 
         $YOUR_DOMAIN = 'https://lanyardsforyou.com/views/checkout/index.php';
+        echo json_encode("buenas");exit;
 
         $checkout_session = \Stripe\Checkout\Session::create([
           'line_items' => [[
