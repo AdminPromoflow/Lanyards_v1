c6c8fdf564d7d390f80d92ad31a0676b17f18048
Aleina Rossui <admin@promoflow.net>
2025-06-04 20:47:14 -0700

    “…”

--- controller/stripe/setSessionStrype.php
+++ controller/stripe/setSessionStrype.php
@@ -2,32 +2,28 @@
 require '../../vendor/autoload.php';
 
 
-\Stripe\Stripe::setApiKey('sk_test_51RVWm7Iy7ZwkjsYRhmh4hsLctFV3lGr2HlAK5qn8eb7yAOTc9z2BTYRc2DVzvyRhLrndFR4MYMWBe6Kw2PA9Od3Z00UpRTyB8P');
-header('Content-Type: application/json');
+
+$stripe = new \Stripe\StripeClient('sk_test_51RVWm7Iy7ZwkjsYRhmh4hsLctFV3lGr2HlAK5qn8eb7yAOTc9z2BTYRc2DVzvyRhLrndFR4MYMWBe6Kw2PA9Od3Z00UpRTyB8P');
 
 
-$checkout_session = \Stripe\Checkout\Session::create([
+$checkout_session = $stripe->checkout->sessions->create([
   'line_items' => [[
     'price_data' => [
-      'currency' => 'gbp',
-      'unit_amount' => 2499, // $24.99 en centavos
+      'currency' => 'usd',
       'product_data' => [
-        'name' => 'Lanyard personalizado'
-      ]
+        'name' => 'T-shirt',
+      ],
+      'unit_amount' => 2000,
     ],
-    'quantity' => 1
+    'quantity' => 1,
   ]],
   'mode' => 'payment',
   'success_url' => 'https://www.lanyardsforyou.com/views/success_payment/index.php',
   'cancel_url' => 'https://www.lanyardsforyou.com/views/success_payment/index.php',
 ]);
 
-
 header("HTTP/1.1 303 See Other");
 header("Location: " . $checkout_session->url);
-
-
-
 
 
 
