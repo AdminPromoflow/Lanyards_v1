5fd5d30420309120de817c90dd03c8a1121ac032
Aleina Rossui <admin@promoflow.net>
2025-06-04 18:15:56 -0700

    “…”

--- views/assets/js/checkout/sections/checkout.js
+++ views/assets/js/checkout/sections/checkout.js
@@ -78,7 +78,7 @@ class Checkout {
           data = JSON.parse(data);
 
 
-          const url = data["data"]["url"];
+          const url = data["url"];
           alert(url);
 
           window.location.href = url;
