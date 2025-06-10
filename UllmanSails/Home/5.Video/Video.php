49c678b1a0d4d7bddd183908fd626c64071642cf
Aleina Rossui <admin@promoflow.net>
2025-06-10 07:50:22 -0700

    “…”

--- UllmanSails/Home/5.Video/Video.php
+++ UllmanSails/Home/5.Video/Video.php
@@ -2,12 +2,13 @@
 $basePath = '../Home/5.Video/';
 $cssTime = filemtime($basePath . 'video.css');
 $jsTime = filemtime($basePath . 'video.js');
+$videoTime = filemtime($basePath . 'video.mp4'); // 🔹 Añadido
 ?>
 
 <link rel="stylesheet" href="<?= $basePath ?>video.css?v=<?= $cssTime ?>">
 
 <section class="videoHome">
-  <video id="videoHover" src="<?= $basePath ?>video.mp4" controls muted playsinline></video>
+  <video id="videoHover" src="<?= $basePath ?>video.mp4?v=<?= $videoTime ?>" controls muted playsinline></video>
 </section>
 
 <script src="<?= $basePath ?>video.js?v=<?= $jsTime ?>" type="text/javascript"></script>
