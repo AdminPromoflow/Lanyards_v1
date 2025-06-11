f612e2134d75ff36d81d627d676e4bff81de59f7
Aleina Rossui <admin@promoflow.net>
2025-06-10 17:53:37 -0700

    “…”

--- views/assets/js/shopping_cart/sections/shopping_cart.js
+++ views/assets/js/shopping_cart/sections/shopping_cart.js
@@ -503,11 +503,14 @@ class ShoppingCart {
     const price_shipping = document.getElementById("price_shipping");
     const total_price = document.getElementById("total_price");
 
+
+  //  alert(this.shippingPrice);
+
     if (this.shippingDays == 15) {
       this.shippingPrice = 0;
     }
     else if (this.shippingDays == 10) {
-      this.shippingPrice = this.getSubtotal()* 0.2 ;
+      this.shippingPrice = this.getSubtotal()* 0.25 ;
     }
     else if (this.shippingDays == 8) {
       this.shippingPrice = this.getSubtotal()* 0.5 ;
@@ -520,7 +523,7 @@ class ShoppingCart {
     this.total = this.subtotal + this.tax + this.shippingPrice;
 
 
-    alert(this.shippingPrice);
+
 
     price_shipping.textContent = "£" +this.shippingPrice;
 
