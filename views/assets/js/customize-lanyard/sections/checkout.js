9dafaedc08fa391db7b1ef53617f704442eda00e
Aleina Rossui <admin@promoflow.net>
2025-06-10 20:32:17 -0700

    “…”

--- views/assets/js/customize-lanyard/sections/checkout.js
+++ views/assets/js/customize-lanyard/sections/checkout.js
@@ -41,21 +41,25 @@ class Checkout {
   }
 
   async sendPDF() {
-    html2canvas(document.body, {
-  scale: 30, // alta resolución
-  useCORS: true
-}).then(canvas => {
-  const imgData = canvas.toDataURL("image/png");
+    const { jsPDF } = window.jspdf;
 
-  const pdf = new jsPDF({
-    orientation: 'portrait',
-    unit: 'px',
-    format: [canvas.width, canvas.height]
-  });
+      const elemento = document.body;
 
-  pdf.addImage(imgData, 'PNG', 0, 0, canvas.width, canvas.height);
-  pdf.save('pagina-completa.pdf');
-});
+      const canvas = await html2canvas(elemento, {
+        scale: 3,         // Alta resolución (2 o 3 es suficiente)
+        useCORS: true     // Permite imágenes externas si tienen CORS
+      });
+
+      const imgData = canvas.toDataURL('image/png');
+
+      const pdf = new jsPDF({
+        orientation: 'portrait',
+        unit: 'px',
+        format: [canvas.width, canvas.height]
+      });
+
+      pdf.addImage(imgData, 'PNG', 0, 0, canvas.width, canvas.height);
+      pdf.save('pagina-completa.pdf');
   }
 
 
