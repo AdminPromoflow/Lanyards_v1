7109d949cbb21a785a1c2be0329cb5a03bd2c90c
Aleina Rossui <admin@promoflow.net>
2025-06-16 13:06:21 -0700

    “…”

--- controller/users/session-user.php
+++ controller/users/session-user.php
@@ -94,9 +94,29 @@ class HandlerSessionUser {
         $this->sendResponse(200, ["message" => "Logout successful."]);
     }
     public function checkStripeSession(){
-      $session = \Stripe\Checkout\Session::retrieve($_GET['session_id']);
-      $orderId = $session->metadata->idOrder;
-      echo json_encode($orderId);
+      \Stripe\Stripe::setApiKey('sk_test_51RVWm7Iy7ZwkjsYRhmh4hsLctFV3lGr2HlAK5qn8eb7yAOTc9z2BTYRc2DVzvyRhLrndFR4MYMWBe6Kw2PA9Od3Z00UpRTyB8P'); // Tu clave secreta
+
+      if (!isset($_GET['session_id'])) {
+          echo json_encode(["error" => "Missing session_id"]);
+          exit;
+      }
+
+      try {
+          $session = \Stripe\Checkout\Session::retrieve($_GET['session_id']);
+
+          if (!isset($session->metadata->idOrder)) {
+              echo json_encode(["error" => "idOrder not found in metadata"]);
+              exit;
+          }
+
+          $orderId = $session->metadata->idOrder;
+          echo json_encode(["success" => true, "idOrder" => $orderId]);
+      } catch (Exception $e) {
+          echo json_encode([
+              "success" => false,
+              "error" => $e->getMessage()
+          ]);
+      }
     }
 }
 require '../../vendor/autoload.php';
