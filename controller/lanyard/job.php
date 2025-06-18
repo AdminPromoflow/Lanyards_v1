d464c8e7ea0ef202006cb0e7b7ed7c9899400970
Aleina Rossui <admin@promoflow.net>
2025-06-17 18:06:00 -0700

    “…”

--- controller/lanyard/job.php
+++ controller/lanyard/job.php
@@ -472,9 +472,9 @@ class Job {
           $connection = new Database();
           $order_model = new Order_Model($connection);
           $order_model->setIdOrder($idOrder);
-          $result = $order_model->getOrderByIdOrder()->status;
+        //  $result = $order_model->getOrderByIdOrder()->status;
 
-          echo json_encode(["message" => $order_model->getOrderByIdOrder()->$status]);exit;
+          echo json_encode(["message" => $order_model->getOrderByIdOrder()]);exit;
 
         }
 
