8acdfc49f2dfb362cbb5a52da75863d962a1a6a2
Aleina Rossui <admin@promoflow.net>
2025-06-18 12:39:36 -0700

    “…”

---
+++ views/assets/js/my_lanyards/sections/artwork_details.js
@@ -0,0 +1,12 @@
+class ArtworkDetailsClass {
+  constructor() {
+
+    
+  }
+
+}
+
+
+const section_artwork_detailss = document.getElementById("section_artwork_detailss");
+
+const artworkDetailsClass = new ArtworkDetailsClass();
---
+++ views/my_lanyards/sections/artwork_details.php
@@ -0,0 +1,28 @@
+<?php
+// Corregir rutas absolutas para evitar problemas con __DIR__
+$cssFile = realpath(__DIR__ . '/../../views/assets/css/artwork_details/sections/artwork_details.css');
+$jsFile = realpath(__DIR__ . '/../../views/assets/js/artwork_details/sections/artwork_details.js');
+
+$cssVersion = $cssFile && file_exists($cssFile) ? filemtime($cssFile) : time();
+$jsVersion = $jsFile && file_exists($jsFile) ? filemtime($jsFile) : time();
+?>
+
+<!-- Link to external stylesheet for styling the section -->
+<link rel="stylesheet" href="/views/assets/css/artwork_details/sections/artwork_details.css?v=<?= $cssVersion; ?>">
+
+<section id="section_artwork_details" class="section_artwork_details">
+  <!-- Title section for ABOUT US -->
+  <div class="title_artwork_details">
+    <h3>My products</h3>
+  </div>
+
+  <!-- Group container for about us boxes -->
+  <div class="groupBox_artwork_details">
+
+  </div>
+
+
+</section>
+
+<!-- Link to external JavaScript for additional functionality -->
+<script src="/views/assets/js/artwork_details/sections/artwork_details.js?v=<?= $jsVersion; ?>" type="text/javascript"></script>
