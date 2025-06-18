1061c778c1e0a678658d873626c12d0418dabde2
Aleina Rossui <admin@promoflow.net>
2025-06-17 21:38:05 -0700

    “…”

--- views/my_lanyards/index.php
+++ views/my_lanyards/index.php
@@ -4,10 +4,6 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Lanyards for you</title>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
-
 
 
   <?php
@@ -22,18 +18,11 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,200&family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
 
-  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-    integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-    crossorigin=""/>
 
-  <!-- Make sure you put this AFTER Leaflet's CSS -->
-  <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
-    integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
-    crossorigin=""></script>
 
   <?php
   // Validar CSS
-  $cssPath = '../../views/assets/css/home/style.css';
+  $cssPath = '../../views/assets/css/my_lanyards/style.css';
   $cssVersion = file_exists($cssPath) ? filemtime($cssPath) : time();
   ?>
   <link rel="stylesheet" type="text/css" href="<?= $cssPath ?>?v=<?= $cssVersion; ?>">
@@ -59,7 +48,7 @@
           <div class="bgLanyards">
 
             <!-- Include the slider component for the index page -->
-            <?php include "../../views/home/sections/4-about-us.php"; ?>
+            <?php include "../../views/home/sections/my_lanyards.php"; ?>
             <?php include "../../views/global/footer.php"; ?>
 
           </div>
@@ -78,7 +67,7 @@
 
   <?php
   // Validar archivos JavaScript
-  $jsApp = '../../views/assets/js/home/app.js';
+  $jsApp = '../../views/assets/js/my_lanyards/app.js';
   $jsLogin = '../../views/assets/js/global/login.js';
   $jsRegister = '../../views/assets/js/global/register.js';
 
--- views/my_lanyards/sections/my_lanyards.php
+++ views/my_lanyards/sections/my_lanyards.php
@@ -1,26 +1,26 @@
 <?php
 // Corregir rutas absolutas para evitar problemas con __DIR__
-$cssFile = realpath(__DIR__ . '/../../views/assets/css/home/sections/4-about-us.css');
-$jsFile = realpath(__DIR__ . '/../../views/assets/js/home/sections/4-about-us.js');
+$cssFile = realpath(__DIR__ . '/../../views/assets/css/home/sections/my_lanyards.css');
+$jsFile = realpath(__DIR__ . '/../../views/assets/js/home/sections/my_lanyards.js');
 
 $cssVersion = $cssFile && file_exists($cssFile) ? filemtime($cssFile) : time();
 $jsVersion = $jsFile && file_exists($jsFile) ? filemtime($jsFile) : time();
 ?>
 
 <!-- Link to external stylesheet for styling the section -->
-<link rel="stylesheet" href="/views/assets/css/home/sections/4-about-us.css?v=<?= $cssVersion; ?>">
+<link rel="stylesheet" href="/views/assets/css/home/sections/my_lanyards.css?v=<?= $cssVersion; ?>">
 
-<section class="section_about_us">
+<section class="section_my_lanyards">
   <!-- Title section for ABOUT US -->
-  <div class="titleAboutUs">
+  <div class="title_my_lanyards">
     <h3>About us</h3>
   </div>
 
   <!-- Group container for about us boxes -->
-  <div class="groupBoxAboutUs">
+  <div class="groupBox_my_lanyards">
 
     <!-- Box for first individual (Crafting Unique Lanyards) -->
-    <div class="boxAboutUs">
+    <div class="box_my_lanyards">
       <h3>Crafting Unique Lanyards</h3>
       <p>We are a company dedicated to creating bespoke lanyards from scratch,
         combining quality materials with unique designs. Our goal is to provide
@@ -29,7 +29,7 @@ $jsVersion = $jsFile && file_exists($jsFile) ? filemtime($jsFile) : time();
     </div>
 
     <!-- Box for second individual (Elevating Your Brand) -->
-    <div class="boxAboutUs">
+    <div class="box_my_lanyards">
       <h3>Elevating <br> Your Brand</h3>
       <p>With a focus on delivering the most popular lanyard styles, we help elevate
       your brand or event. Our experienced team is committed to innovation and
@@ -40,8 +40,8 @@ $jsVersion = $jsFile && file_exists($jsFile) ? filemtime($jsFile) : time();
   </div>
 
   <!-- Explore More Button -->
-  <button type="button" onclick="window.location.href='/views/about_us/index.php'">Explore More</button>
+  <button type="button" onclick="window.location.href='/views/my_lanyards/index.php'">Explore More</button>
 </section>
 
 <!-- Link to external JavaScript for additional functionality -->
-<script src="/views/assets/js/home/sections/4-about-us.js?v=<?= $jsVersion; ?>" type="text/javascript"></script>
+<script src="/views/assets/js/home/sections/my_lanyards.js?v=<?= $jsVersion; ?>" type="text/javascript"></script>
