2f4a7ef2eef13f02cace70bcf4b70606fc489ec0
Aleina Rossui <admin@promoflow.net>
2025-06-17 22:34:19 -0700

    “…”

--- views/assets/js/my_lanyards/sections/my_lanyards.js
+++ views/assets/js/my_lanyards/sections/my_lanyards.js
@@ -2,12 +2,12 @@ class MyLanyardsClass {
   constructor() {
     for (let i = 0; i < box_my_jobs.length; i++) {
       box_my_jobs[i].addEventListener("click", function(){
-        alert("bu");
+        alert(i);
       })
     }
   }
 }
 
 
-const box_my_jobs = document.getElementById("box_my_jobs");
+const box_my_jobs = document.querySelectorAll(".box_my_jobs");
 const myLanyardsClass = new MyLanyardsClass();
