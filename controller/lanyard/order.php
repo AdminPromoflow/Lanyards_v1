81e29ea8650f3ccfe36276235877d16109d39973
Aleina Rossui <admin@promoflow.net>
2025-06-18 15:19:54 -0700

    “…”

--- controller/lanyard/order.php
+++ controller/lanyard/order.php
@@ -193,6 +193,7 @@ class Order {
     }
 
     private function getOrdersWithJobsByEmail() {
+      echo json_encode("buenas");exit;
       if (session_status() === PHP_SESSION_NONE) {
           session_start();
       }
