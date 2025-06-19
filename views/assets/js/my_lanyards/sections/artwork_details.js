46e4cf7f1da55c615cfe032ba7e435f568328384
Aleina Rossui <admin@promoflow.net>
2025-06-19 04:20:24 -0700

    “…”

--- views/assets/js/my_lanyards/sections/artwork_details.js
+++ views/assets/js/my_lanyards/sections/artwork_details.js
@@ -11,7 +11,13 @@ class ArtworkDetailsClass {
     section_artwork_details.style.display = action;
   }
   manageLanyardDataJob(data){
-    alert(JSON.stringify(data));
+    //alert(JSON.stringify(data));
+
+
+    const descriptionObj = JSON.parse(data.description);
+
+    alert(descriptionObj.width.value);
+
 
     const  my_lanyards_left_side = document.getElementById('my_lanyards_left_side');
     const  my_lanyards_right_side = document.getElementById('my_lanyards_right_side');
