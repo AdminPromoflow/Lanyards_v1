f333b079ea81e518cfb3f2090fc48c2d30b42412
Aleina Rossui <admin@promoflow.net>
2025-06-19 04:26:53 -0700

    “…”

--- views/assets/js/my_lanyards/sections/artwork_details.js
+++ views/assets/js/my_lanyards/sections/artwork_details.js
@@ -12,15 +12,34 @@ class ArtworkDetailsClass {
   }
   manageLanyardDataJob(data){
     //alert(JSON.stringify(data));
-
+    const  my_lanyards_left_side = document.getElementById('my_lanyards_left_side');
+    const  my_lanyards_right_side = document.getElementById('my_lanyards_right_side');
 
     const descriptionObj = JSON.parse(data.description);
 
-    alert(descriptionObj.width.value);
+    if (descriptionObj.width.value == "10mm") {
+      my_lanyards_left_side.style.height = "0.48cm";
+      my_lanyards_right_side.style.height = "0.48cm";
+    }
+    else if (descriptionObj.width.value == "15mm") {
+      my_lanyards_left_side.style.height = "0.72cm";
+      my_lanyards_right_side.style.height = "0.72cm";
+    }
+    else if (descriptionObj.width.value == "20mm") {
+      my_lanyards_left_side.style.height = "0.96cm";
+      my_lanyards_right_side.style.height = "0.96cm";
+    }
+    else if (descriptionObj.width.value == "25mm") {
+      my_lanyards_left_side.style.height = "1.2cm";
+      my_lanyards_right_side.style.height = "1.2cm";
+    }
+    else if (descriptionObj.width.value == "30mm") {
+      my_lanyards_left_side.style.height = "1.44cm";
+      my_lanyards_right_side.style.height = "1.44cm";
+    }
 
 
-    const  my_lanyards_left_side = document.getElementById('my_lanyards_left_side');
-    const  my_lanyards_right_side = document.getElementById('my_lanyards_right_side');
+
 
   }
   manageLanyardArtwork(artwork){
