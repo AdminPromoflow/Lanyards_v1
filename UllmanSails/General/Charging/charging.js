eb87330d7409116df951e804b8323ae75a28cd9e
Aleina Rossui <admin@promoflow.net>
2025-07-12 12:57:32 -0700

    “…”

--- UllmanSails/General/Charging/charging.js
+++ UllmanSails/General/Charging/charging.js
@@ -1,6 +1,5 @@
 class Charging {
   constructor() {
-    alert('Constructor de Charging.js ejecutado');
     this.hideShowcharging(true);
   }
   hideShowcharging(action){
@@ -13,10 +12,9 @@ class Charging {
 
 // Esperamos a que el DOM esté completamente cargado antes de crear la instancia
 window.addEventListener('DOMContentLoaded', function() {
-  alert('DOMContentLoaded: Charging.js cargado');
   const chargingClass = new Charging();
   // Corregimos el nombre del método
   window.addEventListener('load', function() {
-    chargingClass.hideShowcharging(true);
+    chargingClass.hideShowcharging(false);
   });
 });
